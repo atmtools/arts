2e07f418848301fac719e271156bf3c8aea33539
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2001-07-23 10:47:15 +0000

    Renamed md.h to auto_md.h md.cc to auto_md.cc. Moved auto_md.cc creation to src/make_md_cc.cc.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@913 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/make_md_h.cc
+++ src/make_md_h.cc
@@ -17,14 +17,14 @@
 
 /*!
   \file   make_md_h.cc
-  \brief  This is a little C++ program that generates the file md.h from the
+  \brief  This is a little C++ program that generates the file auto_md.h from the
           workspace methods data md_data. 
 
-  The file md.h declares the enum
+  The file auto_md.h declares the enum
   type MdHandle that is used to access the method data, so it has
   to be made sure that the two are allways consistent.
 
-  A second file is produced: md.cc.
+  A second file is produced: auto_md.cc.
   This contains the `get-away' functions that provided the interface
   between the engine and the workspace methods. The get-functions all 
   have the same arguments:
@@ -69,7 +69,7 @@
 #include "token.h"
 #include "vecmat.h"
 #include "file.h"
-#include "wsv.h"
+#include "auto_wsv.h"
 #include "methods.h"
 
 
@@ -119,10 +119,10 @@ int main()
 	  return 1;
 	}
 
-      // Write md.h:
+      // Write auto_md.h:
       // -----------
       ofstream ofs;
-      open_output_file(ofs,"md.h");
+      open_output_file(ofs,"auto_md.h");
 
       ofs << "// This file was generated automatically by make_md_h.cc.\n";
       ofs << "// DO NOT EDIT !\n";
@@ -130,11 +130,11 @@ int main()
 	  << __DATE__ << ", "
 	  << __TIME__ << "\n\n";
 
-      ofs << "#ifndef md_h\n";
-      ofs << "#define md_h\n\n";
+      ofs << "#ifndef auto_md_h\n";
+      ofs << "#define auto_md_h\n\n";
 
       ofs << "#include \"vecmat.h\"\n"
-	  << "#include \"wsv.h\"\n"
+	  << "#include \"auto_wsv.h\"\n"
 	  << "#include \"parser.h\"\n"
 	  << "\n";
 
@@ -371,193 +371,10 @@ int main()
 
       ofs << "\n";
 
-      ofs << "\n#endif  // md_h\n";
+      ofs << "\n#endif  // auto_md_h\n";
 
-      // Close md.h.
+      // Close auto_md.h.
       ofs.close();
-
-      // Write md.cc:
-      // -----------
-      open_output_file(ofs,"md.cc");
-  
-      ofs << "// This file was generated automatically by make_md_h.cc.\n";
-      ofs << "// DO NOT EDIT !\n";
-      ofs << "// Generated: "
-	  << __DATE__ << ", "
-	  << __TIME__ << "\n\n";
-
-      ofs << "#include \"arts.h\"\n"
-	  << "#include \"make_array.h\"\n"
-	  << "#include \"md.h\"\n"
-	  << "\n";
-
-      // Declare wsv_data:
-      ofs << "// The workspace variable pointers:\n"
-	  << "extern const ARRAY<WsvP*> wsv_pointers;\n\n"
-
-	  << "// Other wsv data:\n"
-	  << "extern const ARRAY<WsvRecord> wsv_data;\n\n";
-
-
-      // Write all get-away functions:
-      // -----------------------------
-      for (size_t i=0; i<n_md; ++i)
-	{
-	  // This is needed to flag the first function parameter, which 
-	  // needs no line break before being written:
-	  bool is_first_parameter = true;
-	  // The string indent is needed to achieve the correct
-	  // indentation of the functin parameters:
-	  string indent = string(md_data[i].Name().size()+3,' ');;
-	  
-	  // There are four lists of parameters that we have to
-	  // write. 
-	  ARRAY<size_t>  vo=md_data[i].Output();   // Output 
-	  ARRAY<size_t>  vi=md_data[i].Input();    // Input
-	  ARRAY<size_t>  vgo=md_data[i].GOutput();   // Generic Output 
-	  ARRAY<size_t>  vgi=md_data[i].GInput();    // Generic Input
-	  // vo and vi contain handles of workspace variables, 
-	  // vgo and vgi handles of workspace variable groups.
-
-	  // Check, if some workspace variables are in both the
-	  // input and the output list, and erase those from the input 
-	  // list:
-	  for (ARRAY<size_t>::const_iterator j=vo.begin(); j<vo.end(); ++j)
-	    for (ARRAY<size_t>::iterator k=vi.begin(); k<vi.end(); ++k)
-	      if ( *j == *k )
-		{
-		  //		  erase_vector_element(vi,k);
-		  k = vi.erase(k) - 1;
-		  // We need the -1 here, otherwise due to the
-		  // following increment we would miss the element
-		  // behind the erased one, which is now at the
-		  // position of the erased one.
-		}
-
-	  // There used to be a similar block here for the generic
-	  // input/output variables. However, this was a mistake. For
-	  // example, if a method has a vector as generic input and a
-	  // vector as generic output, this does not mean that it is
-	  // the same vector!
-
-
-	  ofs << "void " << md_data[i].Name()
-	      << "_g(WorkSpace& ws, const MRecord& mr)\n";
-	  ofs << "{\n";
-
-
-	  // Define generic output pointers
-	  for (size_t j=0; j<vgo.size(); ++j)
-	    {
-	      ofs << "  " << wsv_group_names[md_data[i].GOutput()[j]]
-		  << " *GO" << j << " = *wsv_pointers[mr.Output()[" << j
-		  << "]];\n";
-	    }
-
-	  // Define generic input pointers
-	  for (size_t j=0; j<vgi.size(); ++j)
-	    {
-	      ofs << "  " << wsv_group_names[md_data[i].GInput()[j]]
-		  << " *GI" << j << " = *wsv_pointers[mr.Input()[" << j
-		  << "]];\n";
-	    }
-
-	  ofs << "  " << md_data[i].Name() << "(";
-
-	  // Write the Output workspace variables:
-	  for (size_t j=0; j<vo.size(); ++j)
-	    {
-	      // Add comma and line break, if not first element:
-	      align(ofs,is_first_parameter,indent);
-
-	      ofs << "ws." << wsv_data[vo[j]].Name();
-	    }
-
-	  // Write the Generic output workspace variables:
-	  for (size_t j=0; j<vgo.size(); ++j)
-	    {
-	      // Add comma and line break, if not first element:
-	      align(ofs,is_first_parameter,indent);
-
-	      ofs << "*GO" << j;
-	    }
-
-	  // Write the Generic output workspace variable names:
-	  for (size_t j=0; j<vgo.size(); ++j)
-	    {
-	      // Add comma and line break, if not first element:
-	      align(ofs,is_first_parameter,indent);
-
-	      ofs << "wsv_data[mr.Output()["
-		  << j
-		  << "]].Name()";
-	    }
-
-	  // Write the Input workspace variables:
-	  for (size_t j=0; j<vi.size(); ++j)
-	    {
-	      // Add comma and line break, if not first element:
-	      align(ofs,is_first_parameter,indent);
-
-	      ofs << "ws." << wsv_data[vi[j]].Name();
-	    }
-
-	  // Write the Generic input workspace variables:
-	  for (size_t j=0; j<vgi.size(); ++j)
-	    {
-	      // Add comma and line break, if not first element:
-	      align(ofs,is_first_parameter,indent);
-
-	      ofs << "*GI" << j;
-	    }
-
-	  // Write the Generic input workspace variable names:
-	  for (size_t j=0; j<vgi.size(); ++j)
-	    {
-	      // Add comma and line break, if not first element:
-	      align(ofs,is_first_parameter,indent);
-
-	      ofs << "wsv_data[mr.Input()["
-		  << j
-		  << "]].Name()";
-	    }
-
-	  // Write the control parameters:
-	  {
-	    // The mr parameters look all the same (mr[i]), so we just
-	    // need to know the number of them: 
-	    size_t n_mr = md_data[i].Keywords().size();
-
-	    for (size_t j=0; j!=n_mr; ++j)
-	      {
-		// Add comma and line break, if not first element:
-		align(ofs,is_first_parameter,indent);
-
-		ofs << "mr.Values()[" << j << "]";
-	      }
-	  }
-
-	  ofs << ");\n";
-	  ofs << "}\n\n";
-	}
-
-      // Add getaways, the array that hold pointers to the getaway functions:
-      {
-	string indent = "     ";
-	bool is_first_parameter = true;
-
-	ofs << "// The array holding the pointers to the getaway functions.\n"
-	    << "void (*getaways[])(WorkSpace&, const MRecord&)\n"
-	    << "  = {";
-	for (size_t i=0; i<n_md; ++i)
-	  {
-	    // Add comma and line break, if not first element:
-	    align(ofs,is_first_parameter,indent);
-
-	    ofs << md_data[i].Name() << "_g";
-	  }
-	ofs << "};\n\n";
-      }
 
     }
   catch (exception x)
