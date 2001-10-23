0510a6f6828539bfc7256d018e24d16e5e33b4ed
cverdes <cverdes@aaf1aab0-4228-0410-ad68-8dceda47f409>
2001-10-23 17:24:40 +0000

    * arts-0-1-24
    	* ami/physics/za2geomtan.m adde to ami directory. This function
    	converts zenith angles to tangent altitudes
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1186 aaf1aab0-4228-0410-ad68-8dceda47f409

--- ami/physics/za2geomtan.m
+++ ami/physics/za2geomtan.m
@@ -1,22 +1,16 @@
 %------------------------------------------------------------------------
-% NAME:    za2geomtan
+% NAME:    za2geomztan
 %
-%          Converts a vector of zenith angles to geometrical tangent
-%          altitudes 
-%
-% FORMAT:  z_tan = geomtan2za(r_geoid,z_plat,za)
-%
-% RETURN:  z_tan     Geometrical tangent altitudes. 
+%          Converts a vector of zenith angles to geometrical tangent altitudes.
+% RETURN:  z_tan     tangent altitudes corresponding to za.
 % IN:      r_geoid   Geoid radius.
 %          z_plat    Platform altitude (above geoid).
-%          za        Zenith angles corresponding to z_tan.
+%          za        zenith angles. 
+%------------------------------------------------------------------------
+function z_tan =  za2geomztan(r_geoid,z_plat,za);
 %------------------------------------------------------------------------
 
-% HISTORY: 2001.10.15  Created by Carlos Jimenez 
+DEG2RAD        = 0.01745329251994;
+z_tan = (r_geoid+z_plat)*sin(DEG2RAD*za) - r_geoid;
 
-
-function z_tan = za2geomtan(r_geoid,z_plat,za)
-
-
-z_tan = (r_geoid+z_plat).*sin(pi*(180-za)/180)-r_geoid;
 
