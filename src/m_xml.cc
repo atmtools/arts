b0cb7f643d73cdd39279f49a20dbb9a0d10e7bfc
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-05-15 19:12:24 +0000

    Removed dummy output from remaining functions.
    Added read/write functions for Tensor3 - Tensor7.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1629 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/m_xml.cc
+++ src/m_xml.cc
@@ -36,6 +36,11 @@
 #include <math.h>
 #include "arts.h"
 #include "matpackI.h"
+#include "matpackIII.h"
+#include "matpackIV.h"
+#include "matpackV.h"
+#include "matpackVI.h"
+#include "matpackVII.h"
 #include "xml_io.h"
 #include "messages.h"
 #include "auto_md.h"
@@ -93,79 +98,130 @@ GenericWriteXML (// WS Generic Output:
   xml_write_to_file (filename, v);
 }
 
+//=== ArrayOfIndex =====================================================
+
+void
+ArrayOfIndexReadXML (// WS Generic Output:
+                     ArrayOfIndex& ai,
+                     // WS Generic Output Names:
+                     const String& ai_name,
+                     // Control Parameters:
+                     const String& f)
+{
+  GenericReadXML (ai, ai_name, f);
+}
+
+
+void
+ArrayOfIndexWriteXML (// WS Generic Output:
+                      const ArrayOfIndex& ai,
+                      // WS Generic Output Names:
+                      const String&       ai_name,
+                      // Control Parameters:
+                      const String&       f)
+{
+  GenericWriteXML (ai, ai_name, f);
+}
+
+
+//=== ArrayOfMatrix ====================================================
+
+void
+ArrayOfMatrixReadXML (// WS Generic Output:
+                      ArrayOfMatrix& am,
+                      // WS Generic Output Names:
+                      const String&  am_name,
+                      // Control Parameters:
+                      const String&  f)
+{
+  GenericReadXML (am, am_name, f);
+}
+
+
+void
+ArrayOfMatrixWriteXML (// WS Generic Output:
+                       const ArrayOfMatrix& am,
+                       // WS Generic Output Names:
+                       const String&        am_name,
+                       // Control Parameters:
+                       const String&        f)
+{
+  GenericWriteXML (am, am_name, f);
+}
+
+//=== ArrayOfString ====================================================
+
+void
+ArrayOfStringReadXML (// WS Generic Output:
+                      ArrayOfString& as,
+                      // WS Generic Output Names:
+                      const String& as_name,
+                      // Control Parameters:
+                      const String& f)
+{
+  GenericReadXML (as, as_name, f);
+}
+
+
+void
+ArrayOfStringWriteXML (// WS Generic Input:
+                       const ArrayOfString& as,
+                       // WS Generic Input Names:
+                       const String& as_name,
+                       // Control Parameters:
+                       const String& f)
+{
+  GenericWriteXML (as, as_name, f);
+}
+
+//=== ArrayOfVector ====================================================
+
+void
+ArrayOfVectorReadXML (// WS Generic Output:
+                      ArrayOfVector& av,
+                      // WS Generic Output Names:
+                      const String&  av_name,
+                      // Control Parameters:
+                      const String&  f)
+{
+  GenericReadXML (av, av_name, f);
+}
+
+
+void
+ArrayOfVectorWriteXML (// WS Generic Output:
+                       const ArrayOfVector& av,
+                       // WS Generic Output Names:
+                       const String&        av_name,
+                       // Control Parameters:
+                       const String&        f)
+{
+  GenericWriteXML (av, av_name, f);
+}
+
 //=== Index ===========================================================
 
 void
 IndexReadXML (// WS Generic Output:
-              Index&        v,
+              Index&        i,
               // WS Generic Output Names:
-              const String& v_name,
+              const String& i_name,
               // Control Parameters:
               const String& f )
 {
-  GenericReadXML (v, v_name, f);
+  GenericReadXML (i, i_name, f);
 }
 
 
 void
 IndexWriteXML (// WS Generic Output:
-               const Index&  v,
+               const Index&  i,
                // WS Generic Output Names:
-               const String& v_name,
+               const String& i_name,
                // Control Parameters:
                const String& f)
 {
-  GenericWriteXML (v, v_name, f);
-}
-
-//=== Numeric ==========================================================
-
-void
-NumericReadXML (// WS Generic Output:
-                Numeric&      v,
-                // WS Generic Output Names:
-                const String& v_name,
-                // Control Parameters:
-                const String& f)
-{
-  GenericReadXML (v, v_name, f);
-}
-
-
-void
-NumericWriteXML (// WS Generic Output:
-                 const Numeric& v,
-                 // WS Generic Output Names:
-                 const String&  v_name,
-                 // Control Parameters:
-                 const String&  f)
-{
-  GenericWriteXML (v, v_name, f);
-}
-
-//=== Vector ==========================================================
-
-void
-VectorReadXML (// WS Generic Output:
-               Vector&       v,
-               // WS Generic Output Names:
-               const String& v_name,
-               // Control Parameters:
-               const String& f)
-{
-  GenericReadXML (v, v_name, f);
-}
-
-
-void
-VectorWriteXML (// WS Generic Output:
-                const Vector& v,
-                // WS Generic Output Names:
-                const String& v_name,
-                // Control Parameters:
-                const String& f)
-{
-  GenericWriteXML (v, v_name, f);
+  GenericWriteXML (i, i_name, f);
 }
 
 //=== Matrix ==========================================================
@@ -193,104 +249,32 @@ MatrixWriteXML (// WS Generic Output:
   GenericWriteXML (m, m_name, f);
 }
 
-//=== ArrayOfIndex =====================================================
+//=== Numeric ==========================================================
 
 void
-ArrayOfIndexWriteXML (// WS Generic Output:
-                      const ArrayOfIndex& v,
-                      // WS Generic Output Names:
-                      const String&       v_name,
-                      // Control Parameters:
-                      const String&       f)
-{
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
-}
-
-
-void
-ArrayOfIndexReadXML (// WS Generic Output:
-                     ArrayOfIndex& v,
-                     // WS Generic Output Names:
-                     const String& v_name,
-                     // Control Parameters:
-                     const String& f)
-{
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
-}
-
-//=== ArrayOfVector ====================================================
-
-void
-ArrayOfVectorWriteXML (// WS Generic Output:
-                       const ArrayOfVector& av,
-                       // WS Generic Output Names:
-                       const String&        av_name,
-                       // Control Parameters:
-                       const String&        f)
-{
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
-}
-
-
-void
-ArrayOfVectorReadXML (// WS Generic Output:
-                      ArrayOfVector& av,
-                      // WS Generic Output Names:
-                      const String&  av_name,
-                      // Control Parameters:
-                      const String&  f)
-{
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
-}
-
-//=== ArrayOfMatrix ====================================================
-
-void
-ArrayOfMatrixReadXML (// WS Generic Output:
-                      ArrayOfMatrix& am,
-                      // WS Generic Output Names:
-                      const String&  am_name,
-                      // Control Parameters:
-                      const String&  f)
-{
-  GenericReadXML (am, am_name, f);
-}
-
-
-void
-ArrayOfMatrixWriteXML (// WS Generic Output:
-                       const ArrayOfMatrix& am,
-                       // WS Generic Output Names:
-                       const String&        am_name,
-                       // Control Parameters:
-                       const String&        f)
-{
-  GenericWriteXML (am, am_name, f);
-}
-
-//=== String ===============================================================
-
-void
-StringWriteXML (// WS Generic Output:
-                const String& s,
+NumericReadXML (// WS Generic Output:
+                Numeric&      n,
                 // WS Generic Output Names:
-                const String& s_name,
+                const String& n_name,
                 // Control Parameters:
                 const String& f)
 {
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
+  GenericReadXML (n, n_name, f);
 }
 
+
+void
+NumericWriteXML (// WS Generic Output:
+                 const Numeric& n,
+                 // WS Generic Output Names:
+                 const String&  n_name,
+                 // Control Parameters:
+                 const String&  f)
+{
+  GenericWriteXML (n, n_name, f);
+}
+
+//=== String ===============================================================
 
 void
 StringReadXML (// WS Generic Output:
@@ -300,42 +284,168 @@ StringReadXML (// WS Generic Output:
                // Control Parameters:
                const String& f)
 {
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
+  GenericReadXML (s, s_name, f);
 }
-
-
-
-//=== ArrayOfString ====================================================
-
-void
-ArrayOfStringWriteXML (// WS Generic Input:
-                       const ArrayOfString& as,
-                       // WS Generic Input Names:
-                       const String& as_name,
-                       // Control Parameters:
-                       const String& f)
-{
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
-}
-
 
 
 void
-ArrayOfStringReadXML (// WS Generic Output:
-                      ArrayOfString& as,
-                      // WS Generic Output Names:
-                      const String& as_name,
-                      // Control Parameters:
-                      const String& f)
+StringWriteXML (// WS Generic Output:
+                const String& s,
+                // WS Generic Output Names:
+                const String& s_name,
+                // Control Parameters:
+                const String& f)
 {
-  ostringstream os;
-  os << "Not yet implemented";
-  throw runtime_error(os.str());
+  GenericWriteXML (s, s_name, f);
+}
+
+//=== Tensor3 ==============================================================
+
+void
+Tensor3ReadXML (// WS Generic Output:
+               Tensor3& t,
+               // WS Generic Output Names:
+               const String& t_name,
+               // Control Parameters:
+               const String& f)
+{
+  GenericReadXML (t, t_name, f);
 }
 
 
+void
+Tensor3WriteXML (// WS Generic Output:
+                const Tensor3& t,
+                // WS Generic Output Names:
+                const String& t_name,
+                // Control Parameters:
+                const String& f)
+{
+  GenericWriteXML (t, t_name, f);
+}
+
+//=== Tensor4 ==============================================================
+
+void
+Tensor4ReadXML (// WS Generic Output:
+               Tensor4& t,
+               // WS Generic Output Names:
+               const String& t_name,
+               // Control Parameters:
+               const String& f)
+{
+  GenericReadXML (t, t_name, f);
+}
+
+
+void
+Tensor4WriteXML (// WS Generic Output:
+                const Tensor4& t,
+                // WS Generic Output Names:
+                const String& t_name,
+                // Control Parameters:
+                const String& f)
+{
+  GenericWriteXML (t, t_name, f);
+}
+
+//=== Tensor5 ==============================================================
+
+void
+Tensor5ReadXML (// WS Generic Output:
+               Tensor5& t,
+               // WS Generic Output Names:
+               const String& t_name,
+               // Control Parameters:
+               const String& f)
+{
+  GenericReadXML (t, t_name, f);
+}
+
+
+void
+Tensor5WriteXML (// WS Generic Output:
+                const Tensor5& t,
+                // WS Generic Output Names:
+                const String& t_name,
+                // Control Parameters:
+                const String& f)
+{
+  GenericWriteXML (t, t_name, f);
+}
+
+//=== Tensor6 ==============================================================
+
+void
+Tensor6ReadXML (// WS Generic Output:
+               Tensor6& t,
+               // WS Generic Output Names:
+               const String& t_name,
+               // Control Parameters:
+               const String& f)
+{
+  GenericReadXML (t, t_name, f);
+}
+
+
+void
+Tensor6WriteXML (// WS Generic Output:
+                const Tensor6& t,
+                // WS Generic Output Names:
+                const String& t_name,
+                // Control Parameters:
+                const String& f)
+{
+  GenericWriteXML (t, t_name, f);
+}
+
+//=== Tensor7 ==============================================================
+
+void
+Tensor7ReadXML (// WS Generic Output:
+               Tensor7& t,
+               // WS Generic Output Names:
+               const String& t_name,
+               // Control Parameters:
+               const String& f)
+{
+  GenericReadXML (t, t_name, f);
+}
+
+
+void
+Tensor7WriteXML (// WS Generic Output:
+                const Tensor7& t,
+                // WS Generic Output Names:
+                const String& t_name,
+                // Control Parameters:
+                const String& f)
+{
+  GenericWriteXML (t, t_name, f);
+}
+
+//=== Vector ==========================================================
+
+void
+VectorReadXML (// WS Generic Output:
+               Vector&       v,
+               // WS Generic Output Names:
+               const String& v_name,
+               // Control Parameters:
+               const String& f)
+{
+  GenericReadXML (v, v_name, f);
+}
+
+
+void
+VectorWriteXML (// WS Generic Output:
+                const Vector& v,
+                // WS Generic Output Names:
+                const String& v_name,
+                // Control Parameters:
+                const String& f)
+{
+  GenericWriteXML (v, v_name, f);
+}
 
