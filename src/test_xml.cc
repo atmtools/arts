55b33104795436a08293f9e0214a9e3f3c5245c6
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-05-30 11:21:24 +0000

    (main): Added exception handler.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1678 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/test_xml.cc
+++ src/test_xml.cc
@@ -3,20 +3,28 @@ using namespace std;
 #include <iostream>
 #include <string>
 #include "xml_io.h"
+#include "exceptions.h"
 
 int
 main (int argc, char *argv[])
 {
   String str;
 
-  cout << "Testing StringWriteXML" << endl;
-  str = "Hello World";
-  xml_write_to_file ("string.xml", str);
-  cout << "Wrote: " << str << endl;
-
-  cout << endl << "Testing StringReadXML" << endl;
-  xml_read_from_file ("string.xml", str);
-  cout << "Read: " << str << endl;
+  try
+    {
+      cout << "Testing StringWriteXML" << endl;
+      str = "Hello World";
+      xml_write_to_file ("string.xml", str);
+      cout << "Wrote: " << str << endl;
+      
+      cout << endl << "Testing StringReadXML" << endl;
+      xml_read_from_file ("string.xml", str);
+      cout << "Read: " << str << endl;
+    }
+  catch (runtime_error e)
+    {
+      cerr << e.what ();
+    }
 
   return (0);
 }
