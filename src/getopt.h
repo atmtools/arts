9898d035d6f811080071d5c63c8cda2a2f64fe4c
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-06-14 16:35:18 +0000

    Use getopt_long prototyping for GNU C, HP-UX and SunOS.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1773 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/getopt.h
+++ src/getopt.h
@@ -99,7 +99,8 @@ struct option
 #define optional_argument	2
 
 #if defined (__STDC__) && __STDC__
-#ifdef __GNU_LIBRARY__
+#if defined (__GNU_LIBRARY__) || defined (HPUX) || defined (SUNOS)
+
 /* Many other libraries have conflicting prototypes for getopt, with
    differences in the consts, in stdlib.h.  To avoid compilation
    errors, only prototype getopt for the GNU C library.  */
