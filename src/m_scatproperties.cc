0eff4ea3edad2a17afdd28853e57b9c3434b6752
rekha <rekha@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-06-14 17:18:57 +0000

    *** empty log message ***
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1779 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/m_scatproperties.cc
+++ src/m_scatproperties.cc
@@ -18,6 +18,7 @@
 #include "scatproperties.h"
 #include "logic.h"
 
+
 //! This method computes the extinction matrix for a single particle type
 //  from teh amplitude matrix.
 /*! 
@@ -28,6 +29,7 @@
 \param scat_za_index  Input : local zenith angle
 \param scat_aa_index  Input : local azimuth angle
 */
+
 void ext_mat_sptCalc(Tensor3& ext_mat_spt,
 		     const Tensor6& amp_mat,
 		     const Index& scat_za_index,
@@ -38,16 +40,21 @@ void ext_mat_sptCalc(Tensor3& ext_mat_spt,
 {
   Index npt = ext_mat_spt.npages();
   Index stokes_dim = ext_mat_spt.nrows();
+
   if (amp_mat.ncols() != 8)
-    {
-      throw runtime_error("amplitude matrix has 8 columns");
-    }
+    throw runtime_error(
+			"Amplitude matrix must have 8 columns.");
+  
   if (stokes_dim > 4 || stokes_dim <1){
-    throw runtime_error("the dimension of stokes vector can be only 1,2,3, or 4");
+    throw runtime_error(
+			"The dimension of stokes vector can be "
+			"only 1,2,3, or 4");
   }
+
   //find out frequency
   Numeric freq = f_grid[scat_f_index];
-  for (Index i=0; i<npt; ++i)
+  
+  for (Index i = 0; i < npt; ++i)
     {
       ConstVectorView amp_coeffs = amp_mat(i,
 				      scat_za_index,
@@ -57,9 +64,13 @@ void ext_mat_sptCalc(Tensor3& ext_mat_spt,
 				      Range(joker)
 				      );
       
-      amp2ext(ext_mat_spt(i,Range(joker),Range(joker)),amp_coeffs,freq);
+      amp2ext(ext_mat_spt(i,Range(joker),Range(joker)),
+	      amp_coeffs,
+	      freq);
     }
 }
+
+
 //! this function calculates phase matrix for a single particle type from 
 //  the amplitude matrix
 /*! 
@@ -70,6 +81,8 @@ void ext_mat_sptCalc(Tensor3& ext_mat_spt,
  \param scat_aa_index  Input : azimuth angle index
   
 */
+
+
 void pha_mat_sptCalc(Tensor5& pha_mat_spt,
 		     const Tensor6& amp_mat,
 		     const Index& scat_za_index,
@@ -78,28 +91,35 @@ void pha_mat_sptCalc(Tensor5& pha_mat_spt,
 {
   Index npt = pha_mat_spt.nshelves();
   Index stokes_dim = pha_mat_spt.nrows();
-  if (pha_mat_spt.nrows() != stokes_dim || pha_mat_spt.ncols() != stokes_dim ){
-    throw runtime_error(" pha_mat_spt : should have 4 rows and 4 columns");
+
+  if (pha_mat_spt.nrows() != stokes_dim || 
+      pha_mat_spt.ncols() != stokes_dim){
+    throw runtime_error(" The tensor pha_mat_spt should have"
+			"4 rows and 4 columns.");
   }
   if (amp_mat.ncols() != 8){
-    throw runtime_error("amplitude matrix has 8 columns");
+    throw runtime_error("Amplitude matrix must have 8 columns.");
   }
   if (stokes_dim > 4 || stokes_dim <1){
-    throw runtime_error("the dimension of stokes vector can be only 1,2,3, or 4");
+    throw runtime_error("The dimension of stokes vector" 
+                         "can be only 1,2,3, or 4");
   }
-  for (Index i=0; i<npt; ++i)
+  for (Index i = 0; i < npt; ++i)
     {
       const Tensor3 amp_coeffs = amp_mat(i,
-					   scat_za_index,
-					   scat_aa_index,
-					   Range(joker),
-					   Range(joker),
-					   Range(joker)
-					   );
-      amp2pha(pha_mat_spt(i,Range(joker),Range(joker),Range(joker),Range(joker)),
-	      amp_coeffs);
+					 scat_za_index,
+					 scat_aa_index,
+					 Range(joker),
+					 Range(joker),
+					 Range(joker)
+					 );
+      amp2pha(pha_mat_spt(i, Range(joker), Range(joker), 
+			  Range(joker), Range(joker)),
+	      amp_coeffs); 
     }
 }
+
+
 //! this function calculates the absorption coefficient for a single particle type
 // from the extinction matrix and phase matrix.
 /*! 
@@ -109,6 +129,7 @@ void pha_mat_sptCalc(Tensor5& pha_mat_spt,
   \param pha_mat_spt  Input : phase matrix for  a single particle type
  
 */
+
 void abs_vec_sptCalc(Matrix& abs_vec_spt,
 		     const Tensor3& ext_mat_spt,
 		     const Tensor5& pha_mat_spt
@@ -116,28 +137,43 @@ void abs_vec_sptCalc(Matrix& abs_vec_spt,
 {
  Index npt = abs_vec_spt.nrows();
  Index stokes_dim = abs_vec_spt.ncols();
+
  if (abs_vec_spt.ncols() != stokes_dim ){
-    throw runtime_error(" abs_vec_spt : should have 4 columns");
+    throw runtime_error("The vector abs_vec_spt should have 4 columns");
  }
- if (ext_mat_spt.nrows() != stokes_dim || ext_mat_spt.ncols() != stokes_dim ){
-   throw runtime_error(" ext_mat_spt : should have 4 rows and 4 columns");
+
+ if (ext_mat_spt.nrows() != stokes_dim || 
+     ext_mat_spt.ncols() != stokes_dim ){
+   throw runtime_error("The tensor ext_mat_spt should"
+		       " have 4 rows and 4 columns");
  }
- if (pha_mat_spt.nrows() != stokes_dim || pha_mat_spt.ncols() != stokes_dim ){
-   throw runtime_error(" pha_mat_spt : should have 4 rows and 4 columns");
+
+ if (pha_mat_spt.nrows() != stokes_dim || 
+     pha_mat_spt.ncols() != stokes_dim ){
+   throw runtime_error("The tensor pha_mat_spt should"
+		       "have 4 rows and 4 columns");
  }
+
  if (stokes_dim > 4 || stokes_dim <1){
-    throw runtime_error("the dimension of stokes vector can be only 1,2,3, or 4");
+    throw runtime_error("The dimension of stokes vector "
+                        "can be only 1,2,3, or 4");
  }
- for (Index i=0;i<npt;++i)
+
+ for (Index i = 0; i < npt; ++i)
    {
      ConstMatrixView ext = ext_mat_spt(i,
-				  Range(joker),
-				  Range(joker));
+				       Range(joker),
+				       Range(joker));
+     
      ConstTensor4View pha=pha_mat_spt(i,
-				 Range(joker),
-				 Range(joker),
-				 Range(joker),
-				 Range(joker));
-     amp2abs(abs_vec_spt(i,Range(joker)),ext,pha);
+				      Range(joker),
+				      Range(joker),
+				      Range(joker),
+				      Range(joker));
+     
+     amp2abs(abs_vec_spt(i, Range(joker)),
+	     ext,
+	     pha);
    }	   
 }
+
--- src/scatproperties.cc
+++ src/scatproperties.cc
@@ -6,6 +6,7 @@
   \brief  This file has functions for calculating extinction and phase matrices 
   from amplitude matrix 
 */
+
 #include <iostream>
 #include "scatproperties.h"  
 extern const Numeric DEG2RAD;
@@ -26,7 +27,8 @@ extern const Numeric SPEED_OF_LIGHT;
 #define Re_S21ij amp_coeffs(i,j,4)
 #define Im_S21ij amp_coeffs(i,j,5) 
 #define Re_S22ij amp_coeffs(i,j,6) 
-#define Im_S22ij amp_coeffs(i,j,7) 
+#define Im_S22ij amp_coeffs(i,j,7)
+ 
 //! Calculates extinction cross-section matrix for a single particle type for
 // given combination of  angles. 
 /*! 
@@ -62,30 +64,34 @@ extern const Numeric SPEED_OF_LIGHT;
   and combination of angle
   \param freq Input : frequency
 */
-//FIXME; change all matrix ext_mat_spt to Tensor3 ext_mat_spt(l,j,k)
+
 void amp2ext(MatrixView ext,
 	     ConstVectorView amp_coeffs,
 	     const Numeric& freq)
 {
-  Index stokes_dim=ext.nrows();
+  Index stokes_dim = ext.nrows();
+
   if (stokes_dim > 4 || stokes_dim <1){
-    throw runtime_error("the dimension of stokes vector can be only 1,2,3, or 4");
+    throw runtime_error("the dimension of stokes vector"
+                         "can be only 1,2,3, or 4");
   }
-  assert (is_size(ext,stokes_dim,stokes_dim));
-  assert (is_size(amp_coeffs,8));
-  const Numeric wavelength=SPEED_OF_LIGHT/freq;
   
-  ext(0,0) = wavelength * (Im_S11+Im_S22) ; 
+  assert (is_size(ext, stokes_dim, stokes_dim));
+  assert (is_size(amp_coeffs, 8));
   
-  if(1 == stokes_dim){
+  const Numeric wavelength = SPEED_OF_LIGHT / freq;
+  
+  ext(0, 0) = wavelength * (Im_S11 + Im_S22) ; 
+  
+  if( 1 == stokes_dim ){
     return;
   }
 
   //only the first element is required if stokes_dim =1
 
-  ext(0,1) = wavelength*(Im_S11-Im_S22);
-  ext(1,0) = ext(0,1);
-  ext(1,1) = ext(0,0);
+  ext(0, 1) = wavelength * (Im_S11 - Im_S22);
+  ext(1, 0) = ext(0, 1);
+  ext(1, 1) = ext(0, 0);
   
   if(2 == stokes_dim){
     return;
@@ -93,11 +99,11 @@ void amp2ext(MatrixView ext,
 
   // if stokes_dim =2 only these 4 elements need be evaluated.
 
-  ext(0,2) = -wavelength*(Im_S12+Im_S21);
-  ext(1,2) = wavelength*(Im_S21-Im_S12);
-  ext(2,0) = ext(0,2);
-  ext(2,1) = -ext(1,2);
-  ext(2,2) = ext(0,0);
+  ext(0, 2) = - wavelength * (Im_S12 + Im_S21);
+  ext(1, 2) = wavelength * (Im_S21 - Im_S12);
+  ext(2, 0) = ext(0, 2);
+  ext(2, 1) = - ext(1, 2);
+  ext(2, 2) = ext(0, 0);
   
   if(3 == stokes_dim){
     return;
@@ -106,13 +112,13 @@ void amp2ext(MatrixView ext,
   // if stokes_dim =3 only these 9 elements need be evaluated.
 
 
-  ext(0,3) = wavelength*(Re_S21-Re_S12);
-  ext(1,3) = -wavelength*(Re_S12+Re_S21);
-  ext(2,3) = wavelength*(Re_S22-Re_S11);
-  ext(3,0) = ext(0,3);
-  ext(3,1) = -ext(1,3);
-  ext(3,2) = -ext(2,3);
-  ext(3,3) = ext(0,0);
+  ext(0, 3) = wavelength * (Re_S21 - Re_S12);
+  ext(1, 3) = - wavelength*(Re_S12 + Re_S21);
+  ext(2, 3) = wavelength * (Re_S22 - Re_S11);
+  ext(3, 0) = ext(0, 3);
+  ext(3, 1) = - ext(1, 3);
+  ext(3, 2) = - ext(2, 3);
+  ext(3, 3) = ext(0, 0);
   if(4 == stokes_dim){
     return;
   }
@@ -141,29 +147,31 @@ void amp2ext(MatrixView ext,
   \param amp_coeffs  Input : amplitude matrix
 */
 
-void amp2pha(Tensor4View phasemat,ConstTensor3View amp_coeffs)
+void amp2pha(Tensor4View phasemat,
+	     ConstTensor3View amp_coeffs)
 {
-  Index stokes_dim=phasemat.nrows();
+  Index stokes_dim = phasemat.nrows();
   if (stokes_dim > 4 || stokes_dim <1){
-    throw runtime_error("the dimension of stokes vector can be only 1,2,3, or 4");
+    throw runtime_error("the dimension of stokes vector" 
+			" can be only 1,2,3, or 4");
   }
   Index nza = phasemat.nbooks();
   Index naa = phasemat.npages();
-  assert (is_size(amp_coeffs,nza,naa,8));
-  assert (is_size(phasemat,nza,naa,stokes_dim,stokes_dim));
+  assert (is_size(amp_coeffs, nza, naa, 8));
+  assert (is_size(phasemat, nza, naa, stokes_dim, stokes_dim));
   
-  for (Index i=0;i<nza;++i)
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
-	  phasemat(i,j,0,0) = 0.5 * (Re_S11ij * Re_S11ij + 
-				     Im_S11ij * Im_S11ij + 
-				     Re_S12ij * Re_S12ij + 
-				     Im_S12ij * Im_S12ij +
-				     Re_S21ij * Re_S21ij + 
-				     Im_S21ij * Im_S21ij +
-				     Re_S22ij * Re_S22ij + 
-				     Im_S22ij * Im_S22ij);
+	  phasemat(i, j, 0, 0) = 0.5 * (Re_S11ij * Re_S11ij + 
+					Im_S11ij * Im_S11ij + 
+					Re_S12ij * Re_S12ij + 
+					Im_S12ij * Im_S12ij +
+					Re_S21ij * Re_S21ij + 
+					Im_S21ij * Im_S21ij +
+					Re_S22ij * Re_S22ij + 
+					Im_S22ij * Im_S22ij);
 	}
     }
   
@@ -173,36 +181,36 @@ void amp2pha(Tensor4View phasemat,ConstTensor3View amp_coeffs)
   
   //only the first element is required if stokes_dim =1
   
-  for (Index i=0;i<nza;++i)
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
-	  phasemat(i,j,0,1) = 0.5 * (Re_S11ij * Re_S11ij + 
-				     Im_S11ij * Im_S11ij - 
-				     Re_S12ij * Re_S12ij - 
-				     Im_S12ij * Im_S12ij +
-				     Re_S21ij * Re_S21ij +
-				     Im_S21ij * Im_S21ij -
-				     Re_S22ij * Re_S22ij -
-				     Im_S22ij * Im_S22ij);
+	  phasemat(i, j, 0, 1) = 0.5 * (Re_S11ij * Re_S11ij + 
+					Im_S11ij * Im_S11ij - 
+					Re_S12ij * Re_S12ij - 
+					Im_S12ij * Im_S12ij +
+					Re_S21ij * Re_S21ij +
+					Im_S21ij * Im_S21ij -
+					Re_S22ij * Re_S22ij -
+					Im_S22ij * Im_S22ij);
 
-	  phasemat(i,j,1,0) = 0.5 * (Re_S11ij * Re_S11ij + 
-				      Im_S11ij * Im_S11ij +
-				      Re_S12ij * Re_S12ij + 
-				      Im_S12ij * Im_S12ij - 
-				      Re_S21ij * Re_S21ij - 
-				      Im_S21ij * Im_S21ij - 
-				      Re_S22ij * Re_S22ij - 
-				      Im_S22ij * Im_S22ij);
+	  phasemat(i, j, 1, 0) = 0.5 * (Re_S11ij * Re_S11ij + 
+					Im_S11ij * Im_S11ij +
+					Re_S12ij * Re_S12ij + 
+					Im_S12ij * Im_S12ij - 
+					Re_S21ij * Re_S21ij - 
+					Im_S21ij * Im_S21ij - 
+					Re_S22ij * Re_S22ij - 
+					Im_S22ij * Im_S22ij);
 
-	   phasemat(i,j,1,1) = 0.5 * (Re_S11ij * Re_S11ij + 
-				     Im_S11ij * Im_S11ij - 
-				     Re_S12ij * Re_S12ij - 
-				     Im_S12ij * Im_S12ij -
-				     Re_S21ij * Re_S21ij - 
-				     Im_S21ij * Im_S21ij + 
-				     Re_S22ij + Re_S22ij + 
-				     Im_S22ij * Im_S22ij);
+	  phasemat(i, j, 1, 1) = 0.5 * (Re_S11ij * Re_S11ij + 
+					Im_S11ij * Im_S11ij - 
+					Re_S12ij * Re_S12ij - 
+					Im_S12ij * Im_S12ij -
+					Re_S21ij * Re_S21ij - 
+					Im_S21ij * Im_S21ij + 
+					Re_S22ij + Re_S22ij + 
+					Im_S22ij * Im_S22ij);
 	}
     }
 	  
@@ -212,34 +220,34 @@ void amp2pha(Tensor4View phasemat,ConstTensor3View amp_coeffs)
 
   // if stokes_dim =2 only these 4 elements need be evaluated.
 
-  for (Index i=0;i<nza;++i)
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
-	  phasemat(i,j,0,2) = - (Re_S11ij * Re_S12ij +
-				 Im_S11ij * Im_S12ij + 
-				 Re_S22ij * Re_S21ij + 
-				 Im_S22ij * Im_S21ij);
+	  phasemat(i, j, 0, 2) = - (Re_S11ij * Re_S12ij +
+				    Im_S11ij * Im_S12ij + 
+				    Re_S22ij * Re_S21ij + 
+				    Im_S22ij * Im_S21ij);
 	  
-	  phasemat(i,j,1,2) = - (Re_S11ij * Re_S12ij +
-				 Im_S11ij * Im_S12ij -
-				 Re_S22ij * Re_S21ij -
-				 Im_S22ij * Im_S21ij);
+	  phasemat(i, j, 1, 2) = - (Re_S11ij * Re_S12ij +
+				    Im_S11ij * Im_S12ij -
+				    Re_S22ij * Re_S21ij -
+				    Im_S22ij * Im_S21ij);
 	  
-	  phasemat(i,j,2,0) = - (Re_S11ij * Re_S21ij + 
-				 Im_S11ij * Im_S21ij + 
-				 Re_S22ij * Re_S12ij + 
-				 Im_S22ij * Im_S12ij);
-	  	  
-	  phasemat(i,j,2,1) = - (Re_S11ij * Re_S21ij + 
-				 Im_S11ij * Im_S21ij -
-				 Re_S22ij * Re_S12ij - 
-				 Im_S22ij * Im_S12ij);
+	  phasemat(i, j, 2, 0) = - (Re_S11ij * Re_S21ij + 
+				    Im_S11ij * Im_S21ij + 
+				    Re_S22ij * Re_S12ij + 
+				    Im_S22ij * Im_S12ij);
 	  
-	  phasemat(i,j,2,2) =  (Re_S11ij * Re_S22ij + 
-				Im_S11ij * Im_S22ij +
-				Re_S12ij * Re_S21ij + 
-				Im_S12ij * Im_S21ij);
+	  phasemat(i, j, 2, 1) = - (Re_S11ij * Re_S21ij + 
+				    Im_S11ij * Im_S21ij -
+				    Re_S22ij * Re_S12ij - 
+				    Im_S22ij * Im_S12ij);
+	  
+	  phasemat(i, j, 2, 2) =  (Re_S11ij * Re_S22ij + 
+				   Im_S11ij * Im_S22ij +
+				   Re_S12ij * Re_S21ij + 
+				   Im_S12ij * Im_S21ij);
 	}
     }
   
@@ -249,45 +257,44 @@ void amp2pha(Tensor4View phasemat,ConstTensor3View amp_coeffs)
 
    // if stokes_dim =3 only these 9 elements need be evaluated.
 
-  for (Index i=0;i<nza;++i)
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
-	  phasemat(i,j,0,3) = - (Im_S11ij * Re_S12ij -
-				 Re_S11ij * Im_S12ij -
-				 Im_S22ij * Re_S21ij + 
-				 Re_S22ij * Im_S21ij);
+	  phasemat(i, j, 0, 3) = - (Im_S11ij * Re_S12ij -
+				    Re_S11ij * Im_S12ij -
+				    Im_S22ij * Re_S21ij + 
+				    Re_S22ij * Im_S21ij);
 	  
-	  phasemat(i,j,1,3) = - (Im_S11ij * Re_S12ij - 
-				 Re_S11ij * Im_S12ij + 
-				 Im_S22ij * Re_S21ij - 
-				 Re_S22ij * Im_S21ij);
+	  phasemat(i, j, 1, 3) = - (Im_S11ij * Re_S12ij - 
+				    Re_S11ij * Im_S12ij + 
+				    Im_S22ij * Re_S21ij - 
+				    Re_S22ij * Im_S21ij);
 	  
+	  phasemat(i, j, 2, 3) = (Im_S11ij * Re_S22ij - 
+				  Re_S11ij * Im_S22ij +
+				  Im_S21ij * Re_S12ij - 
+				  Re_S21ij * Im_S12ij);
 	  
-	  phasemat(i,j,2,3) = (Im_S11ij * Re_S22ij - 
-			       Re_S11ij * Im_S22ij +
-			       Im_S21ij * Re_S12ij - 
-			       Re_S21ij * Im_S12ij);
+	  phasemat(i, j, 3, 0) = - (Im_S21ij * Re_S11ij - 
+				    Re_S21ij * Im_S11ij +
+				    Im_S22ij * Re_S12ij - 
+				    Re_S22ij * Im_S12ij);
 	  
-	  phasemat(i,j,3,0) = - (Im_S21ij * Re_S11ij - 
-				 Re_S21ij * Im_S11ij +
-				 Im_S22ij * Re_S12ij - 
-				 Re_S22ij * Im_S12ij);
+	  phasemat(i, j, 3, 1) = - (Im_S21ij * Re_S11ij - 
+				    Re_S21ij * Im_S11ij - 
+				    Im_S22ij * Re_S12ij + 
+				    Re_S22ij * Im_S12ij);
 	  
-	  phasemat(i,j,3,1) = - (Im_S21ij * Re_S11ij - 
-				 Re_S21ij * Im_S11ij - 
-				 Im_S22ij * Re_S12ij + 
-				 Re_S22ij * Im_S12ij);
+	  phasemat(i, j, 3, 2) = (Im_S22ij * Re_S11ij - 
+				  Re_S22ij * Im_S11ij - 
+				  Im_S12ij * Re_S21ij + 
+				  Re_S12ij * Im_S21ij); 
 	  
-	  phasemat(i,j,3,2) = (Im_S22ij * Re_S11ij - 
-			       Re_S22ij * Im_S11ij - 
-			       Im_S12ij * Re_S21ij + 
-			       Re_S12ij * Im_S21ij); 
-	  
-	  phasemat(i,j,3,3) = (Re_S22ij * Re_S11ij + 
-			       Im_S22ij * Im_S11ij - 
-			       Re_S12ij * Re_S21ij - 
-			       Im_S12ij * Im_S21ij); 
+	  phasemat(i, j, 3, 3) = (Re_S22ij * Re_S11ij + 
+				  Im_S22ij * Im_S11ij - 
+				  Re_S12ij * Re_S21ij - 
+				  Im_S12ij * Im_S21ij); 
 	}
     }
   
@@ -317,24 +324,28 @@ void amp2pha(Tensor4View phasemat,ConstTensor3View amp_coeffs)
   \param ext  Input : Extinction Matrix
   \param pha  Input : Phase matrix
 */
-void amp2abs(VectorView abs,ConstMatrixView ext,ConstTensor4View pha)
+
+void amp2abs(VectorView abs,
+	     ConstMatrixView ext,
+	     ConstTensor4View pha)
 {
-  Index stokes_dim=abs.nelem();
+  Index stokes_dim = abs.nelem();
   if (stokes_dim > 4 || stokes_dim <1){
-    throw runtime_error("the dimension of stokes vector can be only 1,2,3, or 4");
+    throw runtime_error("the dimension of stokes vector" 
+			"can be only 1,2,3, or 4");
   }
   Index nza = pha.nbooks();
   Index naa = pha.npages();
-  assert (is_size(pha,nza,naa,stokes_dim,stokes_dim));
-  assert (is_size(ext,stokes_dim,stokes_dim));
-  assert (is_size(abs,stokes_dim));
+  assert (is_size(pha, nza, naa, stokes_dim, stokes_dim));
+  assert (is_size(ext, stokes_dim, stokes_dim));
+  assert (is_size(abs, stokes_dim));
   Vector za_grid(nza);
   Vector aa_grid(naa);
   Numeric Z11_integrated, Z21_integrated, Z31_integrated, Z41_integrated ;
-  Matrix Z11_mat = pha (Range(joker),Range(joker),0,0);
-  for (Index i=0;i<nza;++i)
+  Matrix Z11_mat = pha (Range(joker), Range(joker), 0, 0);
+  for (Index i = 0;i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
 	  Z11_integrated = AngIntegrate_trapezoid(Z11_mat,
 						  za_grid,
@@ -342,7 +353,7 @@ void amp2abs(VectorView abs,ConstMatrixView ext,ConstTensor4View pha)
 	}
     }
 
-  abs[0] = ext(0,0)-Z11_integrated;
+  abs[0] = ext(0, 0) - Z11_integrated;
   
   if(1 == stokes_dim){
     return;
@@ -350,18 +361,18 @@ void amp2abs(VectorView abs,ConstMatrixView ext,ConstTensor4View pha)
    
   //only the first element is required if stokes_dim =1
   
-  Matrix Z21_mat = pha (Range(joker),Range(joker),1,0);
-  for (Index i=0;i<nza;++i)
+  Matrix Z21_mat = pha (Range(joker), Range(joker), 1, 0);
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
 	  Z21_integrated = AngIntegrate_trapezoid(Z21_mat,
-						    za_grid,
-						    aa_grid);
+						  za_grid,
+						  aa_grid);
 	}
     }
 
-  abs[1] = ext(1,1)-Z21_integrated;
+  abs[1] = ext(1, 1)- Z21_integrated;
   
   if(2 == stokes_dim){
     return;
@@ -369,18 +380,18 @@ void amp2abs(VectorView abs,ConstMatrixView ext,ConstTensor4View pha)
 
   // if stokes_dim =2 only these 2 elements need be evaluated.
 
-  Matrix Z31_mat = pha (Range(joker),Range(joker),2,0);
-  for (Index i=0;i<nza;++i)
+  Matrix Z31_mat = pha (Range(joker), Range(joker), 2, 0);
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
 	  Z31_integrated = AngIntegrate_trapezoid(Z31_mat,
-						    za_grid,
-						    aa_grid);
+						  za_grid,
+						  aa_grid);
 	}
     }
-
-  abs[2] = ext(2,2)-Z31_integrated;
+  
+  abs[2] = ext(2, 2) - Z31_integrated;
   
   if(3 == stokes_dim){
     return;
@@ -388,10 +399,10 @@ void amp2abs(VectorView abs,ConstMatrixView ext,ConstTensor4View pha)
 
    // if stokes_dim =3 only these 3 elements need be evaluated.
 
-  Matrix Z41_mat = pha (Range(joker),Range(joker),3,0);
-  for (Index i=0;i<nza;++i)
+  Matrix Z41_mat = pha (Range(joker), Range(joker), 3, 0);
+  for (Index i = 0; i < nza; ++i)
     {
-      for (Index j=0;j<naa;++j)
+      for (Index j = 0; j < naa; ++j)
 	{
 	  
 	  Z41_integrated = AngIntegrate_trapezoid(Z41_mat,
@@ -400,7 +411,7 @@ void amp2abs(VectorView abs,ConstMatrixView ext,ConstTensor4View pha)
 	}
     }
 
-  abs[3] = ext(3,3)-Z41_integrated;
+  abs[3] = ext(3, 3) - Z41_integrated;
 
   if(4 == stokes_dim){
     return;
@@ -427,11 +438,11 @@ Numeric AngIntegrate_trapezoid(ConstMatrixView Integrand,
   Index n = za_grid.nelem();
   Index m = aa_grid.nelem();
   Vector res1(n);
-  assert (is_size(Integrand,n,m));
+  assert (is_size(Integrand, n, m));
   for (Index i = 0; i < n ; ++i)
     {
       res1[i] = 0.0;
-      Numeric sintheta = sin(za_grid[i]*DEG2RAD);
+      Numeric sintheta = sin(za_grid[i] * DEG2RAD);
       
       for (Index j = 0; j < m - 1; ++j)
 	{
@@ -450,6 +461,7 @@ Numeric AngIntegrate_trapezoid(ConstMatrixView Integrand,
   cout<<res<<"\n";
   return res;
 }
+
 //! Extinction Coefficient Matrix for the particle 
 /*! 
   
@@ -459,21 +471,24 @@ Numeric AngIntegrate_trapezoid(ConstMatrixView Integrand,
   \param pnd Input : particle number density givs the local concentration for all particles.
 */
 //FIXME; change all matrix ext_mat_spt to Tensor3 ext_mat_spt(l,j,k)
-void ext_mat_partCalc(MatrixView ext_mat_part,MatrixView ext_mat_spt,VectorView pnd)
+void ext_mat_partCalc(MatrixView ext_mat_part,
+		      MatrixView ext_mat_spt,
+		      VectorView pnd)
 {
-  Index N_pt,j,k,l,N_i;
-  for (j=0;j< N_i;j++)
+  Index N_pt, j, k, l, N_i;
+  for (j = 0; j < N_i; j++)
     {
-      for (k=0;k<N_i;k++)
-	ext_mat_part(j,k)=0.0;// Initialisation
+      for (k = 0; k < N_i; k++)
+	ext_mat_part(j, k) = 0.0;// Initialisation
     }
-  for (l=0;l< N_pt;l++)
+  for (l = 0; l < N_pt; l++)
     {
-      for (j=0;j< N_i;j++)
+      for (j = 0; j < N_i; j++)
 	{
-	  for (k=0;k<N_i;k++)
-	    ext_mat_spt(j,k) *= pnd[l]; //multiplying with particle number density.
-	  ext_mat_part(j,k) += ext_mat_spt(j,k); // summing up for all particle types.
+	  for (k = 0; k < N_i; k++)
+	    ext_mat_spt(j, k) *= pnd[l]; //multiplying with particle number density.
+	  ext_mat_part(j, k) += ext_mat_spt(j,k); // summing up for all particle types.
 	}
-    }
-}
+    } 
+} 
+ 
