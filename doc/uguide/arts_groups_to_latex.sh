faa144b47bcab7469ef73e3af70e16811b3b693e
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-06-17 10:43:56 +0000

    Fixed doc header to use $ARTS and only use the first line from arts -v output.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1786 aaf1aab0-4228-0410-ad68-8dceda47f409

--- doc/uguide/arts_groups_to_latex.sh
+++ doc/uguide/arts_groups_to_latex.sh
@@ -2,14 +2,20 @@
 
 ARTS=$1
 
+if [ ! -x "$ARTS" ]
+then
+	echo "Error: Unable to execute arts" 1>&2
+	exit 1
+fi
+
 GROUP_LIST=`$ARTS -g | tail +5 | sed 's/^-//' | sed 's/^.-*.$//'`
 
 echo "%------------------------------------------------------------"
 echo "% This file has been generated automatically by the script"
 echo "% arts_groups_to_latex.sh, which calls arts with the -g flag."
 echo "%"
-echo "% Generation date: " `date`
-echo "% ARTS version:    " `arts -v`
+echo "% Generation date: `date`"
+echo "% ARTS version:    `$ARTS -v | head -n 1`"
 echo "%"
 echo "% DO NOT EDIT!"
 echo "%------------------------------------------------------------"
