403e76560c7e0ed081d16f6bbd6b3142a1881d5e
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-06-28 08:43:58 +0000

    Removed non-used headers from includes.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@1897 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/scatproperties.h
+++ src/scatproperties.h
@@ -3,19 +3,15 @@
   \author Sreerekha T.R. 
   \date   Mon May 13 11:32:27 2002
 */
+
 #ifndef scatproperties_h
 #define scatproperties_h
-#include <stdexcept>
-#include <math.h>
+
 #include "arts.h"
 #include "matpackI.h"
-#include "make_vector.h"
-#include "array.h"
-#include "logic.h"
-#include "auto_md.h"
-#include "check_input.h"
-#include "math_funcs.h"
-#include "mystring.h"
+#include "matpackIV.h"
+
+
 void amp2ext(MatrixView ext,
 	     ConstVectorView amp_coeffs,
 	     const Numeric& freq);
