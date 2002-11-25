cfd407ebc3ae6b2e69c3d81d5b53ebaa7cf79d3a
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2002-11-25 23:17:34 +0000

    Moved comment after include to separate line.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@2354 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/getopt.c
+++ src/getopt.c
@@ -43,7 +43,9 @@
 #endif
 
 #include <stdio.h>
-#include <string.h>		// SAB 08.03.2000
+
+/* SAB 08.03.2000 */
+#include <string.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
