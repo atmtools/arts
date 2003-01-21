8177779925060b7bb38cfa9f1578935eb89fd837
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-01-21 17:15:40 +0000

    Added. Contains class for writing binary data by using output operators <<.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@2634 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ src/bofstream.cc
@@ -0,0 +1,51 @@
+/* Copyright (C) 2003 Oliver Lemke <olemke@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+
+////////////////////////////////////////////////////////////////////////////
+//   File description
+////////////////////////////////////////////////////////////////////////////
+/*!
+  \file   bofstream.cc
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2003-01-21
+
+  \brief This file contains the class implementation of bofstream.
+
+*/
+
+#include <fstream>
+#include "matpackI.h"
+#include "bofstream.h"
+using namespace std;
+
+
+template<typename T>
+bofstream &operator<< (bofstream &bof, T n)
+{
+  bof.write ((char *)&n, sizeof (n));
+
+  return (bof);
+}
+
+/* Explicit instantiation of output operators */
+template
+bofstream &operator<< <double> (bofstream &bof, double n);
+
+template
+bofstream &operator<< <float> (bofstream &bof, float n);
+
---
+++ src/bofstream.h
@@ -0,0 +1,60 @@
+/* Copyright (C) 2003 Oliver Lemke <olemke@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+
+////////////////////////////////////////////////////////////////////////////
+//   File description
+////////////////////////////////////////////////////////////////////////////
+/*!
+  \file   bofstream.h
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2003-01-21
+
+  \brief This file contains the class declaration of bofstream.
+
+*/
+
+#ifndef BOFSTREAM_H_INCLUDED
+#define BOFSTREAM_H_INCLUDED
+
+#include <fstream>
+using namespace std;
+
+
+//! Binary output file stream class
+/*!
+  Handles writing to an output file stream in binary format. It makes it
+  possible to use the operator<< for binary output.
+*/
+class bofstream : public ofstream
+{
+public:
+  bofstream () : ofstream () { }
+
+  explicit
+  bofstream (const char* name,
+             ios::openmode mode = ios::out | ios::trunc | ios::binary)
+  : ofstream (name, mode) { }
+
+  ~bofstream () { }
+};
+
+
+template<typename T>
+bofstream &operator<< (bofstream &bof, T n);
+
+#endif
