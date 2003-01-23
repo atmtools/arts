dbac1209ec238352c41322c98c3b81e67e53b206
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-01-23 13:28:17 +0000

    Added class for binary input.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@2655 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ src/bifstream.cc
@@ -0,0 +1,50 @@
+/* Copyright (C) 2003 Oliver Lemke <olemke@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+
+////////////////////////////////////////////////////////////////////////////
+//   File description
+////////////////////////////////////////////////////////////////////////////
+/*!
+  \file   bifstream.cc
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2003-01-23
+
+  \brief This file contains the class implementation of bifstream.
+
+*/
+
+#include <fstream>
+#include "bifstream.h"
+using namespace std;
+
+
+template<typename T>
+bifstream &operator>> (bifstream &bif, T &n)
+{
+  bif.read ((char *)&n, sizeof (n));
+
+  return (bif);
+}
+
+/* Explicit instantiation of output operators */
+template
+bifstream &operator>> <double> (bifstream &bif, double &n);
+
+template
+bifstream &operator>> <float> (bifstream &bif, float &n);
+
---
+++ src/bifstream.h
@@ -0,0 +1,59 @@
+/* Copyright (C) 2003 Oliver Lemke <olemke@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+
+////////////////////////////////////////////////////////////////////////////
+//   File description
+////////////////////////////////////////////////////////////////////////////
+/*!
+  \file   bifstream.h
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2003-01-23
+
+  \brief This file contains the class declaration of bifstream.
+
+*/
+
+#ifndef BIFSTREAM_H_INCLUDED
+#define BIFSTREAM_H_INCLUDED
+
+#include <fstream>
+using namespace std;
+
+
+//! Binary output file stream class
+/*!
+  Handles writing to an output file stream in binary format. It makes it
+  possible to use the operator<< for binary output.
+*/
+class bifstream : public ifstream
+{
+public:
+  bifstream () : ifstream () { }
+
+  explicit
+  bifstream (const char* name,
+             ios::openmode mode = ios::in | ios::trunc | ios::binary)
+  : ifstream (name, mode) { }
+
+};
+
+
+template<typename T>
+bifstream &operator>> (bifstream &bif, T &n);
+
+#endif
