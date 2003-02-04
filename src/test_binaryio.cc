0672c2a537556dfdbc43aeb17a3f42ce21c20dfa
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-02-04 10:41:13 +0000

    Updated.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@2737 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/test_binaryio.cc
+++ src/test_binaryio.cc
@@ -18,7 +18,7 @@ main (int argc, char *argv[])
         for (Index l = 0; l < 4; l++)
           v(i, j, k, l) = double (i*4*4*4+j*4*4+k*4+l);
 
-  xml_write_to_file ("outfile.xml", v, FTYPE_BINARY);
+  xml_write_to_file ("outfile.xml", v, FILE_TYPE_BINARY);
 
   // Read binary file
   Tensor4 w;
