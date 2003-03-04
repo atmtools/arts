dd49ac1d4204cb4a9c9cbdad476a28832ef13f62
patrick <patrick@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-03-04 22:11:10 +0000

    2003-03-04  Patrick Eriksson  <Patrick.Eriksson@rss.chalmers.se>
    	* arts-1-1-468
    
    	* doc/uguide/Figs/fm_definitions/mkfigs_atm_dims.m: Added 3D figure.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@2788 aaf1aab0-4228-0410-ad68-8dceda47f409

--- doc/uguide/Figs/fm_definitions/mkfigs_atm_dims.m
+++ doc/uguide/Figs/fm_definitions/mkfigs_atm_dims.m
@@ -13,14 +13,13 @@ cb_lims  = [6 9];
 %
 figure(1)
 clf
-fit_to_paper( 'a5l' );
 [h,hs] = arts_plot_atmgrids( dim, lat_grid, lon_grid, z_field,...
                                                r_geoid, z_ground, cb_lims, 1 );
 axes_frame( gca, 'off' )
 axis tight
 hl = legend( h, hs );
 disp('Adjust the legend');
-%pause;
+pause;
 
 
 
@@ -31,28 +30,58 @@ nlat     = 16;
 dim      = 2;
 lat_grid = linspace(45,135,nlat);
 lon_grid = [];
-z_field  = (1:18)'*ones(1,nlat);% - ones(18,1)*linspace(0,0.8,nlat);
+z_field  = (1:18)'*ones(1,nlat) - ones(18,1)*linspace(0,0.8,nlat);
 r_geoid  = 50 * ones(nlat,1);
-z_ground = 1.5 + 2*randn(nlat,1)
+z_ground = 1.5 + 0.5*randn(nlat,1);
 cb_lims  = [6 9 8 13];
 %
 figure(2)
 clf
-fit_to_paper( 'a5l' );
 [h,hs] = arts_plot_atmgrids( dim, lat_grid, lon_grid, z_field,...
                                                r_geoid, z_ground, cb_lims, 1 );
 axes_frame( gca, 'off' )
 axis tight
-hl = legend( h, hs );
-disp('Adjust the legend');
+%hl = legend( h, hs );
+%disp('Adjust the legend');
 %pause;
 
-return
 
-if answer_is_yes('Print figures')
+
+%--- 3D
+%
+nlat     = 5;
+nlon     = 5;
+%
+dim      = 3;
+lat_grid = linspace(-30,30,nlat);
+lon_grid = linspace(-30,30,nlon);
+z_field  = repmat( (1:5:41)'*ones(1,nlat), [1 1 nlon] );
+r_geoid  = 50 * ones(nlat,nlon);
+z_ground = 1.5 + 0.5*randn(nlat,nlon);
+cb_lims  = [];
+%
+figure(3)
+clf
+[h,hs] = arts_plot_atmgrids( dim, lat_grid, lon_grid, z_field,...
+                                               r_geoid, z_ground, cb_lims, 0 );
+axes_frame( gca, 'off' )
+axis tight
+%hl = legend( h, hs );
+%disp('Adjust the legend');
+%pause;
+
+
+
+if yes_or_no('Print figures')
+  print atm_dim_3d.eps -depsc
+  ! epstopdf atm_dim_3d.eps
+  ! rm  atm_dim_3d.eps
+  figure(2)
   print atm_dim_2d.eps -depsc
   ! epstopdf atm_dim_2d.eps
+  ! rm  atm_dim_2d.eps
   figure(1)
   print atm_dim_1d.eps -depsc
   ! epstopdf atm_dim_1d.eps
+  ! rm  atm_dim_1d.eps
 end
