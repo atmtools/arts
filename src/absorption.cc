f54f6a484db3d4a98641d08d78c8377128bebcec
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-05-05 11:32:36 +0000

    Try to work around a bug in older compilers.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts/branches/ARTS-1-0@2994 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/absorption.cc
+++ src/absorption.cc
@@ -1127,7 +1127,7 @@ bool LineRecord::ReadFromJplStream(istream& is)
     extract(tag,line,7);
 
     // make sure tag is not negative (damned jpl cat):
-    tag = abs(tag);
+    tag = abs(INDEX(tag));
   }
 
   // ok, now for the cool index map:
