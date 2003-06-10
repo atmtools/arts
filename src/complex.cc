0e1e39eb65ff181354725fa3c30f950a81e133a1
(no author) <(no author)@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-06-10 13:59:59 +0000

    This commit was manufactured by cvs2svn to create branch 'ARTS-1-0'.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts/branches/ARTS-1-0@3134 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ src/complex.cc
@@ -0,0 +1,70 @@
+/* Copyright (C) 2002 Stefan Buehler <sbuehler@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+/*!
+  \file   complex.cc
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2002-12-19
+  
+  \brief  A class implementing complex numbers for ARTS.
+*/
+
+#include "complex.h"
+
+complex<float> operator+ (const double &d, const complex<float> &c)
+{
+  return (float(d) + c);
+}
+
+complex<float> operator* (const double &d, const complex<float> &c)
+{
+  return (float(d) * c);
+}
+
+
+complex<float> operator+ (const complex<float> &c, const double &d)
+{
+  return (c + float(d));
+}
+
+complex<float> operator* (const complex<float> &c, const double &d)
+{
+  return (c * float(d));
+}
+
+
+
+complex<double> operator+ (const float &f, const complex<double> &c)
+{
+  return (double(f) + c);
+}
+
+complex<double> operator* (const float &f, const complex<double> &c)
+{
+  return (double(f) * c);
+}
+
+complex<double> operator+ (const complex<double> &c, const float &f)
+{
+  return (c + double(f));
+}
+
+complex<double> operator* (const complex<double> &c, const float &f)
+{
+  return (c * double(f));
+}
+
---
+++ src/complex.h
@@ -0,0 +1,47 @@
+/* Copyright (C) 2002 Stefan Buehler <sbuehler@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+/*!
+  \file   complex.h
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2002-12-19
+  
+  \brief  A class implementing complex numbers for ARTS.
+*/
+
+#ifndef complex_h
+#define complex_h
+
+#include <complex>
+#include "arts.h"
+
+typedef complex<Numeric> Complex;
+
+complex<float> operator+ (const double &d, const complex<float> &c);
+complex<float> operator* (const double &d, const complex<float> &c);
+
+complex<float> operator+ (const complex<float> &c, const double &d);
+complex<float> operator* (const complex<float> &c, const double &d);
+
+complex<double> operator+ (const float &f, const complex<double> &c);
+complex<double> operator* (const float &f, const complex<double> &c);
+
+complex<double> operator+ (const complex<double> &c, const float &d);
+complex<double> operator* (const complex<double> &c, const float &d);
+
+#endif
+
