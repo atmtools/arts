0f4ec344b3289585f6bb5af92f1b46c3df514942
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-06-11 12:38:09 +0000

    Added. Contains the reading/writing routines for compound types like structures. Compound types consist of basic types.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@3157 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ src/xml_io_compound_types.cc
@@ -0,0 +1,503 @@
+/* Copyright (C) 2003 Oliver Lemke <olemke@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+
+////////////////////////////////////////////////////////////////////////////
+//   File description
+////////////////////////////////////////////////////////////////////////////
+/*!
+  \file   xml_io_compound_types.cc
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2003-06-11
+
+  \brief This file contains basic functions to handle XML data files.
+
+*/
+
+#include "arts.h"
+#include "xml_io_private.h"
+#include "xml_io_basic_types.h"
+#include "xml_io_compound_types.h"
+#include "xml_io_array_types.h"
+#include "matpackI.h"
+#include "matpackII.h"
+#include "matpackIII.h"
+#include "matpackIV.h"
+#include "matpackV.h"
+#include "matpackVI.h"
+#include "matpackVII.h"
+#include "bofstream.h"
+#include "bifstream.h"
+
+
+////////////////////////////////////////////////////////////////////////////
+//   Overloaded functions for reading/writing data from/to XML stream
+////////////////////////////////////////////////////////////////////////////
+
+
+//=== GasAbsLookup ===========================================================
+
+//! Reads GasAbsLookup from XML input stream
+/*!
+  Checks whether the next tag in input stream is <GasAbsLookup>
+  and if so, verifies the order of the components and writes them
+  to the 'gal' parameter.
+
+  \param is_xml  XML Input stream
+  \param gal GasAbsLookup return value
+  \param pbifs   Pointer to binary input stream. NULL in case of ASCII file.
+*/
+void
+xml_read_from_stream (istream& is_xml,
+                      GasAbsLookup& gal,
+                      bifstream *pbifs)
+{
+  ArtsXMLTag tag;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("GasAbsLookup");
+
+  xml_read_from_stream (is_xml, gal.species, pbifs);
+  xml_read_from_stream (is_xml, gal.nonlinear_species, pbifs);
+  xml_read_from_stream (is_xml, gal.f_grid, pbifs);
+  xml_read_from_stream (is_xml, gal.p_grid, pbifs);
+  xml_read_from_stream (is_xml, gal.vmrs_ref, pbifs);
+  xml_read_from_stream (is_xml, gal.t_ref, pbifs);
+  xml_read_from_stream (is_xml, gal.t_pert, pbifs);
+  xml_read_from_stream (is_xml, gal.nls_pert, pbifs);
+  xml_read_from_stream (is_xml, gal.xsec, pbifs);
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("/GasAbsLookup");
+}
+
+
+//! Writes GasAbsLookup to XML output stream
+/*!
+  \param os_xml  XML Output stream
+  \param gas GasAbsLookup
+  \param pbofs    Pointer to binary file stream. NULL for ASCII output.
+*/
+void
+xml_write_to_stream (ostream& os_xml,
+                     const GasAbsLookup& gal,
+                     bofstream *pbofs)
+{
+  ArtsXMLTag open_tag;
+  ArtsXMLTag close_tag;
+
+  open_tag.set_name ("GasAbsLookup");
+  open_tag.write_to_stream (os_xml);
+
+  xml_write_to_stream (os_xml, gal.species, pbofs);
+  xml_write_to_stream (os_xml, gal.nonlinear_species, pbofs);
+  xml_write_to_stream (os_xml, gal.f_grid, pbofs);
+  xml_write_to_stream (os_xml, gal.p_grid, pbofs);
+  xml_write_to_stream (os_xml, gal.vmrs_ref, pbofs);
+  xml_write_to_stream (os_xml, gal.t_ref, pbofs);
+  xml_write_to_stream (os_xml, gal.t_pert, pbofs);
+  xml_write_to_stream (os_xml, gal.nls_pert, pbofs);
+  xml_write_to_stream (os_xml, gal.xsec, pbofs);
+
+  close_tag.set_name ("/GasAbsLookup");
+  close_tag.write_to_stream (os_xml);
+  os_xml << '\n';
+}
+
+
+//=== GridPos =====================================================
+
+//! Reads GridPos from XML input stream
+/*!
+  Checks whether the next tag in input stream is <GridPos>
+  and if so, write the values to 'gpos' parameter.
+
+  \param is_xml  XML Input stream
+  \param gpos GridPos return value
+  \param pbifs   Pointer to binary input stream. NULL in case of ASCII file.
+*/
+void
+xml_read_from_stream (istream& is_xml,
+                      GridPos& gpos,
+                      bifstream *pbifs)
+{
+  ArtsXMLTag tag;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("GridPos");
+
+  xml_read_from_stream (is_xml, gpos.idx, pbifs);
+  xml_read_from_stream (is_xml, gpos.fd[0], pbifs);
+  xml_read_from_stream (is_xml, gpos.fd[1], pbifs);
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("/GridPos");
+}
+
+//! Writes GridPos to XML output stream
+/*!
+  \param os_xml  XML Output stream
+  \param gpos  GridPos
+  \param pbofs    Pointer to binary file stream. NULL for ASCII output.
+*/
+void
+xml_write_to_stream (ostream& os_xml,
+                     const GridPos& gpos,
+                     bofstream *pbofs)
+{
+  ArtsXMLTag open_tag;
+  ArtsXMLTag close_tag;
+
+  open_tag.set_name ("GridPos");
+  open_tag.write_to_stream (os_xml);
+
+  xml_write_to_stream (os_xml, gpos.idx, pbofs);
+  xml_write_to_stream (os_xml, gpos.fd[0], pbofs);
+  xml_write_to_stream (os_xml, gpos.fd[1], pbofs);
+
+  close_tag.set_name ("/GridPos");
+  close_tag.write_to_stream (os_xml);
+  os_xml << '\n';
+}
+
+
+//=== Ppath =====================================================
+
+//! Reads Ppath from XML input stream
+/*!
+  Checks whether the next tag in input stream is <Ppath>
+  and if so, write the values to 'ppath' parameter.
+
+  \param is_xml  XML Input stream
+  \param ppath Ppath return value
+  \param pbifs   Pointer to binary input stream. NULL in case of ASCII file.
+*/
+void
+xml_read_from_stream (istream& is_xml,
+                      Ppath& ppath,
+                      bifstream *pbifs)
+{
+  ArtsXMLTag tag;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("Ppath");
+
+  xml_read_from_stream (is_xml, ppath.dim, pbifs);
+  xml_read_from_stream (is_xml, ppath.np, pbifs);
+  xml_read_from_stream (is_xml, ppath.refraction, pbifs);
+  xml_read_from_stream (is_xml, ppath.method, pbifs);
+  xml_read_from_stream (is_xml, ppath.constant, pbifs);
+  xml_read_from_stream (is_xml, ppath.pos, pbifs);
+  xml_read_from_stream (is_xml, ppath.z, pbifs);
+  xml_read_from_stream (is_xml, ppath.l_step, pbifs);
+  xml_read_from_stream (is_xml, ppath.gp_p, pbifs);
+  xml_read_from_stream (is_xml, ppath.gp_lat, pbifs);
+  xml_read_from_stream (is_xml, ppath.gp_lon, pbifs);
+  xml_read_from_stream (is_xml, ppath.los, pbifs);
+  xml_read_from_stream (is_xml, ppath.background, pbifs);
+  xml_read_from_stream (is_xml, ppath.tan_pos, pbifs);
+  xml_read_from_stream (is_xml, ppath.geom_tan_pos, pbifs);
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("/Ppath");
+}
+
+//! Writes Ppath to XML output stream
+/*!
+  \param os_xml  XML Output stream
+  \param ppath Ppath
+  \param pbofs    Pointer to binary file stream. NULL for ASCII output.
+*/
+void
+xml_write_to_stream (ostream& os_xml,
+                     const Ppath& ppath,
+                     bofstream *pbofs)
+{
+  ArtsXMLTag open_tag;
+  ArtsXMLTag close_tag;
+
+  open_tag.set_name ("Ppath");
+  open_tag.write_to_stream (os_xml);
+
+  xml_write_to_stream (os_xml, ppath.dim, pbofs);
+  xml_write_to_stream (os_xml, ppath.np, pbofs);
+  xml_write_to_stream (os_xml, ppath.refraction, pbofs);
+  xml_write_to_stream (os_xml, ppath.method, pbofs);
+  xml_write_to_stream (os_xml, ppath.constant, pbofs);
+  xml_write_to_stream (os_xml, ppath.pos, pbofs);
+  xml_write_to_stream (os_xml, ppath.z, pbofs);
+  xml_write_to_stream (os_xml, ppath.l_step, pbofs);
+  xml_write_to_stream (os_xml, ppath.gp_p, pbofs);
+  xml_write_to_stream (os_xml, ppath.gp_lat, pbofs);
+  xml_write_to_stream (os_xml, ppath.gp_lon, pbofs);
+  xml_write_to_stream (os_xml, ppath.los, pbofs);
+  xml_write_to_stream (os_xml, ppath.background, pbofs);
+  xml_write_to_stream (os_xml, ppath.tan_pos, pbofs);
+  xml_write_to_stream (os_xml, ppath.geom_tan_pos, pbofs);
+
+  close_tag.set_name ("/Ppath");
+  close_tag.write_to_stream (os_xml);
+  os_xml << '\n';
+}
+
+
+//=== SingleScatteringData ======================================
+
+//! Reads SingleScatteringData from XML input stream
+/*!
+  Checks whether the next tag in input stream is <SingleScatteringData>
+  and if so, write the values to 'ssdata' parameter.
+
+  \param is_xml  XML Input stream
+  \param ssdata  SingleScatteringData return value
+  \param pbifs   Pointer to binary input stream. NULL in case of ASCII file.
+*/
+void
+xml_read_from_stream (istream& is_xml,
+                      SingleScatteringData &ssdata,
+                      bifstream *pbifs)
+{
+  ArtsXMLTag tag;
+  Index ptype;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("SingleScatteringData");
+
+  xml_read_from_stream (is_xml, ptype, pbifs);
+  ssdata.ptype = PType (ptype);
+  xml_read_from_stream (is_xml, ssdata.description, pbifs);
+  xml_read_from_stream (is_xml, ssdata.f_grid, pbifs);
+  xml_read_from_stream (is_xml, ssdata.T_grid, pbifs);
+  xml_read_from_stream (is_xml, ssdata.za_grid, pbifs);
+  xml_read_from_stream (is_xml, ssdata.aa_grid, pbifs);
+
+  xml_read_from_stream (is_xml, ssdata.pha_mat_data, pbifs);
+  if (ssdata.pha_mat_data.nvitrines () != ssdata.f_grid.nelem ())
+    {
+      throw runtime_error("Number of frequencies in f_grid and pha_mat_data "
+                          "not matching!!!");
+    }
+
+  xml_read_from_stream (is_xml, ssdata.ext_mat_data, pbifs);
+  xml_read_from_stream (is_xml, ssdata.abs_vec_data, pbifs);
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("/SingleScatteringData");
+}
+
+
+//! Writes SingleScatteringData to XML output stream
+/*!
+  \param os_xml  XML Output stream
+  \param ssdata  SingleScatteringData
+  \param pbofs   Pointer to binary file stream. NULL for ASCII output.
+*/
+void
+xml_write_to_stream (ostream& os_xml,
+                     const SingleScatteringData &ssdata,
+                     bofstream *pbofs)
+{
+  ArtsXMLTag open_tag;
+  ArtsXMLTag close_tag;
+
+  open_tag.set_name ("SingleScatteringData");
+  open_tag.write_to_stream (os_xml);
+
+  xml_write_to_stream (os_xml, Index (ssdata.ptype), pbofs);
+  xml_write_to_stream (os_xml, ssdata.description, pbofs);
+  xml_write_to_stream (os_xml, ssdata.f_grid, pbofs);
+  xml_write_to_stream (os_xml, ssdata.T_grid, pbofs);
+  xml_write_to_stream (os_xml, ssdata.za_grid, pbofs);
+  xml_write_to_stream (os_xml, ssdata.aa_grid, pbofs);
+  xml_write_to_stream (os_xml, ssdata.pha_mat_data, pbofs);
+  xml_write_to_stream (os_xml, ssdata.ext_mat_data, pbofs);
+  xml_write_to_stream (os_xml, ssdata.abs_vec_data, pbofs);
+
+  close_tag.set_name ("/SingleScatteringData");
+  close_tag.write_to_stream (os_xml);
+  os_xml << '\n';
+}
+
+
+//=== SpeciesRecord ================================================
+
+//! Reads SpeciesRecord from XML input stream
+/*!
+  Checks whether the next tag in input stream is <SpeciesRecord>
+  and if so, write the values to 'srecord' parameter.
+
+  \param is_xml   XML Input stream
+  \param srecord  SpeciesRecord return value
+  \param pbifs    Pointer to binary input stream. NULL in case of ASCII file.
+*/
+void
+xml_read_from_stream (istream& is_xml,
+                      SpeciesRecord& srecord,
+                      bifstream *pbifs)
+{
+  ArtsXMLTag tag;
+  String sname;
+  Index degfr;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("SpeciesRecord");
+
+  xml_read_from_stream (is_xml, sname, pbifs);
+  xml_read_from_stream (is_xml, degfr, pbifs);
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("/SpeciesRecord");
+}
+
+
+//! Writes SpeciesRecord to XML output stream
+/*!
+  \param os_xml   XML Output stream
+  \param srecord  SpeciesRecord
+  \param pbofs    Pointer to binary file stream. NULL for ASCII output.
+*/
+void
+xml_write_to_stream (ostream& os_xml,
+                     const SpeciesRecord& srecord,
+                     bofstream *pbofs)
+{
+  ArtsXMLTag open_tag;
+  ArtsXMLTag close_tag;
+
+  open_tag.set_name ("SpeciesRecord");
+  open_tag.write_to_stream (os_xml);
+
+  xml_write_to_stream (os_xml, srecord.Name (), pbofs);
+  xml_write_to_stream (os_xml, srecord.Degfr (), pbofs);
+
+  close_tag.set_name ("/SpeciesRecord");
+  close_tag.write_to_stream (os_xml);
+  os_xml << '\n';
+}
+
+
+//=== SpeciesTag ================================================
+
+//! Reads SpeciesTag from XML input stream
+/*!
+  Checks whether the next tag in input stream is <SpeciesTag>
+  and if so, write the values to 'stag' parameter.
+
+  \param is_xml  XML Input stream
+  \param stag SpeciesTag return value
+  \param pbifs   Pointer to binary input stream. NULL in case of ASCII file.
+*/
+void
+xml_read_from_stream (istream& is_xml,
+                      SpeciesTag& stag,
+                      bifstream * /* pbifs */)
+{
+  ArtsXMLTag tag;
+  stringbuf  strbuf;
+  char dummy;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("SpeciesTag");
+
+  // Skip whitespaces
+  bool string_starts_with_quotes = true;
+  do
+    {
+      is_xml >> dummy;
+      switch (dummy)
+        {
+      case ' ':
+      case '\"':
+      case '\n':
+      case '\r':
+      case '\t':
+        break;
+      default:
+        string_starts_with_quotes = false;
+        }
+    } while (is_xml.good () && dummy != '"' && string_starts_with_quotes);
+
+  // Throw exception if first char after whitespaces is not a quote
+  if (!string_starts_with_quotes)
+    {
+      xml_parse_error ("SpeciesTag must begin with \"");
+    }
+
+  is_xml.get (strbuf, '"');
+  if (is_xml.fail ())
+    {
+      xml_parse_error ("SpeciesTag must end with \"");
+    }
+
+  stag = SpeciesTag (strbuf.str ());
+
+  // Ignore quote
+  is_xml >> dummy;
+
+  tag.read_from_stream (is_xml);
+  tag.check_name ("/SpeciesTag");
+}
+
+
+//! Writes SpeciesTag to XML output stream
+/*!
+  \param os_xml  XML Output stream
+  \param stag SpeciesTag
+  \param pbofs    Pointer to binary file stream. NULL for ASCII output.
+*/
+void
+xml_write_to_stream (ostream& os_xml,
+                     const SpeciesTag& stag,
+                     bofstream * /* pbofs */)
+{
+  ArtsXMLTag open_tag;
+  ArtsXMLTag close_tag;
+
+  open_tag.set_name ("SpeciesTag");
+  open_tag.write_to_stream (os_xml);
+
+  os_xml << '\"' << stag.Name () << '\"';
+
+  close_tag.set_name ("/SpeciesTag");
+  close_tag.write_to_stream (os_xml);
+  os_xml << '\n';
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+//   Dummy funtion for groups for which
+//   IO function have not yet been implemented
+////////////////////////////////////////////////////////////////////////////
+
+// FIXME: These should be implemented, sooner or later...
+
+void
+xml_read_from_stream (istream&,
+                      Agenda&,
+                      bifstream * /* pbifs */)
+{
+  throw runtime_error("Method not implemented!");
+}
+
+void
+xml_write_to_stream (ostream&,
+                     const Agenda&,
+                     bofstream * /* pbofs */)
+{
+  throw runtime_error("Method not implemented!");
+}
+
---
+++ src/xml_io_compound_types.h
@@ -0,0 +1,93 @@
+/* Copyright (C) 2003 Oliver Lemke <olemke@uni-bremen.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+
+////////////////////////////////////////////////////////////////////////////
+//   File description
+////////////////////////////////////////////////////////////////////////////
+/*!
+  \file   xml_io_private.h
+  \author Oliver Lemke <olemke@uni-bremen.de>
+  \date   2003-06-11
+
+  \brief This file contains private function declarations and
+         template instantiation to handle XML data files.
+
+*/
+
+#ifndef xml_io_compound_h
+#define xml_io_compound_h
+
+#include "messages.h"
+#include "ppath.h"
+#include "agenda_class.h"
+#include "absorption.h"
+#include "gas_abs_lookup.h"
+#include "optproperties.h"
+#include "bifstream.h"
+#include "bofstream.h"
+
+
+////////////////////////////////////////////////////////////////////////////
+//   Overloaded reading/writing routines for XML streams
+////////////////////////////////////////////////////////////////////////////
+
+void
+xml_read_from_stream (istream&, Agenda&, bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const Agenda&, bofstream * = NULL);
+
+void
+xml_read_from_stream (istream&, GasAbsLookup&, bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const GasAbsLookup&, bofstream * = NULL);
+
+void
+xml_read_from_stream (istream&, GridPos&, bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const GridPos&, bofstream * = NULL);
+
+void
+xml_read_from_stream (istream&, Ppath&, bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const Ppath&, bofstream * = NULL);
+
+void
+xml_read_from_stream (istream&, SingleScatteringData&,
+                      bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const SingleScatteringData&,
+                     bofstream * = NULL);
+
+void
+xml_read_from_stream (istream&, SpeciesRecord&, bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const SpeciesRecord&, bofstream * = NULL);
+
+void
+xml_read_from_stream (istream&, SpeciesTag&, bifstream * = NULL);
+
+void
+xml_write_to_stream (ostream&, const SpeciesTag&, bofstream * = NULL);
+
+#endif  /* xml_io_compound_types_h */
