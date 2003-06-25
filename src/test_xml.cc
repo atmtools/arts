d0d25b3078d0b7f3b43969097a6a19083e0f9f4a
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-06-25 08:50:08 +0000

    Updated.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@3228 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/test_xml.cc
+++ src/test_xml.cc
@@ -9,16 +9,21 @@ using namespace std;
 extern Array<SpeciesRecord> species_data;
 
 int
-main (int /* argc */, char * /* argv */ [])
+main (int /*argc*/, char * /*argv*/ [])
 {
   define_species_data ();
   try
     {
-      xml_write_to_file ("sdata.xml", species_data);
+      xml_write_to_file ("sdata1.xml", species_data);
       cout << "Wrote species_data: " << endl;
 
-      //xml_read_from_file ("sparse.xml", b);
-      //cout << "Read: " << b << endl;
+      species_data.clear ();
+
+      xml_read_from_file ("sdata1.xml", species_data);
+      cout << "Read species_data: " << endl;
+
+      xml_write_to_file ("sdata2.xml", species_data);
+      cout << "Wrote species_data: " << endl;
     }
   catch (runtime_error e)
     {
