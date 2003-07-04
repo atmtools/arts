0bfaee5dd305fff1422d9b4d97f3c413882972d1
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-07-04 11:13:10 +0000

    Implemented broadcasting and sending routines for Matrix. Only a first test version for performance testing.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@3284 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/test_mpi.cc
+++ src/test_mpi.cc
@@ -19,10 +19,11 @@
   \file   test_mpi.cc
   \author <olemke@uni-bremen.de>
   \date   2003-07-02
-  
+
   \brief  Test the mpi interface.
 */
 
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
@@ -30,6 +31,17 @@ using namespace std;
 #include "arts_mpi.h"
 #include "matpackI.h"
 
+//#define NCOLS 10 * 10 * 20 * 100
+#define NCOLS 2000
+#define NROWS 100
+
+#ifdef HAVE_MPI
+void
+mpi_broadcast (Matrix &m, Index source);
+
+void
+mpi_send (Matrix &m, Index source, Index dest);
+#endif
 
 #ifndef HAVE_MPI
 int main()
@@ -39,9 +51,14 @@ int main()
 
   return 0;
 }
+
 #else
-int main(int argc, char *argv[])
+
+int
+main(int argc, char *argv[])
 {
+  double starttime;
+
   mpi_manager.startup (argc, argv);
 
   if (mpi_manager.get_nprocs () < 2)
@@ -51,26 +68,108 @@ int main(int argc, char *argv[])
       return 1;
     }
 
-  cout << "nProcs: " << mpi_manager.get_nprocs () << endl;
+  system ("hostname");
 
-  // Create data on master and send it to all nodes
+  Matrix a;
+
+  // Initialize data on master
   if (mpi_manager.get_rank () == 0)
     {
-      const Index n = 100;
-      Matrix a (n, n);
+      const Index c = NCOLS, r = NROWS;
+
+      cout << "nProcs: " << mpi_manager.get_nprocs () << endl;
+
+      a.resize (r, c);
 
       Numeric k = 1.;
-      for (Index i = 0; i < n; i++)
-        for (Index j = 0; j < n; j++, k++)
+      for (Index i = 0; i < r; i++)
+        for (Index j = 0; j < c; j++, k++)
           a (i, j) = k;
 
-      // TODO: Broadcast data
     }
-  else
-    {
-      // TODO: Receive data
-    }
+
+  if (mpi_manager.get_rank () == 0)
+    starttime = MPI::Wtime ();
+
+  // Send matrix from master (0) to first client (1)
+  mpi_send (a, 0, 1);
+
+  // Output timing info on master
+  if (mpi_manager.get_rank () == 0)
+    cout << "Time: " << MPI::Wtime () - starttime << " secs" << endl;
+
+  cout << "Rank: " << mpi_manager.get_rank ();
+  cout << " - nCols: " << a.ncols ();
+  cout << " - nRows: " << a.nrows ();
+  cout << " - a (0, 50) = " << a (0, 50) << endl;
+
+  if (mpi_manager.get_rank () == 0) starttime = MPI::Wtime ();
+
+  // Broadcast matrix to all clients
+  mpi_broadcast (a, 0);
+
+  if (mpi_manager.get_rank () == 0)
+    cout << "Time: " << MPI::Wtime () - starttime << " secs" << endl;
+
+  cout << "Rank: " << mpi_manager.get_rank ();
+  cout << " - nCols: " << a.ncols ();
+  cout << " - nRows: " << a.nrows ();
+  cout << " - a (0, 50) = " << a (0, 50) << endl;
 
   return 0;
 }
+
+
+void
+mpi_broadcast (Matrix &m, Index source)
+{
+  Index n[2];
+  n[0] = m.ncols ();
+  n[1] = m.nrows ();
+
+  if (mpi_manager.get_rank () == source)
+    cout << "Broadcast matrix dimensions" << endl;
+
+  MPI::COMM_WORLD.Bcast (n, 2, MPI::INT, source);
+
+  // Resize the matrix on client nodes
+  if (mpi_manager.get_rank () != source)
+    {
+      m.resize (n[1], n[0]);
+    }
+
+  if (mpi_manager.get_rank () == source)
+    cout << "Broadcast matrix data" << endl;
+  MPI::COMM_WORLD.Bcast (m.get_raw_data (), n[1] * n[0], MPI::DOUBLE, source);
+
+}
+
+
+void
+mpi_send (Matrix &m, Index source, Index dest)
+{
+  Index n[2];
+  n[0] = m.ncols ();
+  n[1] = m.nrows ();
+
+  if (mpi_manager.get_rank () == source)
+    {
+      cout << "Send matrix dimensions" << endl;
+      MPI::COMM_WORLD.Send (n, 2, MPI::INT, dest, 0);
+      cout << "Broadcast matrix data" << endl;
+      MPI::COMM_WORLD.Send (m.get_raw_data (), n[1] * n[0], MPI::DOUBLE,
+                            dest, 0);
+    }
+  else if (mpi_manager.get_rank () == dest)
+    {
+      MPI::Status status;
+      MPI::COMM_WORLD.Recv (n, 2, MPI::INT, source, 0, status);
+      m.resize (n[1], n[0]);
+      MPI::COMM_WORLD.Recv (m.get_raw_data (), n[1] * n[0], MPI::DOUBLE,
+                            source, 0, status);
+    }
+
+}
+
 #endif // HAVE_MPI
+
