9a0299339f326cb8322c7e8ccce3417cbe4ee33f
sbuehler <sbuehler@aaf1aab0-4228-0410-ad68-8dceda47f409>
2003-07-16 15:31:17 +0000

    * src/test_matpack.cc: Moved sparse testing routines to test_sparse.cc.
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@3314 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/test_matpack.cc
+++ src/test_matpack.cc
@@ -148,77 +148,6 @@ void test2()
 }
 
 
-void test3()
-{
-  Sparse M(10,15);
-
-  /*
-  cout << "M.nrows(), M.ncols() = "
-       << M.nrows() << ", " << M.ncols() << "\n";
-  */
-  for (Index i=3; i<10; ++i)
-    M.rw(i,i) = i+1;
-  M.rw(0,0) = 1;
-  M.rw(0,1) = 2;
-  M.rw(0,2) = 3;
-
-
-  cout << "\nM = \n" << M;
-
-  /*
-  // Test Sparse matrix-Matrix multiplication
-  Matrix A(10,5);
-  Matrix C(15,5,2.0);
-  // C = 2;
-
-  mult(A, M, C(Range(joker), Range(joker)));
-  cout << "\nA = \n" << A << "\n";
-
-  */
-
-  // Test Sparse-Sparse multiplication
-  Sparse A(10,5);
-  Sparse C(15,5);
-  for (Index i=0; i<5; i++) {
-    C.rw(i*3,i) = i*3+1;
-    C.rw(i*3+1,i) = i*3+2;
-    C.rw(i*3+2,i) = i*3+3;
-  }
-
-  mult(A,M,C);
-
-  cout << "\nA = \n" << A;
-
-  /*
-  // Test transpose
-  Sparse B(15,10);
-
-  transpose(B,M);
-  cout << "\nM' = \n" << B;
-  */
-
-  /*
-  // Test rw-operator
-  Sparse S(M);
-  S.rw(2,0) = 5;
-
-  cout << "\nS(2,0) = " << S.ro(2,0) << "\n";
-
-  cout << "\nS = \n" << S;
-  */
-
-  /*
-  // Test vector multiplication
-  Vector y(20, 0.0);
-  Vector x(1,30,1);
-
-  mult(y[Range(1,10,2)], S, x[Range(1,15,2)]);
-
-  cout << "\ny = \n" << y << "\n";
-  */
-}
-
-
 void test4()
 {
   Vector a(10);
@@ -781,94 +710,6 @@ void test36()
   cout << "b = " << b << "\n";
 }
 
-// void test37()
-// {
-//   cout << "Test inflate for Vector.\n";
-
-//   Numeric x=7;
-//   describe(cout,x);
-
-// //   ConstVectorView v1 = inflate(x);
-// //   cout << "v1 = " << v1 << "\n";
-
-// //   VectorView v2 = inflate(x);
-// //   v2[0] += 1;
-// //   cout << "x, after increasing v2 by 1 = " << x << "\n";
-// }
-
-void test38()
-{
-  cout << "Test sparse matrix - sparse matrix multiplication\n";
-
-  Sparse B(757,2271);
-  Sparse C(B.ncols(),B.ncols());
-  Sparse A(B.nrows(),B.ncols());
-
-  Index i=0;
-  for (Index j=0; j<B.ncols(); j++) {
-    B.rw(i,j) = (j+1.0);
-    if( i<B.nrows()-1 )
-      i++;
-    else
-      i=0;
-  }
-
-  for (Index i=0; i<C.nrows(); i++)
-    C.rw(i,i) = 1;
-
-  mult(A,B,C);
-
-  cout << "\n(Sparse) A = \n" << A;
-  cout << "\n(Sparse) B = \n" << B;
-  cout << "\n(Sparse) C = \n" << C;
-
-  Matrix a(5,15), b(5,15), c(15,15);
-
-  i=0;
-  for (Index j=0; j<15; j++) {
-    b(i,j) = j+1;
-    if( i<4 )
-      i++;
-    else
-      i=0;
-  }
-
-  for (Index i=0; i<15; i++)
-    c(i,i) = 1;
-
-  mult(a,b,c);
-
-  //cout << "\n(Full) a = \n" << a << "\n";
-
-//  cout << "\n(Sparse) B = \n" << B << "\n";
-//  cout << "\n(Full) b = \n" << b << "\n";
-//  cout << "\n(Sparse) C = \n" << C << "\n";
-//  cout << "\n(Full) c = \n" << c << "\n";
-
-}
-
-void test39() 
-{
-  //Test sparse transpose function
-  Sparse B(1000,2000);
-  Sparse Bt(B.ncols(), B.nrows());
-
-  Index i=0;
-  for (Index j=0; j<B.ncols(); j=j+2) {
-    B.rw(i,j) = j+1;
-    if( i<B.nrows()-2 )
-      i += 2;
-    else
-      i=0;
-  }
-
-  cout << "\nB = \n" << B;
-
-  transpose(Bt,B);
-
-  cout << "\ntranspose(B) = \n" << Bt << "\n";
-}
-
 int main()
 {
 //   test1();
@@ -907,9 +748,6 @@ int main()
 //   test34();
 //   test35();
 //   test36();
-//  test37();
-//  test38();
-  test39();
 
   return 0;
 }
