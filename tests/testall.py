6fe48b2e2584c3e7431ae5865913751ebc61b8ef
claudia <claudia@aaf1aab0-4228-0410-ad68-8dceda47f409>
2005-04-11 10:49:45 +0000

    2005-04-11  Claudia Emde  <claudia@sat.physik.uni-bremen.de>
    	* arts-1-1-940
    
    	* tests/data/pnd_field_1D.xml: Modified. The pnd_field did not
    	match the p_grid for Cory's simpleMC, which resulted in a
    	assertion error. This is now fixed.
    
    	* tests/data/(gas_abs_lookup.xml, gas_abs_lookup.xml.bin): Converted
    	lookup table to binary format to make reading faster.
    
    	* tests/simpleDOIT.arts: Slightly modified.
    
    	* tests/testall.py: Modified DOIT test.
    
    	python testall.py -v works !!!
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@4379 aaf1aab0-4228-0410-ad68-8dceda47f409

--- tests/testall.py
+++ tests/testall.py
@@ -65,13 +65,13 @@ class DoitTest(unittest.TestCase):
         self.DOITrun.run()
         assert self.DOITrun.error=='','Error running simpleDOIT.arts: '+self.DOITrun.error
     def test2(self):
-        """Total radiance should be close to 255.5 K"""
+        """Total radiance should be close to 187.7 K"""
         I=self.DOITrun.get_val('y')[0]
-        assert abs(I-255.) < 1, 'I (='+str(I)+'K) is too far away from 255.5 K'
+        assert abs(I-187.) < 1., 'I (='+str(I)+'K) is too far away from 187.7 K'
     def test3(self):
-        """Polarization difference should be close to 1.0 K"""
+        """Polarization difference should be close to 8.1 K"""
         Q=self.DOITrun.get_val('y')[1]
-        assert abs(Q-1.) < 0.2, 'Q (='+str(Q)+'K) is too far away from 1. K'
+        assert abs(Q-8.) < 1., 'Q (='+str(Q)+'K) is too far away from 8.1 K'
         
 if __name__=='__main__':
     unittest.main()
