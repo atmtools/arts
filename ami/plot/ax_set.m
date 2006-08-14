987ee0f6a86be659c60c141e432978519cec6ee1
patrick <patrick@aaf1aab0-4228-0410-ad68-8dceda47f409>
2006-08-14 11:46:01 +0000

    See ChangeLog
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts/branches/ARTS-1-0@4787 aaf1aab0-4228-0410-ad68-8dceda47f409

--- ami/plot/ax_set.m
+++ ami/plot/ax_set.m
@@ -47,24 +47,24 @@ h = gca;
 
 %= General stuff
 %
-if isfield( A, 'POSITION' ) & ~isfield( A.POSITION )
+if isfield( A, 'POSITION' )
   set( h, 'Position', A.POSITION );
 end
 %
-if isfield( A, 'FONTSIZE' ) & ~isfield( A.FONTSIZE )
+if isfield( A, 'FONTSIZE' )
   set( h, 'FontSize', A.FONTSIZE );
 end
 %
-if isfield( A, 'FONTWEIGHT' ) & ~isfield( A.FONTWEIGHT )
+if isfield( A, 'FONTWEIGHT' )
   set( h, 'FontWeight', A.FONTWEIGHT );
 end
 %
-if isfield( A, 'GRID' ) & ~isfield( A.GRID )
+if isfield( A, 'GRID' )
   set( h, 'XGrid', A.GRID );
   set( h, 'YGrid', A.GRID );
 end
 %
-if isfield( A, 'BOX' ) & ~isfield( A.BOX )
+if isfield( A, 'BOX' )
   set( h, 'Box', A.BOX );
 end
 
@@ -72,15 +72,15 @@ end
 
 %= Title
 %
-if isfield( A, 'TITLE' ) & ~isfield( A.TITLE )
+if isfield( A, 'TITLE' )
   set( get(h,'Title'), 'String', A.TITLE );
 end
 %
-if isfield( A, 'TITLE_SIZE' ) & ~isfield( A.TITLE_SIZE )
+if isfield( A, 'TITLE_SIZE' )
   set( get(h,'Title'), 'FontSize', A.TITLE_SIZE );
 end
 %
-if isfield( A, 'TITLE_WEIGHT' ) & ~isfield( A.TITLE_WEIGHT )
+if isfield( A, 'TITLE_WEIGHT' )
   set( get(h,'Title'), 'FontWeight', A.TITLE_WEIGHT );
 end
 
@@ -88,27 +88,27 @@ end
 
 %= X axis
 %
-if isfield( A, 'XLABEL' ) & ~isfield( A.XLABEL )
+if isfield( A, 'XLABEL' )
   set( get(h,'Xlabel'), 'String', A.XLABEL );
 end
 %
-if isfield( A, 'XLABEL_SIZE' ) & ~isfield( A.XLABEL_SIZE )
+if isfield( A, 'XLABEL_SIZE' ) 
   set( get(h,'Xlabel'), 'FontSize', A.XLABEL_SIZE );
 end
 %
-if isfield( A, 'XLABEL_WEIGHT' ) & ~isfield( A.XLABEL_WEIGHT )
+if isfield( A, 'XLABEL_WEIGHT' ) 
   set( get(h,'Xlabel'), 'FontWeight', A.XLABEL_WEIGHT );
 end
 %
-if isfield( A, 'XGRID' ) & ~isfield( A.XGRID )
+if isfield( A, 'XGRID' )
   set( h, 'XGrid', A.XGRID );
 end
 %
-if isfield( A, 'XTICK' ) & ~isfield( A.XTICK )
+if isfield( A, 'XTICK' )
   set( h, 'XTick', A.XTICK );
 end
 %
-if isfield( A, 'XTICKLABEL' ) & ~isfield( A.XTICKLABEL )
+if isfield( A, 'XTICKLABEL' ) 
   set( h, 'XTickLabel', A.XTICKLABEL );
 end
 
@@ -116,28 +116,28 @@ end
 
 %= Y axis
 %
-if isfield( A, 'YLABEL' ) & ~isfield( A.YLABEL )
+if isfield( A, 'YLABEL' )
   set( get(h,'Ylabel'), 'String', A.YLABEL );
 end
 %
-if isfield( A, 'YLABEL_SIZE' ) & ~isfield( A.YLABEL_SIZE )
+if isfield( A, 'YLABEL_SIZE' )
   set( get(h,'Ylabel'), 'FontSize', A.YLABEL_SIZE );
 end
 %
-if isfield( A, 'YLABEL_WEIGHT' ) & ~isfield( A.YLABEL_WEIGHT )
+if isfield( A, 'YLABEL_WEIGHT' )
   set( get(h,'Ylabel'), 'FontWeight', A.YLABEL_WEIGHT );
 end
 %
-if isfield( A, 'YGRID' ) & ~isfield( A.YGRID )
+if isfield( A, 'YGRID' )
   set( h, 'YGrid', A.YGRID );
 end
 %
-if isfield( A, 'YTICK' ) & ~isfield( A.YTICK )
+if isfield( A, 'YTICK' )
   set( h, 'YTick', A.YTICK );
 end
 
 %
-if isfield( A, 'YTICKLABEL' ) & ~isfield( A.YTICKLABEL )
+if isfield( A, 'YTICKLABEL' )
   set( h, 'YTickLabel', A.YTICKLABEL );
 end
 
