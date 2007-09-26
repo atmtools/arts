3474bb19a3c1ff914ad683a98f5895cb648771eb
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2007-09-26 08:58:54 +0000

    * arts-1-1-1179
    * tests/Makefile.am:
    
    Restructured the tests so that every test has its own directory. All
    files required by a test should be available in that subdirectory.
    A test directory can contain more than one test if they're closely
    related (see tests/MonteCarlo).
    The advantage of having self-contained test directories is that the
    user can just copy one of the directories and use it as a starting
    point for his own calculations (having all necessary input files
    available without collecting them from other directories).
    
    Convention for naming the tests is to have the testnames all start
    with 'Test', followed by the name of the subdirectory they're in.
    If there's more than one test in the subdirectory append a
    descriptive text to the end of the testname. The controlfiles should
    have the same name as the test + .arts.
    
    Example for the MonteCarlo case: 
    
    Test directory:              tests/MonteCarlo
    
    Tests (named in testall.py): TestMonteCarloDataPrepare
    			     TestMonteCarloGeneral
    			     TestMonteCarloGeneralGaussian
    
    Corresponding controlfiles:
    
    tests/MonteCarlo/TestMonteCarloDataPrepare.arts
    tests/MonteCarlo/TestMonteCarloGeneral.arts
    tests/MonteCarlo/TestMonteCarloGeneralGaussian.arts
    
    All tests have to be added to the "check" target in tests/Makefile.am
    and the directories to TESTDIRS in the same Makefile.am.
    Dependencies between tests can also be specified in there.
    
    Tests can now run in parallel by default. E.g. if you have a computer
    with 4 CPUs run: make -j4 check
    To avoid cluttering when running tests in parallel, all output is
    written to log files (TESTNAME.log).
    
    * tests/testall.py:
    
    Added the ability to run controlfiles from subdirectories.
    
    Removed numbering from tests and give them clear names. 
    
    * tests/data/, tests/*.arts: Removed.
    
    * tests/{Abs,ClearSky,DISORT,DOIT,MonteCarlo}/: Added.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@5465 aaf1aab0-4228-0410-ad68-8dceda47f409

--- tests/testall.py
+++ tests/testall.py
@@ -3,12 +3,13 @@ import unittest
 
 class ArtsRun:
     """A baby arts run class for handling test cases"""
-    def __init__(self,control_file):
+    def __init__(self,subdir,control_file):
         """Initialised by the control file name"""
+        self.subdir=subdir
         self.control_file=control_file
     def run(self):
         """Run the control file"""
-        w,r,e=os.popen3('../src/arts '+self.control_file)
+        w,r,e=os.popen3('cd '+self.subdir+'; ../../src/arts '+self.control_file)
         self.output=r.read()
         self.error=e.read()
     def get_val(self,name):
@@ -21,21 +22,21 @@ class ArtsRun:
             val_list.append(float(s))
         return val_list
 
-class MCDataPrepareTest(unittest.TestCase):
+class TestMonteCarloDataPrepare(unittest.TestCase):
     """Preparing data for ARTS-MC tests"""
-    MCrun=ArtsRun('MCDataPrepare.arts')
+    MCrun=ArtsRun('MonteCarlo', 'TestMonteCarloDataPrepare.arts')
     def test1(self):
         """MCDataPrepare.arts should run with no errors"""
         self.MCrun.run()
-        assert self.MCrun.error=='','Error running MCDataPrepare.arts: '+self.MCrun.error
+        assert self.MCrun.error=='','Error running TestMonteCarloDataPrepare.arts: '+self.MCrun.error
             
-class MonteCarloTest(unittest.TestCase):
+class TestMonteCarloSimple(unittest.TestCase):
     """Testing the ARTS-MC algorithm"""
-    MCrun=ArtsRun('simpleMC.arts')
+    MCrun=ArtsRun('MonteCarlo', 'TestMonteCarloSimple.arts')
     def test1(self):
         """Simple Monte Carlo test should run with no errors"""
         self.MCrun.run()
-        assert self.MCrun.error=='','Error running simpleMC.arts: '+self.MCrun.error
+        assert self.MCrun.error=='','Error running TestMonteCarloSimple.arts: '+self.MCrun.error
     def test2(self):
         """Total radiance should be close to 201.8 K"""
         I=self.MCrun.get_val('y')[0]
@@ -47,13 +48,13 @@ class MonteCarloTest(unittest.TestCase):
         dQ=self.MCrun.get_val('mc_error')[1]
         assert abs(Q-7.6) < 4*Q, 'Q (='+str(Q)+'K) is too far away from 7.6 K'
         
-class MonteCarloTest2(unittest.TestCase):
+class TestMonteCarloWithIncomingLookup(unittest.TestCase):
     """Testing the ARTS-MC algorithm with incoming lookup"""
-    MCrun=ArtsRun('simpleMCWithIncomingLookup.arts')
+    MCrun=ArtsRun('MonteCarlo', 'TestMonteCarloWithIncomingLookup.arts')
     def test1(self):
         """Simple Monte Carlo test (with incoming lookup) should run with no errors"""
         self.MCrun.run()
-        assert self.MCrun.error=='','Error running simpleMCWithIncomingLookup.arts: '+self.MCrun.error
+        assert self.MCrun.error=='','Error running TestMonteCarloWithIncomingLookup.arts: '+self.MCrun.error
     def test2(self):
         """Total radiance should be close to 201.8 K"""
         I=self.MCrun.get_val('y')[0]
@@ -65,13 +66,13 @@ class MonteCarloTest2(unittest.TestCase):
         dQ=self.MCrun.get_val('mc_error')[1]
         assert abs(Q-7.6) < 4*Q, 'Q (='+str(Q)+'K) is too far away from 7.6 K'
         
-class MonteCarloTest3(unittest.TestCase):
+class TestMonteCarloGeneral(unittest.TestCase):
     """Testing the MCGeneral algorithm"""
-    MCrun=ArtsRun('simpleMCGeneral.arts')
+    MCrun=ArtsRun('MonteCarlo', 'TestMonteCarloGeneral.arts')
     def test1(self):
         """MCGeneral test should run with no errors"""
         self.MCrun.run()
-        assert self.MCrun.error=='','Error running simpleMCGeneral.arts: '+self.MCrun.error
+        assert self.MCrun.error=='','Error running TestMonteCarloSimple.arts: '+self.MCrun.error
     def test2(self):
         """Total radiance should be close to 201.8 K"""
         I=self.MCrun.get_val('y')[0]
@@ -83,13 +84,13 @@ class MonteCarloTest3(unittest.TestCase):
         dQ=self.MCrun.get_val('mc_error')[1]
         assert abs(Q-7.6) < 4*Q, 'Q (='+str(Q)+'K) is too far away from 7.6 K'
         
-class MonteCarloTest4(unittest.TestCase):
+class TestMonteCarloGeneralGaussian(unittest.TestCase):
     """Testing the MCGeneral algorithm with a Gaussian antenna response"""
-    MCrun=ArtsRun('simpleMCGeneralGaussian.arts')
+    MCrun=ArtsRun('MonteCarlo', 'TestMonteCarloGeneralGaussian.arts')
     def test1(self):
         """MCGeneral (Gaussian Antenna) test should run with no errors"""
         self.MCrun.run()
-        assert self.MCrun.error=='','Error running simpleMCGeneralGaussian.arts: '+self.MCrun.error
+        assert self.MCrun.error=='','Error running TestMonteCarloGeneralGaussian.arts: '+self.MCrun.error
     def test2(self):
         """Total radiance should be close to 201 K"""
         I=self.MCrun.get_val('y')[0]
@@ -101,13 +102,13 @@ class MonteCarloTest4(unittest.TestCase):
         dQ=self.MCrun.get_val('mc_error')[1]
         assert abs(Q-7.6) < 4*Q, 'Q (='+str(Q)+'K) is too far away from 7.6 K'
         
-class DoitTest(unittest.TestCase):
+class TestDOIT(unittest.TestCase):
     """Testing the ARTS-DOIT algorithm"""
-    DOITrun=ArtsRun('simpleDOIT.arts')
+    DOITrun=ArtsRun('DOIT', 'TestDOIT.arts')
     def test1(self):
         """Simple DOIT test should run with no errors"""
         self.DOITrun.run()
-        assert self.DOITrun.error=='','Error running simpleDOIT.arts: '+self.DOITrun.error
+        assert self.DOITrun.error=='','Error running TestDOIT.arts: '+self.DOITrun.error
     def test2(self):
         """Total radiance should be close to 204.5 K"""
         I=self.DOITrun.get_val('y')[0]
@@ -118,26 +119,26 @@ class DoitTest(unittest.TestCase):
         assert abs(Q-7.2) < 1., 'Q (='+str(Q)+'K) is too far away from 7.2 K'
         
 
-class ClearSkyTest(unittest.TestCase):
+class TestClearSky(unittest.TestCase):
     """Testing clear sky calculations"""
-    CSrun=ArtsRun('simpleClearSky.arts')
+    CSrun=ArtsRun('ClearSky', 'TestClearSky.arts')
     def test1(self):
         """Simple clear sky test should run with no errors"""
         self.CSrun.run()
-        assert self.CSrun.error=='','Error running simpleClearSky.arts: '+self.CSrun.error
+        assert self.CSrun.error=='','Error running TestClearSky.arts: '+self.CSrun.error
     def test2(self):
         """Total radiance should be close to 276.523 K"""
         I=self.CSrun.get_val('y')[0]
         assert abs(I-276.523) < 0.01, 'I (='+str(I)+'K) is too far away from 276.523 K'
 
         
-class AbsTest(unittest.TestCase):
+class TestAbs(unittest.TestCase):
     """Testing the ARTS Absorption module"""
-    Absrun=ArtsRun('simpleAbs.arts')
+    Absrun=ArtsRun('Abs', 'TestAbs.arts')
     def test1(self):
         """Simple Absorption test should run with no errors"""
         self.Absrun.run()
-        assert self.Absrun.error=='','Error running simpleAbs.arts: '+self.Absrun.error
+        assert self.Absrun.error=='','Error running TestAbs.arts: '+self.Absrun.error
 #     def test2(self):
 #         """Total radiance should be close to 204.5 K"""
 #         I=self.Absrun.get_val('y')[0]
