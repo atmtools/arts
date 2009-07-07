d95e785f525b79c4b3b4c31ae3e8ed9a592edfef
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2009-07-07 09:49:54 +0000

    * arts-1-12-50
    * cmake/modules/ArtsTestcases.cmake, tests/CMakeLists.txt:
    
    Bad idea to use 'cp -u'... some variants of copy don't support the -u
    option. Instead we're now using the proper system-independent 'cmake
    -E copy_if_different'.
    
    Also fix dependencies between the test cases.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@6064 aaf1aab0-4228-0410-ad68-8dceda47f409

--- cmake/modules/ArtsTestcases.cmake
+++ cmake/modules/ArtsTestcases.cmake
@@ -1,22 +1,32 @@
 macro (ARTS_ADD_PREPARE_TESTCASES TARGETS)
   set (ADD_TARGETS "")
   foreach(TESTCASE ${ARGN})
-    set (TARGET PrepareTest${TESTCASE})
+    set (TARGET PrepareTest${TESTCASE}Directory)
+    add_custom_command(
+      OUTPUT ${TARGET}
+      COMMAND cmake -E
+        ARGS make_directory ${CMAKE_CURRENT_BINARY_DIR}/${TESTCASE}
+    )
+    set (ADD_TARGETS ${ADD_TARGETS} ${TARGET})
+
     file (GLOB COPYFILES
       "${CMAKE_CURRENT_SOURCE_DIR}/${TESTCASE}/*.arts"
       "${CMAKE_CURRENT_SOURCE_DIR}/${TESTCASE}/*.xml"
       "${CMAKE_CURRENT_SOURCE_DIR}/${TESTCASE}/*.xml.bin"
       "${CMAKE_CURRENT_SOURCE_DIR}/${TESTCASE}/*.xml.gz"
     )
-    add_custom_command(
-      OUTPUT ${TARGET}
-      COMMAND cmake -E
-        ARGS make_directory ${CMAKE_CURRENT_BINARY_DIR}/${TESTCASE}
-      COMMAND cp
-        ARGS -u ${COPYFILES}
-             "${CMAKE_CURRENT_BINARY_DIR}/${TESTCASE}/"
-    )
+    foreach (COPYFILE ${COPYFILES})
+      get_filename_component (BASECOPYFILE ${COPYFILE} NAME)
+      set (TARGET PrepareTest${TESTCASE}File${BASECOPYFILE})
+      add_custom_command(
+        OUTPUT ${TARGET}
+        COMMAND cmake -E
+          ARGS copy_if_different ${COPYFILE}
+               "${CMAKE_CURRENT_BINARY_DIR}/${TESTCASE}/"
+        DEPENDS PrepareTest${TESTCASE}Directory
+      )
     set (ADD_TARGETS ${ADD_TARGETS} ${TARGET})
+    endforeach (COPYFILE)
   endforeach (TESTCASE)
   set(${TARGETS} ${ADD_TARGETS})
 endmacro (ARTS_ADD_PREPARE_TESTCASES)
@@ -32,7 +42,7 @@ macro (ARTS_ADD_TESTCASES TARGETS DEPENDENCIES)
               echo "${TARGET} ok."\; else
               echo "${TARGET} failed. Find details in ${TARGET}.log"\; exit 1\;
               fi
-      DEPENDS ../src/arts ${DEPENDENCIES}
+      DEPENDS ../src/arts PrepareTestScripts ${DEPENDENCIES}
     )
     set(ADD_TARGETS ${ADD_TARGETS} ${TARGET})
   endforeach(TESTCASE)
