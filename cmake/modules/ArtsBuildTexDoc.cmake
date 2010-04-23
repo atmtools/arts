0715d8f17ee69780e06c9312b8fb2031b3c9cc2b
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2010-04-23 11:01:33 +0000

    * arts-1-14-25
    * cmake/modules/ArtsBuildTexDoc.cmake:
    
    Split building of each document into two stages. The first stage runs
    latex, makeindex, bibtex, and latex. After that the aux files are
    copied to the build directories of the other documents. The second
    stage is another latex run for each document. Now it's possible to
    parallel build the docs and get the cross references right.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@6352 aaf1aab0-4228-0410-ad68-8dceda47f409

--- cmake/modules/ArtsBuildTexDoc.cmake
+++ cmake/modules/ArtsBuildTexDoc.cmake
@@ -7,47 +7,64 @@ macro (ARTS_ADD_TEX_DOC TARGET TEXFILES FIGFILES)
     "save_size=15000" ${PDFLATEX_COMPILER} ${PDFLATEX_OPTIONS} ${TARGET}
     ${PDFLATEX_LOGGING})
 
-  get_filename_component(MAKEINDEX_PATH "${MAKEINDEX_COMPILER}" PATH)
+  if (${TARGET} STREQUAL "arts_user")
+    set (OTHER_DOC1 "arts_developer")
+    set (OTHER_DOC2 "arts_theory")
+  elseif (${TARGET} STREQUAL "arts_theory")
+    set (OTHER_DOC1 "arts_user")
+    set (OTHER_DOC2 "arts_developer")
+  elseif (${TARGET} STREQUAL "arts_developer")
+    set (OTHER_DOC1 "arts_user")
+    set (OTHER_DOC2 "arts_theory")
+  endif ()
+
+  get_filename_component (MAKEINDEX_PATH "${MAKEINDEX_COMPILER}" PATH)
 
   add_custom_command (
-    OUTPUT ${PDFLATEX_OUTPUTDIR}.stamp
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${PDFLATEX_OUTPUTDIR}
-    COMMAND ${CMAKE_COMMAND} -E touch ${PDFLATEX_OUTPUTDIR}.stamp
+    OUTPUT ${TARGET}.stamp
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${TARGET}
+    COMMAND ${CMAKE_COMMAND} -E touch ${TARGET}.stamp
   )
 
   add_custom_command (
-    OUTPUT ${TARGET}.pdf
+    OUTPUT ${TARGET}.stage1
     COMMAND ${CMAKE_COMMAND} -E remove -f ${TARGET}.log
     COMMAND ${PDFLATEX_COMMAND}
             && PATH=$PATH:${MAKEINDEX_PATH} ${MAKEINDEX_COMPILER} -q ${TARGET}.idx ${PDFLATEX_LOGGING}
             && BSTINPUTS=.:${CMAKE_CURRENT_SOURCE_DIR}: BIBINPUTS=.:${CMAKE_CURRENT_SOURCE_DIR}: save_size=15000
               ${BIBTEX_COMPILER} ${TARGET} ${PDFLATEX_LOGGING}
-            && ${PDFLATEX_COMMAND}
-            && ${PDFLATEX_COMMAND}
             && ${PDFLATEX_COMMAND} || \(cat ${TARGET}.log && exit 1\)
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PDFLATEX_OUTPUTDIR}/${TARGET}.pdf" "${CMAKE_CURRENT_BINARY_DIR}"
+    COMMAND cp "${PDFLATEX_OUTPUTDIR}/*.aux" "${PDFLATEX_OUTPUTDIR}/../${OTHER_DOC1}/" 
+    COMMAND cp "${PDFLATEX_OUTPUTDIR}/*.aux" "${PDFLATEX_OUTPUTDIR}/../${OTHER_DOC2}/" 
+    COMMAND ${CMAKE_COMMAND} -E touch ${PDFLATEX_OUTPUTDIR}.stage1
     WORKING_DIRECTORY ${PDFLATEX_OUTPUTDIR}
-    DEPENDS ${TEXFILES} ${FIGFILES} ${CMAKE_CURRENT_BINARY_DIR}/auto_version.tex ${PDFLATEX_OUTPUTDIR}.stamp
+    DEPENDS ${TEXFILES} ${FIGFILES}
+            ${CMAKE_CURRENT_BINARY_DIR}/auto_version.tex
+            ${TARGET}.stamp
   )
 
-  add_custom_target (${TARGET} ALL DEPENDS
-    ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.pdf)
+  add_custom_command (
+    OUTPUT ${TARGET}.pdf
+    COMMAND ${CMAKE_COMMAND} -E remove -f ${TARGET}.log
+    COMMAND ${PDFLATEX_COMMAND} || \(cat ${TARGET}.log && exit 1\)
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PDFLATEX_OUTPUTDIR}/${TARGET}.pdf" "${CMAKE_CURRENT_BINARY_DIR}"
+    WORKING_DIRECTORY ${PDFLATEX_OUTPUTDIR}
+    DEPENDS ${TARGET}.stage1 ${OTHER_DOC1}.stage1 ${OTHER_DOC2}.stage1
+  )
+
+  add_custom_target (${TARGET}-stamp DEPENDS ${TARGET}.stamp)
+  add_custom_target (${TARGET}-stage1 DEPENDS ${TARGET}.stage1)
+
+  add_custom_target (${TARGET} ALL DEPENDS ${TARGET}.pdf)
+
+  add_dependencies (${TARGET}-stage1
+                    ${TARGET}-stamp ${OTHER_DOC1}-stamp ${OTHER_DOC2}-stamp)
+
+  add_dependencies (${TARGET}
+                    ${TARGET}-stage1 ${OTHER_DOC1}-stage1 ${OTHER_DOC2}-stage1)
 
   install (FILES ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.pdf
            DESTINATION share/doc/arts)
-
-         #  set (TEXCLEANFILES "")
-         #get_directory_property (TEXCLEANFILES ADDITIONAL_MAKE_CLEAN_FILES)
-         #foreach (TEXFILE ${TEXFILES})
-         #get_filename_component (BASETEXFILE ${TEXFILE} NAME_WE)
-         #set (TEXCLEANFILES ${TEXCLEANFILES} ${PDFLATEX_OUTPUTDIR}/${BASETEXFILE}.aux)
-         #endforeach (TEXFILE)
-
-         #  set (TEXCLEANFILES ${TEXCLEANFILES}
-       #       ${PDFLATEX_OUTPUTDIR}/${TARGET}.bbl ${PDFLATEX_OUTPUTDIR}/${TARGET}.blg
-       #       ${PDFLATEX_OUTPUTDIR}/${TARGET}.idx ${PDFLATEX_OUTPUTDIR}/${TARGET}.ilg
-       #       ${PDFLATEX_OUTPUTDIR}/${TARGET}.ind ${PDFLATEX_OUTPUTDIR}/${TARGET}.log
-       #      ${PDFLATEX_OUTPUTDIR}/${TARGET}.out ${PDFLATEX_OUTPUTDIR}/${TARGET}.toc)
 
   get_directory_property (TEXCLEANFILES ADDITIONAL_MAKE_CLEAN_FILES)
   set (TEXCLEANFILES ${TEXCLEANFILES} ${TARGET}.log ${PDFLATEX_OUTPUTDIR})
