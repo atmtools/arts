c29955a618ba6a2348e32cb553e7e27f5b0f7656
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2011-05-24 06:54:20 +0000

    * arts-1-15-4
    * cmake/scripts/update_doxyfile.cmake, doc/doxygen/Doxyfile.in:
    
    Add build/src to the Doxygen input path. Since we no longer build
    inside the source tree, the auto_* files are generated in the
    build/src directory. If doxygen can't find the auto_* files, the
    documentation for workspace methods is missing. Bug reported by Gerrit.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@6970 aaf1aab0-4228-0410-ad68-8dceda47f409

--- cmake/scripts/update_doxyfile.cmake
+++ cmake/scripts/update_doxyfile.cmake
@@ -3,6 +3,7 @@ set (CMAKE_MODULE_PATH "${ARTS_SOURCE_DIR}/cmake/modules")
 include (ArtsVersion)
 
 set (top_srcdir ${ARTS_SOURCE_DIR})
+set (top_builddir ${ARTS_BINARY_DIR})
 
 file (READ "${ARTS_BINARY_DIR}/auto_version.txt" VERSION)
 configure_file (${ARTS_SOURCE_DIR}/doc/doxygen/Doxyfile.in
