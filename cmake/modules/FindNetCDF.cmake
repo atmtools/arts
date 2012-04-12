8d90cede7bafc894db57bd64b30ec44d9e075958
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2012-04-12 06:17:18 +0000

    * arts-2-1-86
    * README: Updated. Add section headings. Add ctest documentation.
    
    * CMakeLists.txt:
    
    'make check' only runs quick controlfiles now. 'make check-all' runs
    all tests.
    
    Add cmake option TEST_JOBS to let user specify how many tests to be
    run in parallel. (cmake -DTEST_JOBS=4 ..)
    
    * cmake/modules/FindNetCDF.cmake:
    
    Netcdf library might also be called netcdf4/netcdf_c++4.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@7500 aaf1aab0-4228-0410-ad68-8dceda47f409

--- cmake/modules/FindNetCDF.cmake
+++ cmake/modules/FindNetCDF.cmake
@@ -9,8 +9,8 @@
 if (NOT NO_NETCDF)
   find_path (NETCDF_INCLUDE_DIR netcdf.h PATH_SUFFIXES netcdf)
 
-  find_library (NETCDF_LIBRARY NAMES netcdf)
-  find_library (NETCDFXX_LIBRARY NAMES netcdf_c++)
+  find_library (NETCDF_LIBRARY NAMES netcdf4 netcdf)
+  find_library (NETCDFXX_LIBRARY NAMES netcdf_c++4 netcdf_c++)
 
   set (NETCDF_LIBRARIES ${NETCDF_LIBRARY} ${NETCDFXX_LIBRARY})
 
