cfbfefe26c14676a631bde934ab54f19cdadbd60
patrick <patrick@aaf1aab0-4228-0410-ad68-8dceda47f409>
2013-10-07 11:54:12 +0000

    2013-10-07  Patrick Eriksson  <patrick.eriksson@chalmers.se>
                * arts-2-1-765
    
                * controlfiles/artscomponents/radiolink/TestRadioLink.arts:
                Updated reference values, to have a clear reference (data and
                platform given as comment in file). Increased tolerance for y to
                2e-17, following data reported by Nicolas. This is about 2%, but
                impact of numerical inaccuracy on defocuisng results are high (see
                comment for arts-2-1-495)
    
                * controlfiles/artscomponents/faraday/test_faraday.m: Improved
                comment and accuracy of scaling factor.
    
                * doc/uguide/faraday.tex: Corrected a citep to citet.
    
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@8666 aaf1aab0-4228-0410-ad68-8dceda47f409

--- controlfiles/artscomponents/faraday/test_faraday.m
+++ controlfiles/artscomponents/faraday/test_faraday.m
@@ -8,17 +8,28 @@ function test_faraday
 % Uncomment some lines to store the data that are required  
 
 % Read data stored by ARTS test file  
-f = xmlLoad( 'f.xml' );
-n = xmlLoad( 'ne_field.xml' );
-b = xmlLoad( 'bw_field.xml' );
-z = xmlLoad( 'z_field.xml' ); 
-r = xmlLoad( 'farrot_totalREFERENCE.xml' );
+f   = xmlLoad( 'f.xml' );
+vmr = xmlLoad( 'vmr_field.xml' );
+b   = xmlLoad( 'bw_field.xml' );
+z   = xmlLoad( 'z_field.xml' ); 
+r   = xmlLoad( 'farrot_totalREFERENCE.xml' );
+%
+n   = vmr(end,:)';
 
-% Use integral expression shown in AUG + convert to degrees and change sign
-% as trapz goes from 0 to TOA while photons travel in the opposite direction
+
+% Use integral expression shown in AUG + convert to degrees and change sign.
+% The later as magnetic field vector and photon direction are reversed, and
+% the angular part of the dot product equals -1. 
 % 
-r0 = -( 180/pi * 23648 * trapz(z,b.*n) ) ./ (f.*f);
+r0 = -( 180/pi * 2.364797970062947e+04 * trapz(z,b.*n) ) ./ (f.*f);
 
-plot(f/1e9,r(1:4:end)-r0)
+figure(1)
+plot(f/1e9, r(1:4:end)-r0 )
 xlabel( 'Frequency [GHz]' )
 ylabel( 'Difference [deg]' )
+
+figure(2)
+plot(f/1e9, r(1:4:end) )
+xlabel( 'Frequency [GHz]' )
+ylabel( 'Calculated rotation [deg]' )
+
