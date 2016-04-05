4116d4c12e1f69f16f951b4448be48e8942620a1
simonpf <simonpf@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-04-05 13:40:11 +0000

    2016-04-05  Simon Pfreundschuh  <simonpf@chalmers.se>
     * arts-2-3-472
    
     * src/CMakeLists.txt Added macro to signal C++11 support.
    
     * m_oem.cc Fixed conditional compilation to also work in absence of
     C++11 support.
    
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@9789 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/m_oem.cc
+++ src/m_oem.cc
@@ -46,11 +46,6 @@
 #include "math_funcs.h"
 #include "physics_funcs.h"
 
-// Include only if compiling with C++11.
-#if __cplusplus >= 201103L
-
-#include "oem.h"
-
 extern const String ABSSPECIES_MAINTAG;
 extern const String TEMPERATURE_MAINTAG;
 
@@ -64,88 +59,6 @@ extern const String SINEFIT_MAINTAG;
   === Help functions 
   ===========================================================================*/
 
-//! Wrapper class for forward model.
-/*!
-  Wrapper class for the inversion_iterate_agendaExecute function to implement
-  the forward model interface used by the non-linear oem function in oem.cc.
-  The object is constructed with the pointers to the variables used as arguments
-  for the function and then simply forwards the calls made to
-  ForwardModel::evaluate() and ForwardModel::evaluate_jacobian() to
-  inversion_iterate_agendaExecute.
-
- */
-class AgendaWrapper
-{
-    Workspace *ws;
-    MatrixView *jacobian;
-    const Agenda *inversion_iterate_agenda;
-public:
-
-    const unsigned int m,n;
-
-//! Create inversion_iterate_agendaExecute wrapper.
-/*!
-  Initializes the wrapper object for the inversion_iterate_agendaExecute
-  method. The object forwards the evaluate() and evaluate_jacobian() calls
-  made by the iterative OEM methods to inversion_iterate_agendaExecute using
-  the arguments provided to the constructor.
-
-  \param ws_ Pointer to the workspace argument of the agenda execution function.
-  function.
-  \param inversion_iterate_agenda_ Pointer to the x argument of the agenda
-  execution function.
-
-*/
-    AgendaWrapper( Workspace *ws_,
-                   MatrixView *jacobian_,
-                   const Agenda *inversion_iterate_agenda_ ) :
-        ws( ws_ ),
-        jacobian( jacobian_ ),
-        inversion_iterate_agenda( inversion_iterate_agenda_ ),
-        m( (unsigned int) jacobian_->nrows()),
-        n( (unsigned int) jacobian_->ncols())
-        {}
-
-//! Evaluate forward model and compute Jacobian.
-/*!
-
-  Forwards the call to evaluate_jacobian() and evaluate() that is made by
-  Gauss-Newton and Levenberg-Marquardt OEM methods using the variables pointed
-  to by the pointers provided to the constructor as arguments.
-
-  \param[out] y The measurement vector y = K(x) for the current state vector x
-  as computed by the forward model.
-  \param[out] J The Jacobian Ki=d/dx(K(x)) of the forward model.
-  \param[in] x The current state vector x.
-*/
-    OEMMatrix Jacobian(const OEMVector &xi)
-    {
-        OEMMatrix Ki;
-        OEMVector yi;
-        inversion_iterate_agendaExecute( *ws, yi, Ki, xi, 1,
-                                         *inversion_iterate_agenda );
-        return Ki;
-    }
-
-//! Evaluate forward model.
-/*!
-
-  Forwards the call to evaluate that is made by Gauss-Newton and
-  Levenberg-Marquardt OEM methods to the function pointers provided.
-
-  \param[out] y The measurement vector y = K(x) for the current state vector x.
-  \param[in] x The current state vector x.
-*/
-    OEMVector evaluate(const OEMVector &xi)
-    {
-        OEMMatrix Ki;
-        OEMVector yi;
-        inversion_iterate_agendaExecute( *ws, yi, Ki, xi, 1,
-                                         *inversion_iterate_agenda );
-        return yi;
-    }
-
-};
 
 //! Determines grid positions for regridding of atmospheric fields to retrieval
 //  grids
@@ -639,7 +552,93 @@ void x2arts_std(
     }   
 }
 
+#ifdef CXX11
 
+// Include only if compiling with C++11.
+#include "oem.h"
+
+//! Wrapper class for forward model.
+/*!
+  Wrapper class for the inversion_iterate_agendaExecute function to implement
+  the forward model interface used by the non-linear oem function in oem.cc.
+  The object is constructed with the pointers to the variables used as arguments
+  for the function and then simply forwards the calls made to
+  ForwardModel::evaluate() and ForwardModel::evaluate_jacobian() to
+  inversion_iterate_agendaExecute.
+
+ */
+class AgendaWrapper
+{
+    Workspace *ws;
+    MatrixView *jacobian;
+    const Agenda *inversion_iterate_agenda;
+public:
+
+    const unsigned int m,n;
+
+//! Create inversion_iterate_agendaExecute wrapper.
+/*!
+  Initializes the wrapper object for the inversion_iterate_agendaExecute
+  method. The object forwards the evaluate() and evaluate_jacobian() calls
+  made by the iterative OEM methods to inversion_iterate_agendaExecute using
+  the arguments provided to the constructor.
+
+  \param ws_ Pointer to the workspace argument of the agenda execution function.
+  function.
+  \param inversion_iterate_agenda_ Pointer to the x argument of the agenda
+  execution function.
+
+*/
+    AgendaWrapper( Workspace *ws_,
+                   MatrixView *jacobian_,
+                   const Agenda *inversion_iterate_agenda_ ) :
+        ws( ws_ ),
+        jacobian( jacobian_ ),
+        inversion_iterate_agenda( inversion_iterate_agenda_ ),
+        m( (unsigned int) jacobian_->nrows()),
+        n( (unsigned int) jacobian_->ncols())
+        {}
+
+//! Evaluate forward model and compute Jacobian.
+/*!
+
+  Forwards the call to evaluate_jacobian() and evaluate() that is made by
+  Gauss-Newton and Levenberg-Marquardt OEM methods using the variables pointed
+  to by the pointers provided to the constructor as arguments.
+
+  \param[out] y The measurement vector y = K(x) for the current state vector x
+  as computed by the forward model.
+  \param[out] J The Jacobian Ki=d/dx(K(x)) of the forward model.
+  \param[in] x The current state vector x.
+*/
+    OEMMatrix Jacobian(const OEMVector &xi)
+    {
+        OEMMatrix Ki;
+        OEMVector yi;
+        inversion_iterate_agendaExecute( *ws, yi, Ki, xi, 1,
+                                         *inversion_iterate_agenda );
+        return Ki;
+    }
+
+//! Evaluate forward model.
+/*!
+
+  Forwards the call to evaluate that is made by Gauss-Newton and
+  Levenberg-Marquardt OEM methods to the function pointers provided.
+
+  \param[out] y The measurement vector y = K(x) for the current state vector x.
+  \param[in] x The current state vector x.
+*/
+    OEMVector evaluate(const OEMVector &xi)
+    {
+        OEMMatrix Ki;
+        OEMVector yi;
+        inversion_iterate_agendaExecute( *ws, yi, Ki, xi, 1,
+                                         *inversion_iterate_agenda );
+        return yi;
+    }
+
+};
 
 /* Workspace method: Doxygen documentation will be auto-generated */
 template <typename Se_t, typename Sx_t>
@@ -877,6 +876,7 @@ void oem(
                   ml_ga_settings, clear_matrices, display_progress, v );
 }
 
+
 #else
 
 void oem(
@@ -915,4 +915,4 @@ void oem(
     std::runtime_error("You have to compile ARTS with C++11 support to enable OEM.");
 }
 
-#endif // C++11
+#endif // CXX11
