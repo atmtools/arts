56e7b1306bc40b076c85e4ab8314bb7e88a1cc57
simonpf <simonpf@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-06-07 21:10:01 +0000

    2017-06-07  Simon Pfreundschuh <simonpf@chalmers.se>
    	* arts-2-3-704
    
    	* 3rdparty/invlib/src/invlib/invlib.h: Added missing invlib file.
    
    
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@10504 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ 3rdparty/invlib/src/invlib/invlib.h
@@ -0,0 +1,18 @@
+/**
+ * @file traits.h
+ * @author Simon Pfreundschuh
+ * @date 2017-06-07
+ * @brief Invlib super class used to distinguish invlib code from  external code.
+ */
+
+#ifndef INVLIB_H
+#define INVLIB_H
+
+namespace invlib
+{
+    
+class Invlib {};
+
+}     // namespace::invlib
+
+#endif // TRAITS_H
