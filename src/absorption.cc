f1d90e5f3b4ca99f0625e203dcdc4ca1d3ae41f0
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2018-04-13 05:47:35 +0000

    * arts-2-3-1022
    * src/absorption.cc (xsec_species):
    
    Only generate extra info output if it will be visible.
    
    git-svn-id: https://www.sat.ltu.se/svn/rt/arts/trunk@10955 aaf1aab0-4228-0410-ad68-8dceda47f409

--- src/absorption.cc
+++ src/absorption.cc
@@ -819,7 +819,8 @@ void xsec_species( MatrixView               xsec_attenuation,
                    const SpeciesAuxData&    partition_functions,
                    const Verbosity&         verbosity )
 {
-    
+    CREATE_OUT3;
+
     extern const Numeric BOLTZMAN_CONST;
     extern const Numeric AVOGADROS_NUMB;
     extern const Numeric SPEED_OF_LIGHT;
@@ -1124,11 +1125,16 @@ firstprivate(ls_attenuation, fac, f_local, aux, qt_cache, qref_cache, iso_cache,
                                            l_l.NLTEUpperIndex(),
                                            t_nlte_i);
 
-			// Output line strength data for Theresa Lang to lowest priority output stream.
-			// This can be commented out again, when Theresa no longer needs it.
-			CREATE_OUT3;
-			out3 << "Line strength data (species, frequency, catalogue intensity, partition ratio, Boltzmann factor): "
-			     << l_l.Name() << ", " << l_l.F() << ", " << l_l.I0() << ", " << partition_ratio << ", " << K1*K2 << "\n"; 
+                        // Output line strength data for Theresa Lang to lowest priority output stream.
+                        // This can be commented out again, when Theresa no longer needs it.
+                        if (out3.sufficient_priority())
+                        {
+                            ostringstream os;
+                            os << "Line strength data (species, frequency, catalogue intensity, partition ratio, Boltzmann factor): "
+                               << l_l.Name() << ", " << l_l.F() << ", " << l_l.I0()
+                               << ", " << partition_ratio << ", " << K1 * K2 << "\n";
+                            out3 << os.str();
+                        }
                        
                         // Dopple broadening
                         const Numeric sigma = l_l.F() * doppler_const *sqrt( t_i / l_l.IsotopologueData().Mass());
