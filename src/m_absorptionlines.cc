bf0b3e8a95646f8bfcb9549537509ffb00aa8ad2
Richard Larsson <ric.larsson@gmail.com>
2019-09-11 22:18:30 +0200

    Add missing file

---
+++ src/m_absorptionlines.cc
@@ -0,0 +1,70 @@
+/* Copyright (C) 2019
+ Richard Larsson <larsson@mps.mpg.de>
+
+ 
+ This program is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by the
+ Free Software Foundation; either version 2, or (at your option) any
+ later version.
+
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ USA. */
+
+/** Contains the absorption namespace
+ * @file   m_absorptionlines.cc
+ * @author Richard Larsson
+ * @date   2019-09-11
+ * 
+ * @brief  Contains the user interaction with absorption lines
+ **/
+
+#include "absorptionlines.h"
+#include "file.h"
+
+void ReadHITRAN(ArrayOfAbsorptionLines& abs_lines2,
+                const String& hitran_file,
+                const Numeric& fmax,
+                const Verbosity&)
+{
+  ifstream is;
+  open_input_file(is, hitran_file);
+  
+  std::vector<Absorption::SingleLineExternal> v;
+  v.resize(0);
+  
+  bool go_on = true;
+  Index n = 0;
+  while (go_on) {
+    v.push_back(Absorption::ReadFromHitran2004Stream(is));
+    
+    if(v.back().bad) {
+      v.pop_back();
+      go_on = false;
+    }
+    else if(v.back().line.F0() > fmax) {
+      v.pop_back();
+      go_on = false;
+    }
+    else if(v.back().quantumidentity.Species() < 0 or v.back().quantumidentity.Isotopologue() < 0) {
+      v.pop_back();
+      go_on = true;
+      n++;
+    }
+  }
+  
+  auto x = Absorption::split_list_of_external_lines(v, {}, true);
+  abs_lines2.resize(0);
+  abs_lines2.reserve(x.size());
+  for(auto& lines: x)
+    abs_lines2.push_back(lines);
+  
+}
