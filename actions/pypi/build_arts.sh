2abaeb6892ed3509e7bbce75ce258a42a066ca2c
Oliver Lemke <olemke@gmail.com>
2020-03-13 08:50:33 +0100

    Make mkdir not fail if directory already exists
    Fixes volume mapping in docker for debugging.

--- actions/pypi/build_arts.sh
+++ actions/pypi/build_arts.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Get arts source
-mkdir arts && cd arts && git init .
+mkdir -p arts && cd arts && git init .
 git remote add origin https://github.com/${GITHUB_REPOSITORY}
 git fetch --prune --depth=1 origin
 
 # Build pyarts
-git checkout --force ${GITHUB_REF#refs/heads/}; mkdir build; cd build;
+git checkout --force ${GITHUB_REF#refs/heads/}; mkdir -p build; cd build;
 cmake3 -DENABLE_FORTRAN=1 -DBLAS_blas_LIBRARY=/usr/lib64/atlas/libtatlas.so -DLAPACK_lapack_LIBRARY=/usr/lib64/atlas/libtatlas.so ..
 make -j2 pyarts
 
