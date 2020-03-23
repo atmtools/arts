d53dafb585feef9abb778b6019f6064590fe37f1
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-03-23 12:25:20 +0100

    Removed python version of controlfiles.

--- controlfiles/artscomponents/scatsolvercomp/run_disort.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pyarts
-from pyarts.workspace import Workspace, arts_agenda
-
-ws = Workspace(verbosity=0)
-# DISORT
-ws.DisortCalc(pfct_method="interpolate")
-ws.yCalc(y=ws.y_disort)
