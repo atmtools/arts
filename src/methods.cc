4e1fd3f373b5e20941c9b7169ace4208b30ba446
erikssonpatrick <patrick.eriksson@chalmers.se>
2020-04-20 09:07:48 +0200

    Clarification to previous commit.
    za_restrict=1 should be default as it gives better performance for standard calculations.

--- src/methods.cc
+++ src/methods.cc
@@ -8084,8 +8084,8 @@ void define_md_data_raw() {
       GIN_TYPE("Index", "Index", "Index", "Numeric", "Index"),
       GIN_DEFAULT("1", "1", "0", "0.5", "1"),
       GIN_DESC("Zenith angle interpolation order.",
-               "Flag whether to restric zenith angle interpolation to one"
-               " hemisphere.",
+               "Flag whether to restric zenith angle interpolation to one "
+               "hemisphere.",
                "Flag whether to do zenith angle interpolation in cosine space.",
                "Maximum allowed extrapolation range in zenith angle.",
                "Azimuth angle interpolation order.")));
