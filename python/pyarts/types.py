bd9e030d94021c338ebc50530ec152d9077cd71e
Richard Larsson <ric.larsson@gmail.com>
2021-02-17 14:21:54 +0100

    Remove value-holder classes from python
    These are better served by the pyarts.classes functionality

--- python/pyarts/types.py
+++ python/pyarts/types.py
@@ -13,61 +13,20 @@ from .covariancematrix import (CovarianceMatrix)
 from .scattering import (SingleScatteringData,
                          ScatteringMetaData,
                         )
-from .retrieval import (RetrievalQuantity)
-from .catalogues import (ArrayOfLineRecord,
-                         CIARecord,
-                         GasAbsLookup,
-                         LineMixingRecord,
-                         QuantumIdentifier,
-                         QuantumNumberRecord,
-                         QuantumNumbers,
-                         Sparse,
-                         SpeciesAuxData,
-                         SpeciesTag,
-                         PropagationMatrix,
-                         StokesVector,
-                         Ppath,
-                         GridPos,
+from .catalogues import (Sparse
                          )
-from .internals import (ARTSCAT5,
-                        Rational,
-                        PressureBroadening,
-                        LineMixing,
-                        PartitionFunctions,
-                        )
-from .xsec import (XsecRecord)
 
 __all__ = []
 
 classes = {
-    'ArrayOfLineRecord': ArrayOfLineRecord,
-    'CIARecord': CIARecord,
     'CovarianceMatrix': CovarianceMatrix,
-    'GasAbsLookup': GasAbsLookup,
     'GriddedField1': GriddedField1,
     'GriddedField2': GriddedField2,
     'GriddedField3': GriddedField3,
     'GriddedField4': GriddedField4,
     'GriddedField5': GriddedField5,
     'GriddedField6': GriddedField6,
-    'LineMixingRecord': LineMixingRecord,
-    'QuantumIdentifier': QuantumIdentifier,
-    'QuantumNumberRecord': QuantumNumberRecord,
-    'QuantumNumbers': QuantumNumbers,
-    'RetrievalQuantity': RetrievalQuantity,
     'ScatteringMetaData': ScatteringMetaData,
     'SingleScatteringData': SingleScatteringData,
     'Sparse': Sparse,
-    'SpeciesAuxData': SpeciesAuxData,
-    'SpeciesTag': SpeciesTag,
-    'ARTSCAT5': ARTSCAT5,
-    'Rational': Rational,
-    'PressureBroadening': PressureBroadening,
-    'LineMixing': LineMixing,
-    'PartitionFunctions': PartitionFunctions,
-    'XsecRecord': XsecRecord,
-    'PropagationMatrix': PropagationMatrix,
-    'StokesVector': StokesVector,
-    'Ppath': Ppath,
-    'GridPos': GridPos,
 }
