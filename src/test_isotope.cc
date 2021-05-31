df20a8cb52709a79ff597add0d4cf090a6686b3a
Richard Larsson <ric.larsson@gmail.com>
2021-05-31 14:45:46 +0200

    Make test_isotope.cc more verbose in namespace usage (hopefully satisfying some weird antics of clang with these names)

--- src/test_isotope.cc
+++ src/test_isotope.cc
@@ -1,11 +1,9 @@
 #include "isotopologues.h"
 
 int main() {
-  using namespace Species;
-  
   std::cout << "Test correctness of Species fails here:\n";
-  for (std::size_t i=0; i<Isotopologues.size(); i++) {
-    auto& x = Isotopologues[i];
+  for (std::size_t i=0; i<Species::Isotopologues.size(); i++) {
+    auto& x = Species::Isotopologues[i];
     if (not good_enum(x.spec)) {
       std::cout << "Position: " << i << " has bad values\n";
     }
@@ -14,26 +12,26 @@ int main() {
   std::cout << "\n\nTest correctness of Species short-name conversion fails here:\n";
   for (Index i=0; i<Index(Species::Species::FINAL); i++) {
     auto a = Species::Species(i);
-    auto b = toShortName(a);
-    auto c = fromShortName(b);
+    auto b = Species::toShortName(a);
+    auto c = Species::fromShortName(b);
     if (not good_enum(c) or c not_eq a) {
       std::cout << i << ' ' << a << ' ' << b << ' ' << c << '\n';
     }
   }
   
   std::cout << "\n\nTest of order of Species fails for these pairs:\n";
-  for (std::size_t i=0; i<Isotopologues.size()-1; i++) {
-    auto& a = Isotopologues[i];
-    auto& b = Isotopologues[i+1];
+  for (std::size_t i=0; i<Species::Isotopologues.size()-1; i++) {
+    auto& a = Species::Isotopologues[i];
+    auto& b = Species::Isotopologues[i+1];
     if (std::size_t(a.spec) > std::size_t(b.spec)) {
       std::cout << a.FullName() << ' ' << b.FullName() << '\n';
     }
   }
   
   std::cout << "\n\nTest of order of Isotopologues fails for these pairs:\n";
-  for (std::size_t i=0; i<Isotopologues.size()-1; i++) {
-    auto& a = Isotopologues[i];
-    auto& b = Isotopologues[i+1];
+  for (std::size_t i=0; i<Species::Isotopologues.size()-1; i++) {
+    auto& a = Species::Isotopologues[i];
+    auto& b = Species::Isotopologues[i+1];
     if (a.spec == b.spec) {
       if (a.isotname.compare(b.isotname) >= 0) {
         std::cout << a.FullName() << ' ' << b.FullName() << '\n';
@@ -42,10 +40,10 @@ int main() {
   }
   
   std::cout << "\n\nTest that all Isotopologues (that are explicit isotopes) has a ratio in the builtin IsotopologueRatios fails here:\n";
-  auto iso_rat = isotopologue_ratiosInitFromBuiltin();
+  auto iso_rat = Species::isotopologue_ratiosInitFromBuiltin();
   for (Index i=0; i<iso_rat.maxsize; i++) {
-    if (not is_predefined_model(Isotopologues[i]) and not Isotopologues[i].joker() and nonstd::isnan(iso_rat.data[i])) {
-      std::cout << Isotopologues[i].FullName() << " has no isotopologue ratio by default: " << iso_rat.data[i] << '\n';
+    if (not is_predefined_model(Species::Isotopologues[i]) and not Species::Isotopologues[i].joker() and nonstd::isnan(iso_rat.data[i])) {
+      std::cout << Species::Isotopologues[i].FullName() << " has no isotopologue ratio by default: " << iso_rat.data[i] << '\n';
     }
   }
 }
