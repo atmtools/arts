e3aa1906d98ee0621d134a73c27b02848b26b944
Oliver Lemke <olemke@gmail.com>
2022-03-24 12:32:39 +0100

    Revert "Fix missing array features and test"
    This reverts commit 3f1b1b186ca5352756683262096c4ce7085fb3d7.

--- controlfiles-python/classes/TestMatrix.py
+++ controlfiles-python/classes/TestMatrix.py
@@ -7,11 +7,11 @@ x = cxx.Matrix([[1, 2, 3]])
 test.io(x, delete=True)
 
 y = cxx.Matrix([[1], [2], [3]])
-assert (x @ y).shape == (1, 1)
-assert (y @ x).shape == (3, 3)
+assert (x @ y).shape == [1, 1]
+assert (y @ x).shape == [3, 3]
 
 z = cxx.Vector([1, 2, 3])
-assert ((y @ x) @ z).shape == (3,)
+assert ((y @ x) @ z).shape == [3]
 
 x = cxx.Matrix(np.zeros(shape=(3, 3)))
 assert np.all(np.array(x) == 0)
