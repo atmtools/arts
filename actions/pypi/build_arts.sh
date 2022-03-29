1143d41e9388cfea27a30d259e3b0e6c6553686a
Oliver Lemke <olemke@gmail.com>
2022-03-29 12:49:08 +0200

    Use Python 3.9
    Move pip install from Dockerfile to arts_build.sh.

--- actions/pypi/build_arts.sh
+++ actions/pypi/build_arts.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+PYTHONDIR=/opt/python/cp39-cp39
+if [[ ! -d "${PYTHONDIR}" ]]; then
+    echo "Python directory ${PYTHONDIR} not found"
+    exit 1
+fi
+export PATH=$PYTHONDIR/bin:$PATH
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools wheel auditwheel twine docutils lark-parser matplotlib netCDF4 numpy pytest scipy xarray
+
 # Get arts source
 mkdir -p arts && cd arts && git init .
 git remote add origin https://github.com/${GITHUB_REPOSITORY}
@@ -14,4 +24,6 @@ make -j2 pyarts
 cd python
 python3 setup.py sdist bdist_wheel
 auditwheel repair dist/pyarts*.whl
+ls -lh dist/pyarts*.whl
 python3 -m twine upload wheelhouse/pyarts*.whl -u __token__ -p $INPUT_PYPI_ACCESS
+
