72bbfd8f809dc22fb90a8df26113e8cc1f75f06b
Oliver Lemke <olemke@gmail.com>
2022-03-29 11:38:39 +0200

    Optimize for latest ARTS build changes

--- actions/pypi/build_arts.sh
+++ actions/pypi/build_arts.sh
@@ -7,8 +7,9 @@ git fetch --prune --depth=1 origin
 
 # Build pyarts
 git checkout --force ${GITHUB_REF#refs/heads/}; mkdir -p build; cd build;
-cmake3 -DCMAKE_BUILD_TYPE=Release -DENABLE_FORTRAN=1 -DBLAS_blas_LIBRARY=/usr/lib64/atlas/libtatlas.so -DLAPACK_lapack_LIBRARY=/usr/lib64/atlas/libtatlas.so ..
-make -j2 pyarts
+cmake3 -DCMAKE_BUILD_TYPE=Release -DENABLE_FORTRAN=1 -DBLAS_blas_LIBRARY=/usr/lib64/atlas/libtatlas.so -DNUM_PYARTS_WSM=2 -DNUM_PYARTS_WSV=1 -DNUM_PYARTS_WSC=1 -DNUM_PYARTS_WSG=1 ..
+make -j2 arts
+make -j1 pyarts
 
 # Packaging
 cd python
