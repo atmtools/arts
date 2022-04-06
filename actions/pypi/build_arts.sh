0dafade8007aedfbdb46f924244606b5072b9701
Oliver Lemke <olemke@gmail.com>
2022-04-06 09:48:07 +0200

    Output CMakeCache.txt in wheel build

--- actions/pypi/build_arts.sh
+++ actions/pypi/build_arts.sh
@@ -20,6 +20,9 @@ git fetch --prune --depth=1 origin
 # Build pyarts
 git checkout --force ${GITHUB_REF#refs/heads/}; mkdir -p build; cd build;
 cmake -DCMAKE_PREFIX_PATH=$PYTHONDIR -DCMAKE_BUILD_TYPE=Release -DENABLE_FORTRAN=1 -DBLAS_blas_LIBRARY=/usr/lib64/atlas/libtatlas.so -DNUM_PYARTS_WSM=2 -DNUM_PYARTS_WSV=1 -DNUM_PYARTS_WSC=1 -DNUM_PYARTS_WSG=1 ..
+echo "########## CMakeCache.txt ##########"
+cat CMakeCache.txt
+echo "########## CMakeCache.txt ##########"
 make -j2 arts
 make -j1 pyarts
 echo "########## Check Python version ##########"
