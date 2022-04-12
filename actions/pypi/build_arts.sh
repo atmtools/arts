738403e96f0997341beaf188ed12784bb55b6ea2
Oliver Lemke <olemke@gmail.com>
2022-04-12 12:08:35 +0200

    Revert "Merge pull request #411 from olemke/update-pyarts-wheel-build"
    This reverts commit c577372ef8921862de4a2f7b2f232d8389b0f67d, reversing
    changes made to 77d9a7e029e91cc1fc7ee78332fd14f31307d928.

--- actions/pypi/build_arts.sh
+++ actions/pypi/build_arts.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-PYTHONDIR=/opt/python/$INPUT_PYTHON_VERSION
-echo "PYTHONDIR=$PYTHONDIR"
-if [[ ! -f "${PYTHONDIR}/bin/python3" ]]; then
-    echo "Python directory ${PYTHONDIR} not found"
-    exit 1
-fi
-export PATH=$PYTHONDIR/bin:$PATH
-
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade setuptools wheel auditwheel twine docutils lark-parser matplotlib netCDF4 numpy pytest scipy xarray
-
 # Get arts source
 mkdir -p arts && cd arts && git init .
 git remote add origin https://github.com/${GITHUB_REPOSITORY}
@@ -18,14 +7,11 @@ git fetch --prune --depth=1 origin
 
 # Build pyarts
 git checkout --force ${GITHUB_REF#refs/heads/}; mkdir -p build; cd build;
-cmake3 -DCMAKE_BUILD_TYPE=Release -DENABLE_FORTRAN=1 -DBLAS_blas_LIBRARY=/usr/lib64/atlas/libtatlas.so -DNUM_PYARTS_WSM=2 -DNUM_PYARTS_WSV=1 -DNUM_PYARTS_WSC=1 -DNUM_PYARTS_WSG=1 ..
-make -j2 arts
-make -j1 pyarts
+cmake3 -DCMAKE_BUILD_TYPE=Release -DENABLE_FORTRAN=1 -DBLAS_blas_LIBRARY=/usr/lib64/atlas/libtatlas.so -DLAPACK_lapack_LIBRARY=/usr/lib64/atlas/libtatlas.so ..
+make -j2 pyarts
 
 # Packaging
 cd python
 python3 setup.py sdist bdist_wheel
 auditwheel repair dist/pyarts*.whl
-ls -lh dist/pyarts*.whl
 python3 -m twine upload wheelhouse/pyarts*.whl -u __token__ -p $INPUT_PYPI_ACCESS
-
--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,7 @@ import subprocess
 import shutil
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages, Distribution
+from setuptools import setup, find_packages
 
 # To use a consistent encoding
 from codecs import open
@@ -44,11 +44,6 @@ try:
     if not found: raise
 except:
     raise Exception("Cannot find builtin library")
-
-class BinaryDistribution(Distribution):
-    """Distribution which always forces a binary package with platform name"""
-    def has_ext_modules(foo):
-        return True
 
 setup(
     name="pyarts",
@@ -84,5 +79,4 @@ setup(
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     include_package_data=True,
-    distclass=BinaryDistribution,
 )
