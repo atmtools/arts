e5829aa812491f998716f1ba8e86a7d570856a93
Richard Larsson <ric.larsson@gmail.com>
2022-04-22 09:47:16 +0200

    make py_scattering pickleable

--- src/python_interface/py_scattering.cpp
+++ src/python_interface/py_scattering.cpp
@@ -8,7 +8,16 @@ void py_scattering(py::module_& m) {
       .value("PTYPE_GENERAL", PType::PTYPE_GENERAL)
       .value("PTYPE_AZIMUTH_RND", PType::PTYPE_AZIMUTH_RND)
       .value("PTYPE_TOTAL_RND", PType::PTYPE_TOTAL_RND)
-      .PythonInterfaceCopyValue(PType);
+      .PythonInterfaceCopyValue(PType)
+      .def(py::pickle(
+          [](const PType& self) {
+            return py::make_tuple(static_cast<Index>(self));
+          },
+          [](const py::tuple& t) {
+            ARTS_USER_ERROR_IF(t.size() != 1, "Invalid state!")
+            
+            return static_cast<PType>(t[0].cast<Index>());
+          }));
 
   py::class_<SingleScatteringData>(m, "SingleScatteringData")
       .def(py::init([]() { return new SingleScatteringData{}; }))
@@ -24,7 +33,32 @@ void py_scattering(py::module_& m) {
       .def_readwrite("aa_grid", &SingleScatteringData::aa_grid)
       .def_readwrite("pha_mat_data", &SingleScatteringData::pha_mat_data)
       .def_readwrite("ext_mat_data", &SingleScatteringData::ext_mat_data)
-      .def_readwrite("abs_vec_data", &SingleScatteringData::abs_vec_data);
+      .def_readwrite("abs_vec_data", &SingleScatteringData::abs_vec_data)
+      .def(py::pickle(
+          [](const SingleScatteringData& self) {
+            return py::make_tuple(self.ptype,
+                                  self.description,
+                                  self.f_grid,
+                                  self.T_grid,
+                                  self.za_grid,
+                                  self.aa_grid,
+                                  self.pha_mat_data,
+                                  self.ext_mat_data,
+                                  self.abs_vec_data);
+          },
+          [](const py::tuple& t) {
+            ARTS_USER_ERROR_IF(t.size() != 9, "Invalid state!")
+
+            return new SingleScatteringData{t[0].cast<PType>(),
+                                            t[1].cast<String>(),
+                                            t[2].cast<Vector>(),
+                                            t[3].cast<Vector>(),
+                                            t[4].cast<Vector>(),
+                                            t[5].cast<Vector>(),
+                                            t[6].cast<Tensor7>(),
+                                            t[7].cast<Tensor5>(),
+                                            t[8].cast<Tensor5>()};
+          }));
 
   py::class_<ScatteringMetaData>(m, "ScatteringMetaData")
       .def(py::init([]() { return new ScatteringMetaData{}; }))
@@ -40,7 +74,28 @@ void py_scattering(py::module_& m) {
       .def_readwrite("diameter_volume_equ",
                      &ScatteringMetaData::diameter_volume_equ)
       .def_readwrite("diameter_area_equ_aerodynamical",
-                     &ScatteringMetaData::diameter_area_equ_aerodynamical);
+                     &ScatteringMetaData::diameter_area_equ_aerodynamical)
+      .def(py::pickle(
+          [](const ScatteringMetaData& self) {
+            return py::make_tuple(self.description,
+                                  self.source,
+                                  self.refr_index,
+                                  self.mass,
+                                  self.diameter_max,
+                                  self.diameter_volume_equ,
+                                  self.diameter_area_equ_aerodynamical);
+          },
+          [](const py::tuple& t) {
+            ARTS_USER_ERROR_IF(t.size() != 9, "Invalid state!")
+
+            return new ScatteringMetaData{t[0].cast<String>(),
+                                          t[1].cast<String>(),
+                                          t[2].cast<String>(),
+                                          t[3].cast<Numeric>(),
+                                          t[4].cast<Numeric>(),
+                                          t[5].cast<Numeric>(),
+                                          t[6].cast<Numeric>()};
+          }));
 
   PythonInterfaceWorkspaceArray(ScatteringMetaData);
   PythonInterfaceWorkspaceArray(SingleScatteringData);
