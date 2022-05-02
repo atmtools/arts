958f7ab95a176751d808c7fc1e49479973163e67
Richard Larsson <ric.larsson@gmail.com>
2022-05-02 16:25:59 +0200

    Make convert a submodule

--- src/python_interface/py_constants.cpp
+++ src/python_interface/py_constants.cpp
@@ -6,31 +6,38 @@ namespace Python {
 namespace py = pybind11;
 
 struct ConstantDummy {};
-struct ConversionDummy {};
 
 #define PythonInterfaceConstant(name) \
-  def_readonly_static(                \
-      #name, &Constant::name, py::doc("Value of " #name " in Arts"))
+  constant.def_readonly_static(       \
+      #name, &Constant::name, "Value of " #name " in Arts")
 
-#define PythonInterfaceConvert(name, t, from, to) \
-  def_static(#name,                               \
-             py::vectorize(&Conversion::name<t>), \
-             py::doc("Converts from " from " to " to))
+//! Wraps a conversion function with basic information
+#define PythonInterfaceConvert(name, t, from, to)  \
+  convert.def(#name,                               \
+              py::vectorize(&Conversion::name<t>), \
+              py::doc("Converts from " from " to " to))
 
 void py_constants(py::module_& m) {
-  py::class_<ConstantDummy>(m, "constant")
-      .PythonInterfaceConstant(pi)
-      .PythonInterfaceConstant(c)
-      .PythonInterfaceConstant(h)
-      .PythonInterfaceConstant(h_bar)
-      .PythonInterfaceConstant(k);
+  auto constant = py::class_<ConstantDummy>(m, "constant");
+  constant.doc() = R"--(Contains constant internal values of Arts
 
-  py::class_<ConversionDummy>(m, "convert")
-      .PythonInterfaceConvert(
-          freq2kaycm, Numeric, "Frequency [Hz]", "Kayser [cm-1]")
-      .PythonInterfaceConvert(
-          kaycm2freq, Numeric, "Kayser [cm-1]", "Frequency [Hz]")
-      .PythonInterfaceConvert(pa2torr, Numeric, "Pressure [Pa]", "Torr [Torr]")
-      .PythonInterfaceConvert(torr2pa, Numeric, "Torr [Torr]", "Pressure [Pa]");
+Note that this "class" simply contains static data and that it should NOT
+be initialized
+)--";
+  PythonInterfaceConstant(pi);
+  PythonInterfaceConstant(c);
+  PythonInterfaceConstant(h);
+  PythonInterfaceConstant(h_bar);
+  PythonInterfaceConstant(k);
+
+  auto convert = m.def_submodule("convert");
+  convert.doc() = R"--(Contains several unit conversion functions used in Arts
+
+These all should work with any array-like type
+)--";
+  PythonInterfaceConvert(freq2kaycm, Numeric, "Freq. [Hz]", "Kayser [cm-1]");
+  PythonInterfaceConvert(kaycm2freq, Numeric, "Kayser [cm-1]", "Freq. [Hz]");
+  PythonInterfaceConvert(pa2torr, Numeric, "Pressure [Pa]", "Torr [Torr]");
+  PythonInterfaceConvert(torr2pa, Numeric, "Torr [Torr]", "Pressure [Pa]");
 }
 }  // namespace Python
