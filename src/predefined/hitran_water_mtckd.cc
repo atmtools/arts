31d7fc16c8165942df66415e70e25746daa8e2d1
Richard Larsson <ric.larsson@gmail.com>
2022-05-10 14:14:43 +0200

    Copy copyright statement verbatim (keeping formatting error)

--- src/predefined/hitran_water_mtckd.cc
+++ src/predefined/hitran_water_mtckd.cc
@@ -11,6 +11,27 @@
 
 #include "predef_data.h"
 
+/*!  This code is ported from Fortran90 code that contains the following statement:
+
+!  --------------------------------------------------------------------------
+! |  Copyright �, Atmospheric and Environmental Research, Inc., 2022         |
+! |                                                                          |
+! |  All rights reserved. This source code was developed as part of the      |
+! |  LBLRTM software and is designed for scientific and research purposes.   |
+! |  Atmospheric and Environmental Research Inc. (AER) grants USER the right |
+! |  to download, install, use and copy this software for scientific and     |
+! |  research purposes only. This software may be redistributed as long as   |
+! |  this copyright notice is reproduced on any copy made and appropriate    |
+! |  acknowledgment is given to AER. This software or any modified version   |
+! |  of this software may not be incorporated into proprietary software or   |
+! |  commercial software offered for sale without the express written        |
+! |  consent of AER.                                                         |
+! |                                                                          |
+! |  This software is provided as is without any express or implied          |
+! |  warranties.                                                             |
+!  --------------------------------------------------------------------------
+*/
+
 namespace Absorption::PredefinedModel::Hitran::MTCKD {
 Numeric RADFN_FUN(const Numeric XVI, const Numeric XKT) noexcept {
   // ---------------------------------------------------------------------- B18060
