ae42f7cd7c8c4abd435533d2b517bce2d277962a
Manfred Brath <manfred.brath@uni-hamburg.de>
2022-05-19 16:42:16 +0200

    Add WSM for refractive index of wtare for visible and NIR
    * Add WSM refr_index_waterVisibleNIR.

--- src/refraction.h
+++ src/refraction.h
@@ -134,4 +134,31 @@ void refr_gradients_3d(Workspace& ws,
                        const Numeric& lat,
                        const Numeric& lon);
 
+/** Refractive index of water and steam for the optical and near infrared \n
+ *
+ * From:
+ *  Revised formulation for the Refractive
+ *  Index of Water and Steam as a Function of
+ *  Wavelength, Temperature and Density
+ *  \n
+ *  Journal of Physical and Chemical Reference Data 27, 761 (1998);
+ *  https://doi.org/10.1063/1.556029 27, 761
+ *  \n
+ *  see also
+ *  http://www.iapws.org/release.html
+ *
+ * @param[out]n refractive index
+ * @param[in] only_valid_range flag if true refractive index is calculated only
+ *                             within range of validity. If false no check is made,
+ *                             so you at your own risk.
+ * @param[in] frequency frequency
+ * @param[in] temperature temperature
+ * @param[in] density density of water or steam
+  */
+void refractive_index_water_and_steam_VisNIR(Numeric& n,
+                                             const Index& only_valid_range,
+                                             const Numeric& frequency,
+                                             const Numeric& temperature,
+                                             const Numeric& density);
+
 #endif  // refraction_h
