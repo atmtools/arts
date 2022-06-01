9ff917477c50907b31277f7d6f596d7e72a3b122
Oliver Lemke <olemke@gmail.com>
2022-06-01 09:54:53 +0200

    Fix imfilebrowser for clang 14

--- 3rdparty/gui/imgui-filebrowser/imfilebrowser.h
+++ 3rdparty/gui/imgui-filebrowser/imfilebrowser.h
@@ -324,7 +324,7 @@ inline void ImGui::FileBrowser::Display()
 #endif
 
     int secIdx = 0, newPwdLastSecIdx = -1;
-    for(auto &sec : pwd_)
+    for(auto &&sec : pwd_)
     {
 #ifdef _WIN32
         if(secIdx == 1)
@@ -346,7 +346,7 @@ inline void ImGui::FileBrowser::Display()
     {
         int i = 0;
         std::filesystem::path newPwd;
-        for(auto &sec : pwd_)
+        for(auto &&sec : pwd_)
         {
             if(i++ > newPwdLastSecIdx)
                 break;
