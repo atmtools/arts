3040e42a6bdc3de3bef324eda03cf9a73210fe7b
Richard Larsson <ric.larsson@gmail.com>
2022-06-10 15:42:53 +0200

    Remove cutoff

--- examples/classroom/vibrational-spectra/start_gui.py
+++ examples/classroom/vibrational-spectra/start_gui.py
@@ -12,12 +12,8 @@ ws.abs_speciesSet(
              "O3",
              "H2O",
              "CO2",
-             "O2",
              "CH4"])
 ws.abs_lines_per_speciesReadSpeciesSplitCatalog(basename="lines/")
-
-# Speed up by limiting the frequency range of lines
-ws.abs_lines_per_speciesSetCutoff(option="ByLine", value=750e9)
 
 # Use an automatic agenda
 ws.propmat_clearsky_agendaSetAutomatic()
@@ -37,10 +33,10 @@ ws.rtp_pressure = 110000
 ws.rtp_temperature = 2.942000e+02
 ws.f_grid = pyarts.arts.convert.kaycm2freq(np.linspace(300, 3000, 1000))
 ws.rtp_vmr = [1.000869e-09, 1.000869e-14, 2.850472e-06, 1.501303e-07,
-              3.019448e-08, 3.302947e-04, 1.877431e-02, 2.091960e-01, 1.701397e-06]
+              3.019448e-08, 3.302947e-04, 1.877431e-02, 1.701397e-06]
 
 # Check that the calculations are OK
-ws.lbl_checked = 1
+ws.lbl_checkedCalc()
 
 # Start the explorer
 ws.propmat_clearsky_agendaGUI()
