5c2218c9452377eb1c86a5127a7668db596b8f44
Richard Larsson <ric.larsson@gmail.com>
2022-07-05 14:31:41 +0200

    less fancy

--- src/make_tokval_h.cc
+++ src/make_tokval_h.cc
@@ -97,7 +97,8 @@ TokVal::TokVal(const TokVal& v) { std::visit([&](auto&& in) {*this = *in;}, v.va
 TokVal& TokVal::operator=(const TokVal& v) { std::visit([&](auto&& in) {*this = *in;}, v.value); return *this; }
 
 std::ostream& operator<<(std::ostream& os, const TokVal& t) {
-  return std::visit([&](auto&& val){return os << *val;}, t.value);
+  return std::visit([&](auto&& val){os << *val;}, t.value);
+  return os;
 }
 )--";
 
