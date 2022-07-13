09805dc17674b76531499d4da27395f332e216a7
Richard Larsson <ric.larsson@gmail.com>
2022-07-13 16:19:22 +0200

    Make transforming from Hitran to ARTS CIA easy

---
+++ src/python_interface/py_physics.cpp
@@ -0,0 +1,26 @@
+#include <physics_funcs.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+namespace Python {
+namespace py = pybind11;
+
+void py_physics(py::module_& m) {
+  auto physics = m.def_submodule("physics");
+  physics.doc() = R"--(Contains simple physics functions in arts
+)--";
+
+  physics.def("number_density",
+              py::vectorize(&number_density),
+              py::doc(R"--(Calculates the atmospheric number density.
+
+Parameters:
+-----------
+  P: Pressure [Pa]
+
+  T: Temperature [K]
+)--"),
+              py::arg("P"),
+              py::arg("T"));
+}
+}  // namespace Python
