1bbbed29b6703f5fa28c83d8145d7dadc03070a1
Richard Larsson <ric.larsson@gmail.com>
2022-09-08 13:48:52 +0200

    remove extras

--- src/matpack_eigen.h
+++ src/matpack_eigen.h
@@ -121,9 +121,6 @@ auto row_vec(standard_vector auto&& x) {
 
   return matrix_map(x.data(), x.size(), 1, stride_type(1, 1));
 }
-
-//! Can be converted to a eigen matrix base
-template <typename T, typename U> concept matrix_base = std::is_convertible_v<T, Eigen::MatrixBase<U>>;
 }  // namespace matpack::eigen
 
 auto operator*(matpack::matrix auto&& A, matpack::vector auto&& x) {
