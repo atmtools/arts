9f4d44084e58092782602373908d35d97be2e88b
Patrick Eriksson <patrick.eriksson@chalmers.se>
2022-09-30 23:10:17 +0200

    New ppath: come cleaning, work in progress

--- src/ppath_struct.cc
+++ src/ppath_struct.cc
@@ -13,21 +13,21 @@
 std::ostream& operator<<(std::ostream& os, const Ppath& x) {
   os << "dim: " << x.dim << "\n";
   os << "np: " << x.np << "\n";
-  os << "constant: " << x.constant << "\n";
-  os << "background: " << x.backgroundZZZ << "\n";
+  //os << "constant: " << x.constant << "\n";   // ZZZ
+  os << "background: " << x.backgroundZZZ << "\n";  // ZZZ
   os << "start_pos: " << x.start_pos << "\n";
   os << "start_los: " << x.start_los << "\n";
   os << "start_lstep: " << x.start_lstep << "\n";
   os << "pos: " << x.pos << "\n";
   os << "los: " << x.los << "\n";
-  os << "r: " << x.r << "\n";
+  // os << "r: " << x.r << "\n";   // ZZZ
   os << "lstep: " << x.lstep << "\n";
   os << "end_pos: " << x.end_pos << "\n";
   os << "end_los: " << x.end_los << "\n";
   os << "end_lstep: " << x.end_lstep << "\n";
   os << "nreal: " << x.nreal << "\n";
   os << "ngroup: " << x.ngroup << "\n";
-  os << "gp_z: " << x.gp_p << "\n";
+  os << "gp_z: " << x.gp_p << "\n";   // ZZZ
   if (x.dim >= 2) {
     os << "gp_lat: " << x.gp_lat << "\n";
   }
