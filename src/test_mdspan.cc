38da66752997eb031333811b71dcb0b9514f87d3
Richard Larsson <richard.larsson@uni-hamburg.de>
2022-12-14 16:47:03 +0100

    Some order

--- src/test_mdspan.cc
+++ src/test_mdspan.cc
@@ -10,7 +10,7 @@ std::string demangle(const char *name) {
 
   // enable c++11 by passing the flag -std=c++11 to g++
   std::unique_ptr<char, void (*)(void *)> res{
-      abi::__cxa_demangle(name, NULL, NULL, &status), std::free};
+      abi::__cxa_demangle(name, nullptr, nullptr, &status), std::free};
 
   return (status == 0) ? res.get() : name;
 }
