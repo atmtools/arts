e7ca6905c7eaa03311e1932877a601af5becb402
Richard Larsson <richard.larsson@uni-hamburg.de>
2022-12-14 16:26:03 +0100

    Fix clang and add some conversion operators

--- src/test_mdspan.cc
+++ src/test_mdspan.cc
@@ -24,7 +24,7 @@ int main() {
 
   auto x = TMPVector(4);
   const auto y = TMPMatrix(4, 3, 4);
-  auto yc = TMPMatrix(4, 3, 4);
+  auto yc = TMPMatrix();
   auto z = TMPTensor3(4, 3, 2, 4);
 
   z(1, 2, 1) += 1;
@@ -59,4 +59,61 @@ int main() {
 
   z = simple_view<double, 3, true>{std::move(yc).reshape(4,3,2)};
   std::cout << z << '\n';
+
+  {
+    Vec tmp(3, 0);
+    auto a = VecVF{tmp};
+    auto b = VecVS{a};
+    auto c = VecVS{tmp};
+    const VecCVF d{a};
+    const VecCVS e{b};
+    const VecCVS f{c};
+    const VecVF g{a};
+    const VecVS h{b};
+    const VecVS i{c};
+tmp[0]+=1;
+    std::cout << type(tmp) << ' ' << (tmp[0]+=1) << '\n';
+    std::cout << type(a) << ' ' << (a[0]+=1) << '\n';
+    std::cout << type(b) << ' ' << (b[0]+=1) << '\n';
+    std::cout << type(c) << ' ' << (c[0]+=1) << '\n';
+    std::cout << type(d) << '\n';
+    std::cout << type(e) << '\n';
+    std::cout << type(f) << '\n';
+    std::cout << type(g) << '\n';
+    std::cout << type(h) << '\n';
+    std::cout << type(i) << '\n';
+  }
+  {
+    const Vec tmp;
+    auto a = VecCVF{tmp};
+    auto b = VecCVS{a};
+    auto c = VecCVS{tmp};
+
+    std::cout << type(tmp) << '\n';
+    std::cout << type(a) << '\n';
+    std::cout << type(b) << '\n';
+    std::cout << type(c) << '\n';
+  }
+  {
+    Mat tmp;
+    auto a = MatVF{tmp};
+    auto b = MatVS{a};
+    auto c = MatVS{tmp};
+
+    std::cout << type(tmp) << '\n';
+    std::cout << type(a) << '\n';
+    std::cout << type(b) << '\n';
+    std::cout << type(c) << '\n';
+  }
+  {
+    const Mat tmp;
+    auto a = MatCVF{tmp};
+    auto b = MatCVS{a};
+    auto c = MatCVS{tmp};
+
+    std::cout << type(tmp) << '\n';
+    std::cout << type(a) << '\n';
+    std::cout << type(b) << '\n';
+    std::cout << type(c) << '\n';
+  }
 }
