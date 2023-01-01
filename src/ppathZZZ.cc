1f469bcf67867484c755a86a76957ad50e6ccac3
Patrick Eriksson <patrick.eriksson@chalmers.se>
2023-01-01 09:04:37 +0100

    Restart of new ppath
    Mainly reorganisation to have things more clearly organised

--- src/ppathZZZ.cc
+++ src/ppathZZZ.cc
@@ -29,7 +29,6 @@
   === External declarations
   ===========================================================================*/
 
-#include "ppathZZZ.h"
 
 #include <algorithm>
 
@@ -37,6 +36,8 @@
 #include "interpolation.h"
 #include "jacobian.h"
 #include "math_funcs.h"
+#include "ppathZZZ.h"
+#include "variousZZZ.h"
 
 using GriddedFieldGrids::GFIELD2_LAT_GRID;
 using GriddedFieldGrids::GFIELD2_LON_GRID;
@@ -618,145 +619,6 @@ Numeric find_crossing_cloudbox(const Vector rte_pos,
   }
 
   return l2cbox;
-}
-
-Numeric find_crossing_with_surface_z(const Vector rte_pos,
-                                     const Vector rte_los,
-                                     const Vector ecef,
-                                     const Vector decef,
-                                     const Index& atmosphere_dim,
-                                     const Vector& refellipsoid,
-                                     const GriddedField2& surface_elevation,
-                                     const Numeric& l_accuracy,
-                                     const Index& safe_search) {
-  // Find min and max surface altitude
-  Numeric z_min, z_max;
-  if (atmosphere_dim == 1) {
-    z_min = surface_elevation.data(0, 0);
-    z_max = surface_elevation.data(0, 0);
-  } else {
-    z_min = min(surface_elevation.data);
-    z_max = max(surface_elevation.data);
-  }
-
-  // Catch upward looking cases that can not have a surface intersection
-  if (rte_pos[0] >= z_max && rte_los[0] <= 90) {
-    return -1;
-  }
-
-  // Check that observation position is above ground
-  if (rte_pos[0] < z_max) {
-    Numeric z_surf =
-        surface_z_at_pos(rte_pos, atmosphere_dim, surface_elevation);
-    if (rte_pos[0] < z_surf - l_accuracy)
-      ARTS_USER_ERROR(
-          "The sensor is below the surface. Not allowed!\n"
-          "The sensor altitude is at ",
-          rte_pos[0],
-          " m\n"
-          "The surface altitude is ",
-          z_surf,
-          " m\n"
-          "The position is (lat,lon): (",
-          rte_pos[1],
-          ",",
-          rte_pos[2],
-          ")");
-  }
-
-  // Constant surface altitude (in comparison to *l_accuracy*)
-  if (atmosphere_dim == 1 || z_max - z_min < l_accuracy / 100) {
-    // Catch cases with position on the ground, as they can fail if
-    // intersection_altitude is used
-    if (rte_pos[0] <= z_max) {
-      return 0.0;
-    } else {
-      return intersection_altitude(ecef, decef, refellipsoid, z_min);
-    }
-
-    // The general case
-  } else {
-    // Find max distance that is guaranteed above or at surface
-    // This is the minimum distance to the surface
-    // If below z_max, this distance is 0. Otherwise given by z_max
-    Numeric l_min;
-    if (rte_pos[0] <= z_max)
-      l_min = 0;
-    else {
-      l_min = intersection_altitude(ecef, decef, refellipsoid, z_max);
-      // No intersection if not even z_max is reached
-      if (l_min < 0) return -1;
-    }
-    // Find max distance for search.
-    // If below z_max and upward, given by z_max
-    // Otherwise in general given by z_min. If z_min not reached, the distance
-    // is instead given by tangent point
-    Numeric l_max;
-    bool l_max_could_be_above_surface = false;
-    if (rte_pos[0] <= z_max && rte_los[0] <= 90) {
-      l_max = intersection_altitude(ecef, decef, refellipsoid, z_max);
-      l_max_could_be_above_surface = true;
-    } else {
-      l_max = intersection_altitude(ecef, decef, refellipsoid, z_min);
-    }
-    if (l_max < 0) {
-      Vector ecef_tan(3);
-      approx_geometrical_tangent_point(ecef_tan, ecef, decef, refellipsoid);
-      l_max = ecef_distance(ecef, ecef_tan);
-      // To not miss intersections just after the tangent point, we add a
-      // a distance that depends om planet radius (for Earth 111 km).
-      l_max += refellipsoid[0] * sin(DEG2RAD);
-      l_max_could_be_above_surface = true;
-    }
-
-    // Safe but slow approach
-    // ----------------------
-    if (safe_search) {
-      Numeric l_test =
-          l_min - l_accuracy / 2;  // Remove l/2 to get exact result
-      bool above_surface = true;   // if true l_test is 0
-      while (above_surface && l_test < l_max) {
-        l_test += l_accuracy;
-        Vector pos(3);
-        pos_at_distance(pos, ecef, decef, refellipsoid, l_test);
-        Numeric z_surf =
-            surface_z_at_pos(pos, atmosphere_dim, surface_elevation);
-        if (pos[0] < z_surf) above_surface = false;
-      }
-      if (above_surface) {
-        return -1;
-      } else {
-        return l_test - l_accuracy / 2;
-      }
-
-      // Bisection search
-      // ----------------------
-    } else {
-      // If l_max matches a point above the surface, we have no intersection
-      // according to this search algorithm. And the search fails. So we need
-      // to check that point if status unclear
-      if (l_max_could_be_above_surface) {
-        Vector pos(3);
-        pos_at_distance(pos, ecef, decef, refellipsoid, l_max);
-        Numeric z_surf =
-            surface_z_at_pos(pos, atmosphere_dim, surface_elevation);
-        if (pos[0] > z_surf) return -1;
-      }
-      // Start bisection
-      while (l_max - l_min > 2 * l_accuracy) {
-        const Numeric l_test = (l_min + l_max) / 2;
-        Vector pos(3);
-        pos_at_distance(pos, ecef, decef, refellipsoid, l_test);
-        Numeric z_surf =
-            surface_z_at_pos(pos, atmosphere_dim, surface_elevation);
-        if (pos[0] >= z_surf)
-          l_min = l_test;
-        else
-          l_max = l_test;
-      }
-      return (l_min + l_max) / 2;
-    }
-  }
 }
 
 Index is_pos_outside_atmosphere(const Vector pos,
