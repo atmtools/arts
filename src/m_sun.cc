1e192fb5a9f7cbe69fbe36828771aafdb5dbfbcf
jpetersen <jon.petersen@web.de>
2023-01-06 16:29:48 +0100

    method to defince spectrum at given location
    ToDo: the location is still definde in the global coordinate system and not in the local

--- src/m_sun.cc
+++ src/m_sun.cc
@@ -25,6 +25,7 @@
 #include "physics_funcs.h"
 #include "arts.h"
 #include "auto_md.h"
+#include "geodetic.h"
 #include "sun.h"
 
 
@@ -111,6 +112,67 @@ void sunsAddSingleFromGrid(ArrayOfSun &suns,
   Sun& new_sun = suns.emplace_back();
 
   new_sun.spectrum = int_data; // set spectrum
+
+  new_sun.description = description;
+  new_sun.radius = radius;
+  new_sun.distance = distance;
+  new_sun.latitude = latitude;
+  new_sun.longitude = longitude;
+
+  // set flag
+  suns_do = 1;
+
+}
+
+/* Workspace method: Doxygen documentation will be auto-generated */
+void sunsAddSingleFromGridAtLocation(
+                         ArrayOfSun &suns,
+                         Index &suns_do,
+                         // Inputs:
+                         const Vector &f_grid,
+                         const Index &stokes_dim,
+                         const Vector &refellipsoid,
+                         const GriddedField2 &sun_spectrum_raw,
+                         const Numeric &radius,
+                         const Numeric &distance,
+                         const Numeric &temperature,
+                         const Numeric &latitude,
+                         const Numeric &longitude,
+                         const String &description,
+                         const Numeric &location_latitude,
+                         const Numeric &location_longitude,
+                         const Numeric &location_altitude,
+                         const Verbosity &verbosity) {
+
+  // some sanity checks
+  ARTS_USER_ERROR_IF (distance<radius,
+                      "The distance to the center of the sun (",distance," m) \n"
+                      "is smaller than the radius of the sun (", radius," m )")
+  ARTS_USER_ERROR_IF (location_altitude<0.,
+                      "The altitude of the solar spectrum should be positiv,\n"
+                      "but is ",location_altitude," m) ")
+
+  Numeric toa_altitude = location_altitude + refell2r(refellipsoid, location_latitude);
+
+  Numeric distance_sun_loc;
+  distance3D(distance_sun_loc,
+              toa_altitude,
+              location_latitude,
+              location_longitude,
+              distance,
+              latitude,
+              longitude);
+
+  // Geometric scaling factor, scales the sun spectral irradiance at the given
+  // location to the spectral irradiance of the suns surface.
+  Numeric scale_factor = (radius*radius + distance_sun_loc*distance_sun_loc)/
+                         (radius*radius);
+
+  // create and init sun
+  Sun& new_sun = suns.emplace_back();
+
+  new_sun.spectrum = regrid_sun_spectrum(sun_spectrum_raw, f_grid, stokes_dim, temperature, verbosity);
+  new_sun.spectrum *= scale_factor; // scale to sun surface
 
   new_sun.description = description;
   new_sun.radius = radius;
