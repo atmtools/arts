f98193952e124954d7792610b249bc7a8da211e5
Patrick Eriksson <patrick.eriksson@chalmers.se>
2023-01-07 08:19:33 +0100

    Pppath option include_specular_ppath introduced and tested
    But surface topography not yet considered
    Polishing of documentation

--- src/variousZZZ.h
+++ src/variousZZZ.h
@@ -30,7 +30,7 @@
 
     @author  Patrick Eriksson 
     @date    2022-12-31
- */
+*/
 void chk_rte_los(const String& name,
                  ConstVectorView los);
 
@@ -43,7 +43,7 @@ void chk_rte_los(const String& name,
 
     @author  Patrick Eriksson 
     @date    2022-12-31
- */
+*/
 void chk_rte_pos(const String& name,
                  ConstVectorView pos);
 
@@ -53,7 +53,7 @@ void chk_rte_pos(const String& name,
 
     @author  Patrick Eriksson 
     @date    2021-07-30
- */
+*/
 void chk_refellipsoidZZZ(ConstVectorView refellipsoid);
 
 /** Checks if a matrix is a valid sensor_pos
@@ -63,7 +63,7 @@ void chk_refellipsoidZZZ(ConstVectorView refellipsoid);
 
     @author  Patrick Eriksson 
     @date    2021-07-30
- */
+*/
 void chk_sensor_pos(const String& name,
                     ConstMatrixView sensor_pos);
 
@@ -75,7 +75,7 @@ void chk_sensor_pos(const String& name,
 
     @author  Patrick Eriksson 
     @date    2021-07-30
- */
+*/
 void chk_sensor_los(const String& name,
                     ConstMatrixView sensor_los);
 
@@ -88,7 +88,7 @@ void chk_sensor_los(const String& name,
 
     @author  Patrick Eriksson 
     @date    2021-07-30
- */
+*/
 void chk_sensor_poslos(const String& name1,
                        ConstMatrixView sensor_pos,
                        const String& name2,
@@ -103,31 +103,31 @@ void chk_sensor_poslos(const String& name1,
 
     @author  Patrick Eriksson 
     @date    2021-08-08
- */
+*/
 void chk_surface_elevation(const GriddedField2& surface_elevation);
 
 /** Calculates the geometrical length to the surface
 
-   A negative length is returned if the geomtrical path has no intersection
-   with the surface.
+    A negative length is returned if the geomtrical path has no intersection
+    with the surface.
 
-   The function also check that the observation position is actually above the
-   surface. 
+    The function also check that the observation position is actually above the
+    surface. 
 
-   @param[in]  rte_pos       As the WSV with the same name.
-   @param[in]  rte_los       As the WSV with the same name.
-   @param[in]  ecef          rte_pos in ECEF.
-   @param[in]  decef         rte_los in ECEF.
-   @param[in]  refellipsoid             As the WSV with same name.
-   @param[in]  surface_elevation        As the WSV with same name.
-   @param[in]  surface_search_accuracy  See WSM IntersectionGeometricalWithSurface.
-               surface_search_safe      See WSM IntersectionGeometricalWithSurface.       
+    @param[in]  rte_pos       As the WSV with the same name.
+    @param[in]  rte_los       As the WSV with the same name.
+    @param[in]  ecef          rte_pos in ECEF.
+    @param[in]  decef         rte_los in ECEF.
+    @param[in]  refellipsoid             As the WSV with same name.
+    @param[in]  surface_elevation        As the WSV with same name.
+    @param[in]  surface_search_accuracy  See WSM IntersectionGeometricalWithSurface.
+                surface_search_safe      See WSM IntersectionGeometricalWithSurface.
 
-   @return  Length to the surface.
+    @return  Length to the surface.
 
-   @author  Patrick Eriksson
-   @date    2021-08-06
- */
+    @author  Patrick Eriksson
+    @date    2021-08-06
+*/
 Numeric find_crossing_with_surface_z(const Vector rte_pos,
                                      const Vector rte_los,
                                      const Vector ecef,
@@ -148,14 +148,14 @@ Numeric find_crossing_with_surface_z(const Vector rte_pos,
     This is handled automatically, but longitude can only be the
     last (column) dimension.
 
-   @param[in]  G    Gridded field to interpolate
-   @param[in]  pos  Position vector, length 2
+    @param[in]  G    Gridded field to interpolate
+    @param[in]  pos  Position vector, length 2
 
-   @return  Result of interpolation
+    @return  Result of interpolation
 
-   @author Patrick Eriksson
-   @date   2023-01-03
- */
+    @author Patrick Eriksson
+    @date   2023-01-03
+*/
 Numeric interp_gfield2(const GriddedField2& G,
                        const Vector& pos);
 
@@ -170,14 +170,14 @@ Numeric interp_gfield2(const GriddedField2& G,
     This is handled automatically, but longitude can only be the
     last (column) dimension.
 
-   @param[in]  G    Gridded field to interpolate
-   @param[in]  pos  Position vector, length 3
+    @param[in]  G    Gridded field to interpolate
+    @param[in]  pos  Position vector, length 3
 
-   @return  Result of interpolation
+    @return  Result of interpolation
 
-   @author Patrick Eriksson
-   @date   2023-01-03
- */
+    @author Patrick Eriksson
+    @date   2023-01-03
+*/
 Numeric interp_gfield3(const GriddedField3& G,
                        const Vector& pos);
 
@@ -195,20 +195,20 @@ Numeric interp_gfield3(const GriddedField3& G,
     values. With *do_twosided_perturb* set to true, there is also a
     perturbation in the negative direction.
 
-   @param[out]  refr_index_air           As the WSV with same name
-   @param[out]  refr_index_air_group     As the WSV with same name
-   @param[out]  dndz                     Altitude gradient of n
-   @param[out]  dndlat                   Latitude gradient of n
-   @param[out]  dndlon                   Longitude gradient of n
-   @param[in]   ws                       The workspace
-   @param[in]   refr_index_air_agenda    As the WSV with same name
-   @param[in]   pos                      Position vector
-   @param[in]   do_horizontal_gradients  See above
-   @param[in]   do_twosided_perturb      See above
+    @param[out]  refr_index_air           As the WSV with same name
+    @param[out]  refr_index_air_group     As the WSV with same name
+    @param[out]  dndz                     Altitude gradient of n
+    @param[out]  dndlat                   Latitude gradient of n
+    @param[out]  dndlon                   Longitude gradient of n
+    @param[in]   ws                       The workspace
+    @param[in]   refr_index_air_agenda    As the WSV with same name
+    @param[in]   pos                      Position vector
+    @param[in]   do_horizontal_gradients  See above
+    @param[in]   do_twosided_perturb      See above
 
-   @author Patrick Eriksson
-   @date   2023-01-05
- */
+    @author Patrick Eriksson
+    @date   2023-01-05
+*/
 void refr_index_and_its_gradients(Numeric& refr_index_air,
                                   Numeric& refr_index_air_group,
                                   Numeric& dndz,
@@ -219,5 +219,23 @@ void refr_index_and_its_gradients(Numeric& refr_index_air,
                                   ConstVectorView pos,
                                   const bool& do_horizontal_gradients,
                                   const bool& do_twosided_perturb);
+
+/** Determines the normal vector of the surface
+
+    ...
+
+    @param[out]  normal             ???
+    @param[in]   refellipsoid       As the WSV with same name.
+    @param[in]   surface_elevation  As the WSV with same name.
+    @param[in]   pos                Vector with latitude and longitude
+
+
+    @author  Patrick Eriksson
+    @date    2023-01-07
+*/
+void surface_normal(VectorView normal,
+                    const Vector& refellipsoid,
+                    const GriddedField2& surface_elevation,
+                    ConstVectorView pos);
 
 #endif  // variousZZZ_h
