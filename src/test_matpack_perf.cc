7fe30f4d426f2501b8a9bb1ef00087aec599c28a
Richard Larsson <richard.larsson@uni-hamburg.de>
2023-01-23 09:48:44 +0100

    Make perf composable

--- src/test_matpack_perf.cc
+++ src/test_matpack_perf.cc
@@ -5,11 +5,12 @@
 #include "matpack_view.h"
 
 #include <cstdlib>
+#include <stdexcept>
 
 void test_sum(Index N) {
   Numeric X;
   Vector a(N, 1);
-  const ConstVectorView b = a;
+  auto b = static_cast<ExhaustiveVectorView>(a);
   VectorView c = a;
 
   {
@@ -19,19 +20,19 @@ void test_sum(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Summing a Vector                         "};
+    DebugTime timer{"sum(vec)"};
     X = sum(a);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Summing a const ConstVectorView          "};
+    DebugTime timer{"sum(exview)"};
     X = sum(b);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Summing a VectorView                     "};
+    DebugTime timer{"sum(view)"};
     X = sum(c);
   }
   std::cout << X << '\n';
@@ -43,19 +44,19 @@ void test_sum(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Mean a Vector                         "};
+    DebugTime timer{"mean(vec)"};
     X = mean(a);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Mean a const ConstVectorView          "};
+    DebugTime timer{"mean(exview)"};
     X = mean(b);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Mean a VectorView                     "};
+    DebugTime timer{"mean(view)"};
     X = mean(c);
   }
   std::cout << X << '\n';
@@ -67,19 +68,19 @@ void test_sum(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"NAN-Mean a Vector                         "};
+    DebugTime timer{"nanmean(vec)"};
     X = nanmean(a);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"NAN-Mean a const ConstVectorView          "};
+    DebugTime timer{"nanmean(exview)"};
     X = nanmean(b);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"NAN-Mean a VectorView                     "};
+    DebugTime timer{"nanmean(view)"};
     X = nanmean(c);
   }
   std::cout << X << '\n';
@@ -88,6 +89,9 @@ void test_sum(Index N) {
 void test_dot(Index N) {
   Vector a(N, 1);
   Vector b(N, 1);
+
+  auto ae=static_cast<ExhaustiveVectorView>(a);
+  auto be=static_cast<ExhaustiveVectorView>(b);
 
   VectorView av=a;
   VectorView bv=b;
@@ -100,13 +104,19 @@ void test_dot(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector dot product    "};
+    DebugTime timer{"vec * vec"};
     X = a * b;
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView dot product"};
+    DebugTime timer{"exview * exview"};
+    X = ae * be;
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view * view"};
     X = av * bv;
   }
   std::cout << X << '\n';
@@ -116,6 +126,10 @@ void test_vec_mult(Index N) {
   Vector x(N, 1);
   Matrix A(N, N, 1);
   Vector y(N, 1);
+
+  auto xe=static_cast<ExhaustiveVectorView>(x);
+  MatrixView Ae=static_cast<ExhaustiveMatrixView>(A);
+  auto ye=static_cast<ExhaustiveVectorView>(y);
 
   VectorView xv=x;
   MatrixView Av=A;
@@ -138,14 +152,21 @@ void test_vec_mult(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector vector mult product    "};
+    DebugTime timer{"vec = mat * vec"};
     mult(x, A, y);
     X = sum(x);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView vector mult product"};
+    DebugTime timer{"exview = exmview * exview"};
+    mult(xe, Ae, ye);
+    X = sum(x);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view = mview * view"};
     mult(xv, Av, yv);
     X = sum(xv);
   }
@@ -154,6 +175,7 @@ void test_vec_mult(Index N) {
 
 void test_mat_multiply(Index N) {
   Matrix A(20, 20), C(N, 20, 1), B(20, N, 1);
+  auto Ae=static_cast<ExhaustiveMatrixView>(A), Be=static_cast<ExhaustiveMatrixView>(B), Ce=static_cast<ExhaustiveMatrixView>(C);
   MatrixView Av=A, Bv=B, Cv=C;
 
   Numeric X;
@@ -168,14 +190,21 @@ void test_mat_multiply(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix mult    "};
+    DebugTime timer{"mat = mat * mat"};
     mult(A, B, C);
     X = A(0, 0);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView mult"};
+    DebugTime timer{"exmview = exmview * exmview"};
+    mult(Ae, Be, Ce);
+    X = Ae(0, 0);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview = mview * mview"};
     mult(Av, Bv, Cv);
     X = Av(0, 0);
   }
@@ -184,6 +213,7 @@ void test_mat_multiply(Index N) {
 
 void test_elementary_ops_vec(Index N) {
   Vector A(N, 1);
+  auto Ae = static_cast<ExhaustiveVectorView>(A);
   VectorView Av = A;
 
   Numeric X;
@@ -199,70 +229,105 @@ void test_elementary_ops_vec(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector Set    "};
+    DebugTime timer{"vec = 1"};
     A = 1;
     X = A[0] + A[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView Set"};
+    DebugTime timer{"exview = 1"};
+    Ae = 1;
+    X = Ae[0] + Ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view = 1"};
     Av = 1;
     X = Av[0] + Av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector PlusEq    "};
+    DebugTime timer{"vec += 1.33"};
     A += 1.33;
     X = A[0] + A[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView PlusEq"};
+    DebugTime timer{"exview += 1.33"};
+    Ae += 1.33;
+    X = Ae[0] + Ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view += 1.33"};
     Av += 1.33;
     X = Av[0] + Av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector MinusEq    "};
+    DebugTime timer{"vec -= 1.5"};
     A -= 1.5;
     X = A[0] + A[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView MinusEq"};
+    DebugTime timer{"exview -= 1.5"};
+    Ae -= 1.5;
+    X = Ae[0] + Ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view -= 1.5"};
     Av -= 1.5;
     X = Av[0] + Av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector TimesEq    "};
+    DebugTime timer{"vec *= 3.5"};
     A *= 3.5;
     X = A[0] + A[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView TimesEq"};
+    DebugTime timer{"exview *= 3.5"};
+    Ae *= 3.5;
+    X = Ae[0] + Ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view *= 3.5"};
     Av *= 3.5;
     X = Av[0] + Av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector DivEq    "};
+    DebugTime timer{"vec /= 7.77"};
     A /= 7.77;
     X = A[0] + A[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView DivEq"};
+    DebugTime timer{"exview /= 7.77"};
+    Ae /= 7.77;
+    X = Ae[0] + Ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view /= 7.77"};
     Av /= 7.77;
     X = Av[0] + Av[N-1];
   }
@@ -271,6 +336,7 @@ void test_elementary_ops_vec(Index N) {
 
 void test_elementary_ops_mat(Index N) {
   Matrix A(N, N, 1);
+  auto Ae = static_cast<ExhaustiveMatrixView>(A);
   MatrixView Av = A;
 
   Numeric X;
@@ -286,70 +352,105 @@ void test_elementary_ops_mat(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix Set    "};
+    DebugTime timer{"mat = 1"};
     A = 1;
     X = A(0, 0) + A(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView Set"};
+    DebugTime timer{"exmview = 1"};
+    Ae = 1;
+    X = Ae(0, 0) + Ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview = 1"};
     Av = 1;
     X = Av(0, 0) + Av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix PlusEq    "};
+    DebugTime timer{"mat += 1.33"};
     A += 1.33;
     X = A(0, 0) + A(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView PlusEq"};
+    DebugTime timer{"exmview += 1.33"};
+    Ae += 1.33;
+    X = Ae(0, 0) + Ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview += 1.33"};
     Av += 1.33;
     X = Av(0, 0) + Av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix MinusEq    "};
+    DebugTime timer{"mat -= 1.5"};
     A -= 1.5;
     X = A(0, 0) + A(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView MinusEq"};
+    DebugTime timer{"exmview -= 1.5"};
+    Ae -= 1.5;
+    X = Ae(0, 0) + Ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview -= 1.5"};
     Av -= 1.5;
     X = Av(0, 0) + Av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix TimesEq    "};
+    DebugTime timer{"mat *= 3.5"};
     A *= 3.5;
     X = A(0, 0) + A(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView TimesEq"};
+    DebugTime timer{"exmview *= 3.5"};
+    Ae *= 3.5;
+    X = Ae(0, 0) + Ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview *= 3.5"};
     Av *= 3.5;
     X = Av(0, 0) + Av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix DivEq    "};
+    DebugTime timer{"mat /= 7.77"};
     A /= 7.77;
     X = A(0, 0) + A(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView DivEq"};
+    DebugTime timer{"exmview /= 7.77"};
+    Ae /= 7.77;
+    X = Ae(0, 0) + Ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview /= 7.77"};
     Av /= 7.77;
     X = Av(0, 0) + Av(N-1, N-1);
   }
@@ -359,6 +460,8 @@ void test_elementary_ops_mat(Index N) {
 void test_ops_vec(Index N) {
   Vector a(N, 1.1);
   Vector b(N, 2.2);
+  auto ae=static_cast<ExhaustiveVectorView>(a);
+  auto be=static_cast<ExhaustiveVectorView>(b);
   VectorView av=a;
   VectorView bv=b;
 
@@ -374,56 +477,84 @@ void test_ops_vec(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector     ADD"};
+    DebugTime timer{"vec += vec"};
     a += b;
     X = a[0] + a[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView ADD"};
+    DebugTime timer{"exview += exview"};
+    ae += be;
+    X = ae[0] + ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view += view"};
     av += bv;
     X = av[0] + av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector     SUB"};
+    DebugTime timer{"vec -= vec"};
     a -= b;
     X = a[0] + a[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView SUB"};
+    DebugTime timer{"exview -= exview"};
+    ae -= be;
+    X = ae[0] + ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view -= view"};
     av -= bv;
     X = av[0] + av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector     MUL"};
+    DebugTime timer{"vec *= vec"};
     a *= b;
     X = a[0] + a[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView MUL"};
+    DebugTime timer{"exview *= exview"};
+    ae *= be;
+    X = ae[0] + ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view *= exview"};
     av *= bv;
     X = av[0] + av[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Vector     DIV"};
+    DebugTime timer{"vec /= vec"};
     a /= b;
     X = a[0] + a[N-1];
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"VectorView DIV"};
+    DebugTime timer{"exview /= exview"};
+    ae /= be;
+    X = ae[0] + ae[N-1];
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"view /= view"};
     av /= bv;
     X = av[0] + av[N-1];
   }
@@ -433,6 +564,8 @@ void test_ops_vec(Index N) {
 void test_ops_mat(Index N) {
   Matrix a(N, N, 1.1);
   Matrix b(N, N, 2.2);
+  auto ae=static_cast<ExhaustiveMatrixView>(a);
+  auto be=static_cast<ExhaustiveMatrixView>(b);
   MatrixView av=a;
   MatrixView bv=b;
 
@@ -448,56 +581,84 @@ void test_ops_mat(Index N) {
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix     ADD"};
+    DebugTime timer{"mat += mat"};
     a += b;
     X = a(0, 0) + a(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView ADD"};
+    DebugTime timer{"exmview += exmview"};
+    ae += be;
+    X = ae(0, 0) + ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview += mview"};
     av += bv;
     X = av(0, 0) + av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix     SUB"};
+    DebugTime timer{"mat -= mat"};
     a -= b;
     X = a(0, 0) + a(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView SUB"};
+    DebugTime timer{"exmview -= exmview"};
+    ae -= be;
+    X = ae(0, 0) + ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview -= mview"};
     av -= bv;
     X = av(0, 0) + av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix     MUL"};
+    DebugTime timer{"mat *= mat"};
     a *= b;
     X = a(0, 0) + a(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView MUL"};
+    DebugTime timer{"exmview *= exmview"};
+    ae *= be;
+    X = ae(0, 0) + ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview *= mview"};
     av *= bv;
     X = av(0, 0) + av(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"Matrix     DIV"};
+    DebugTime timer{"mat /= mat"};
     a /= b;
     X = a(0, 0) + a(N-1, N-1);
   }
   std::cout << X << '\n';
 
   {
-    DebugTime timer{"MatrixView DIV"};
+    DebugTime timer{"exmview /= exmview"};
+    ae /= be;
+    X = ae(0, 0) + ae(N-1, N-1);
+  }
+  std::cout << X << '\n';
+
+  {
+    DebugTime timer{"mview /= mview"};
     av /= bv;
     X = av(0, 0) + av(N-1, N-1);
   }
@@ -505,15 +666,23 @@ void test_ops_mat(Index N) {
 }
 
 int main(int argc, char** c) {
-  Index N = 100'000;
-  if (argc == 2) N = static_cast<Index>(std::atoll(c[1]));
+  std::array <Index, 8> N;
+  if (static_cast<std::size_t>(argc) < 1 + 1 + N.size()) {
+    std::cerr << "Expects PROGNAME NREPEAT NSIZE..., wehere NSIZE is " << N.size() << " indices\n";
+    return EXIT_FAILURE;
+  }
 
-  //test_sum(N);
-  //test_dot(N);
-  //test_vec_mult(N);
-  //test_mat_multiply(N);
-  //test_elementary_ops_vec(N);
-  //test_elementary_ops_mat(N);
-  //test_ops_vec(N);
-  test_ops_mat(N);
+  const auto n = static_cast<Index>(std::atoll(c[1]));
+  for (std::size_t i=0; i<N.size(); i++)  N[i] = static_cast<Index>(std::atoll(c[2 + i]));
+
+  for (Index i=0; i<n; i++) {
+    test_sum(N[0]);
+    test_dot(N[1]);
+    test_vec_mult(N[2]);
+    test_mat_multiply(N[3]);
+    test_elementary_ops_vec(N[4]);
+    test_elementary_ops_mat(N[5]);
+    test_ops_vec(N[6]);
+    test_ops_mat(N[7]);
+  }
 }
