18ee0b3227fc98e01595d14db3267f194295982d
Richard Larsson <richard.larsson@uni-hamburg.de>
2023-02-02 11:52:34 +0100

    Make make check work

--- src/test_species_tags.cc
+++ src/test_species_tags.cc
@@ -1,16 +1,19 @@
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
 #include "species_tags.h"
 
-int main(int argc, char ** argv) {
-std::cout << Species::parse_tag(argv[1]) << '\n';
-  /*
+int main(int argc, char ** argv) try {
   ARTS_USER_ERROR_IF(argc not_eq 2, "Call as ", argv[0], " SPECIES_STRING");
   
   const ArrayOfSpeciesTag tags(argv[1]);
-  std::cout << tags << ' ' << tags.Species() << '\n';
   for (auto& x: tags) {
     std::cout << x << ' ' << x.Isotopologue() << ' ' << x.type << '\n';
   }
-  */
+
+  return EXIT_SUCCESS;
+} catch (std::runtime_error& e) {
+  std::cerr << e.what() << '\n';
+  return EXIT_FAILURE;
 }
