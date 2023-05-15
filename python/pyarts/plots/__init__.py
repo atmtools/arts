c4accabfb53c7cd5dbef9d3468d097bd3ff86b1c
Richard Larsson <richard.larsson@uni-hamburg.de>
2023-05-15 08:57:59 +0200

    Make the plotting code available on import

--- python/pyarts/plots/__init__.py
+++ python/pyarts/plots/__init__.py
@@ -2,5 +2,6 @@
 
 from pyarts.plots.arts_lookup import *  # noqa
 from pyarts.plots.ppath import *  # noqa
+from pyarts.plots.ppvar_atm import * # noqa
 
 __all__ = [s for s in dir() if not s.startswith("_")]
