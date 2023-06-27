eb4413effe19c476c24d7df2ed9a856a9e4600cc
Richard Larsson <richard.larsson@uni-hamburg.de>
2023-06-27 09:37:40 +0200

    Better convert docs

--- src/python_interface/py_convert.cpp
+++ src/python_interface/py_convert.cpp
@@ -6,10 +6,15 @@ namespace Python {
 namespace py = pybind11;
 
 //! Wraps a conversion function with basic information
-#define PythonInterfaceConvert(name, t, from, to)  \
-  convert.def(#name,                               \
-              py::vectorize(&Conversion::name<t>), \
-              py::doc("Converts from " from " to " to))
+#define PythonInterfaceConvert(name, t, from, to, var)                                 \
+  convert.def(                                                                         \
+      #name,                                                                           \
+      py::vectorize(&Conversion::name<t>),                                             \
+      py::arg(var),                                                                    \
+      py::doc(                                                                         \
+          "Converts from " from " to " to "\n\nParameters:\n    " var                  \
+          " (:class:`~float` or :class:`~list` or :class:`~numpy.ndarray`): " from \
+          "\n\nReturn\n------\n:class:`~float` or :class:`~list` or :class:`~numpy.ndarray`\n    " to))
 
 void py_conversions(py::module_& m) {
   auto convert = m.def_submodule("convert");
@@ -17,17 +22,17 @@ void py_conversions(py::module_& m) {
 
 These all should work with any array-like type
 )--";
-  PythonInterfaceConvert(freq2kaycm, Numeric, "Frequency [Hz]", "Kayser [cm-1]");
-  PythonInterfaceConvert(kaycm2freq, Numeric, "Kayser [cm-1]", "Frequency [Hz]");
-  PythonInterfaceConvert(freq2wavelen, Numeric, "Frequency [Hz]", "Wavelenth [m]");
-  PythonInterfaceConvert(wavelen2freq, Numeric, "Wavelenth [m]", "Frequency [Hz]");
-  PythonInterfaceConvert(pa2torr, Numeric, "Pressure [Pa]", "Torr [Torr]");
-  PythonInterfaceConvert(torr2pa, Numeric, "Torr [Torr]", "Pressure [Pa]");
-  PythonInterfaceConvert(hz_per_msquared2kaycm_per_cmsquared, Numeric, "Spectral line intensity [Hz/m2]", "CGS spectral line intensity [cm-1/cm-2]");
-  PythonInterfaceConvert(kaycm_per_cmsquared2hz_per_msquared, Numeric, "CGS spectral line intensity [cm-1/cm-2]", "Spectral line intensity [Hz/m2]");
-  PythonInterfaceConvert(hz_per_pa2kaycm_per_atm, Numeric, "Line shape parameter [Hz/Pa]", "CGS line shape parameter [cm-1/atm]");
-  PythonInterfaceConvert(kaycm_per_atm2hz_per_pa, Numeric, "CGS line shape parameter [cm-1/atm]", "Line shape parameter [Hz/Pa]");
-  PythonInterfaceConvert(joule2kaycm, Numeric, "Energy [J]", "CGS Energy [cm-1]");
-  PythonInterfaceConvert(kaycm2joule, Numeric, "CGS Energy [cm-1]", "Energy [J]");
+  PythonInterfaceConvert(freq2kaycm, Numeric, "Frequency [Hz]", "Kayser [cm-1]", "f");
+  PythonInterfaceConvert(kaycm2freq, Numeric, "Kayser [cm-1]", "Frequency [Hz]", "v");
+  PythonInterfaceConvert(freq2wavelen, Numeric, "Frequency [Hz]", "Wavelenth [m]", "f");
+  PythonInterfaceConvert(wavelen2freq, Numeric, "Wavelenth [m]", "Frequency [Hz]", "wl");
+  PythonInterfaceConvert(pa2torr, Numeric, "Pressure [Pa]", "Torr [Torr]", "pa");
+  PythonInterfaceConvert(torr2pa, Numeric, "Torr [Torr]", "Pressure [Pa]", "torr");
+  PythonInterfaceConvert(hz_per_msquared2kaycm_per_cmsquared, Numeric, "Spectral line intensity [Hz/m2]", "CGS spectral line intensity [cm-1/cm-2]", "s");
+  PythonInterfaceConvert(kaycm_per_cmsquared2hz_per_msquared, Numeric, "CGS spectral line intensity [cm-1/cm-2]", "Spectral line intensity [Hz/m2]", "s_cgs");
+  PythonInterfaceConvert(hz_per_pa2kaycm_per_atm, Numeric, "Line shape parameter [Hz/Pa]", "CGS line shape parameter [cm-1/atm]", "lsm");
+  PythonInterfaceConvert(kaycm_per_atm2hz_per_pa, Numeric, "CGS line shape parameter [cm-1/atm]", "Line shape parameter [Hz/Pa]", "lsm_cgs");
+  PythonInterfaceConvert(joule2kaycm, Numeric, "Energy [J]", "CGS Energy [cm-1]", "j");
+  PythonInterfaceConvert(kaycm2joule, Numeric, "CGS Energy [cm-1]", "Energy [J]", "v");
 }
 }  // namespace Python
