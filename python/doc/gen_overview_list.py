745f40f8cad11e670ce771e9ecdfea15de869a81
Oliver Lemke <olemke@gmail.com>
2023-07-05 08:19:43 +0200

    Case-insensitive sorting of docs

--- python/doc/gen_overview_list.py
+++ python/doc/gen_overview_list.py
@@ -2,39 +2,45 @@ import pyarts
 import sys
 
 
+def sort_ignore_case(entities):
+    sorted = [str(e.name) for e in entities]
+    sorted.sort(key=lambda x: x.lower())
+    return sorted
+
+
 def agendas():
-    vars = pyarts.arts.globals.get_agenda_data()
+    vars = sort_ignore_case(pyarts.arts.globals.get_agenda_data())
     txt = """.. hlist::
 """
     for var in vars:
-        txt += f"    * :attr:`~pyarts.workspace.Workspace.{var.name}`\n"
+        txt += f"    * :attr:`~pyarts.workspace.Workspace.{var}`\n"
     return txt
 
 
 def variables():
-    vars = pyarts.arts.globals.get_wsv_data()
+    vars = sort_ignore_case(pyarts.arts.globals.get_wsv_data())
     txt = """.. hlist::
 """
     for var in vars:
-        txt += f"    * :attr:`~pyarts.workspace.Workspace.{var.name}`\n"
+        txt += f"    * :attr:`~pyarts.workspace.Workspace.{var}`\n"
     return txt
 
 
 def groups():
-    vars = pyarts.arts.globals.get_wsv_groups()
+    vars = sort_ignore_case(pyarts.arts.globals.get_wsv_groups())
     txt = """.. hlist::
 """
     for var in vars:
-        txt += f"    * :attr:`~pyarts.arts.{var.name}`\n"
+        txt += f"    * :attr:`~pyarts.arts.{var}`\n"
     return txt
 
 
 def methods():
-    vars = pyarts.arts.globals.get_md_data_raw()
+    vars = sort_ignore_case(pyarts.arts.globals.get_md_data_raw())
     txt = """.. hlist::
 """
     for var in vars:
-        txt += f"    * :func:`~pyarts.workspace.Workspace.{var.name}`\n"
+        txt += f"    * :func:`~pyarts.workspace.Workspace.{var}`\n"
     return txt
 
 
