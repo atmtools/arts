1e77f4300be2e6982e14d0aaa4cdf28143856422
Manfred Brath <manfred.brath@uni-hamburg.de>
2023-09-28 12:30:31 +0200

    Add documentation

--- src/fluxes.h
+++ src/fluxes.h
@@ -11,6 +11,12 @@
 
 #include "matpack_data.h"
 
+/** Calculates the derivative of the irradiance_field with respect to pressure.
+ *
+ * @param[out] flux_divergence The output tensor containing the calculated flux divergence.
+ * @param[in] p_grid The vector of grid points at which to calculate the derivative.
+ * @param[in] irradiance_field The input tensor containing the irradiance field.
+ */
 void FluxDivergenceFromIrradiance(Tensor3& flux_divergence,
                                  const Vector& p_grid,
                                  const Tensor4& irradiance_field);
