1eeb422a92460ee3d4c35ace869e01ae1e4763a7
Richard Larsson <richard.larsson@uni-hamburg.de>
2023-10-16 17:35:38 +0200

    Make GUI follow ARTS color scheme

--- src/gui/gui.cc
+++ src/gui/gui.cc
@@ -1,5 +1,7 @@
 #include "gui.h"
 #include <xml_io.h>
+#include "imgui.h"
+#include "implot.h"
 #include "propagationmatrix.h"
 
 namespace ARTSGUI {
@@ -16,65 +18,37 @@ namespace ARTSGUI {
     style.AntiAliasedLinesUseTex = true;
     style.AntiAliasedFill = true;
     style.ChildRounding = 0.0f;
+
+    const ImVec4 arts_blue{2.f/255.f, 113.f/255.f, 187.f/255.f, 1.0f};
+    const ImVec4 arts_black{0.f, 0.f, 0.f, 1.0f};
+    const ImVec4 arts_grey{0.44f, 0.44f, 0.44f, 1.0f};
+    const ImVec4 arts_white{1.f, 1.f, 1.f, 1.0f};
     
-    style.Colors[ImGuiCol_Text] = ImVec4(0.92f, 0.92f, 0.92f, 1.00f);
-    style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.44f, 0.44f, 0.44f, 1.00f);
-    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
-    style.Colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
-    style.Colors[ImGuiCol_Border] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
-    style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-    style.Colors[ImGuiCol_FrameBg] = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
-    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
-    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
-    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
-    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
-    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.06f, 0.06f, 0.06f, 0.53f);
-    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.21f, 0.21f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_ScrollbarGrabHovered] =
-    ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
-    style.Colors[ImGuiCol_ScrollbarGrabActive] =
-    ImVec4(0.81f, 0.83f, 0.81f, 1.00f);
-    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_Button] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
-    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_Header] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
-    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.93f, 0.65f, 0.14f, 1.00f);
-    style.Colors[ImGuiCol_Separator] = ImVec4(0.21f, 0.21f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.21f, 0.21f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_Tab] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
-    style.Colors[ImGuiCol_TabHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
-    style.Colors[ImGuiCol_TabActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
-    style.Colors[ImGuiCol_TabUnfocused] = ImVec4(0.07f, 0.10f, 0.15f, 0.97f);
-    style.Colors[ImGuiCol_TabUnfocusedActive] =
-    ImVec4(0.14f, 0.26f, 0.42f, 1.00f);
-    style.Colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
-    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
-    style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
-    style.Colors[ImGuiCol_PlotHistogramHovered] =
-    ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
-    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
-    style.Colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
-    style.Colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-    style.Colors[ImGuiCol_NavWindowingHighlight] =
-    ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
-    style.Colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
-    style.Colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+    style.Colors[ImGuiCol_Text] = arts_white;
+    style.Colors[ImGuiCol_TextDisabled] = arts_grey;
+    style.Colors[ImGuiCol_WindowBg] = arts_black;
+    style.Colors[ImGuiCol_Border] = arts_blue;
     
     auto &plot_style = ImPlot::GetStyle();
     plot_style.UseLocalTime = true;
     plot_style.UseISO8601 = true;
     plot_style.Use24HourClock = true;
+
+    // Line colors ???
+    const ImVec4 arts_yellow{235.f/255.f, 172.f/255.f, 35.f/255.f, 1.0f};
+    const ImVec4 arts_red{184.f/255.f, 0.f/255.f, 88.f/255.f, 1.0f};
+    const ImVec4 arts_green{0.f/255.f, 110.f/255.f, 0.f/255.f, 1.0f};
+    const ImVec4 arts_pink{255.f/255.f, 146.f/255.f, 135.f/255.f, 1.0f};
+    const ImVec4 arts_brown{178.f/255.f, 69.f/255.f, 2.f/255.f, 1.0f};
+    const ImVec4 arts_teal{0.f/255.f, 187.f/255.f, 173.f/255.f, 1.0f};
+    static const std::array<ImVec4, 7> arts_line_colors{arts_blue,
+                                                        arts_yellow,
+                                                        arts_red,
+                                                        arts_green,
+                                                        arts_pink,
+                                                        arts_brown,
+                                                        arts_teal};
+    ImPlot::SetColormap(arts_line_colors.data(), arts_line_colors.size());
 }
 
 GLFWmonitor *get_current_monitor(GLFWwindow *window) {
