f1ed8205ee02c09d54a427380e3f56ca94b5e52c
Oliver Lemke <olemke@gmail.com>
2023-10-16 15:39:04 +0200

    Add ARTS logo to navbar and landing page

--- python/doc/source/conf.py
+++ python/doc/source/conf.py
@@ -56,7 +56,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'pyarts'
+project = 'ARTS'
 copyright = 'The ARTS Developers'
 author = 'The ARTS Developers'
 
@@ -128,7 +128,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+}
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
@@ -141,7 +144,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_images/arts-logo-light-bg.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
