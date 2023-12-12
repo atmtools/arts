08769c9bc6d0b78553135298e6ed2c3f6e4004c3
Oliver Lemke <olemke@gmail.com>
2023-12-12 12:37:21 +0100

    Fix istream unget bug
    Use seekg instead of unget to move around while parsing XML files.
    Unget could lead to unexpected failures in rare cases as reported by
    Freddy. So far, only happened on macOS with Clang for one particular
    file.
    
    The fix requires the input stream to be seekable. Therefore, gzipped
    xml files will still be read with the old method.
    
    Also move implementation of double_imanip to cc file.

--- src/double_imanip.h
+++ src/double_imanip.h
@@ -13,46 +13,11 @@
 #ifndef double_imanip_h
 #define double_imanip_h
 
-#include <fstream>
-#include <limits>
-#include <stdexcept>
-
-#include "debug.h"
-
-#include <fast_float/fast_float.h>
-
 /** Input manipulator class for doubles to enable nan and inf parsing. */
+#include <istream>
 class double_imanip {
  public:
-  const double_imanip& operator>>(double& x) const {
-    std::istream& is = *in;
-    std::string buf;
-
-    // Read to the buffer
-    is >> buf;
-    ARTS_USER_ERROR_IF(is.fail(), "Cannot read from stream")
-
-    // Actual conversion
-    const auto res =
-        fast_float::from_chars(buf.c_str(), buf.c_str() + buf.size(), x);
-
-    // Error (only std::errc::invalid_argument possible)
-    ARTS_USER_ERROR_IF(res.ec == std::errc::invalid_argument,
-                       "The argument: \n\n'",
-                       buf,
-                       R"--('
-
-is not convertible to a valid double.  At the very least it
-cannot be converted to one using the standard string-to-double
-routine
-)--")
-
-    // Put the stream to be where it is supposed to be
-    std::size_t n = std::distance(buf.c_str(), res.ptr);
-    while (n++ < buf.size()) is.unget();
-
-    return *this;
-  }
+  const double_imanip& operator>>(double& x) const;
 
   std::istream& operator>>(const double_imanip&) const { return *in; }
 
