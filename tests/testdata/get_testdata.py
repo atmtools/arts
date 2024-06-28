6005a50186e77434bf8f59eb2cc859f75a79376c
Oliver Lemke <olemke@gmail.com>
2024-06-28 14:28:51 +0200

    Improve get_testdata

--- tests/testdata/get_testdata.py
+++ tests/testdata/get_testdata.py
@@ -7,28 +7,22 @@ nargs = len(sys.argv)
 xml = "https://arts.mi.uni-hamburg.de/svn/rt/arts-xml-data/trunk/"
 cat = "https://arts.mi.uni-hamburg.de/svn/rt/arts-cat-data/trunk/"
 
-if nargs > 3:
-    try:
-        path = os.getcwd()
-        os.chdir(sys.argv[1])
-        if sys.argv[2] == "xml":
-            baseurl = xml
-            basedir = "arts-xml-data"
-        elif sys.argv[2] == "cat":
-            baseurl = cat
-            basedir = "arts-cat-data"
-        else:
-            baseurl = ""
-            basedir = "custom-data"
-        
-        for file in sys.argv[3:]:
-            f = f"{basedir}/{file}"
-            if not os.path.exists(f):
-                os.makedirs(os.path.split(f)[0], exist_ok=True)
-                print(f"Downloading {f}")
-                urllib.request.urlretrieve(f"{baseurl}{file}", f)
-            else:
-                print(f"Skipping {file}, exists at {os.path.abspath(f)}")
-    finally:
-        os.chdir(path)
+if sys.argv[1] == "xml":
+    baseurl = xml
+    basedir = "arts-xml-data"
+elif sys.argv[1] == "cat":
+    baseurl = cat
+    basedir = "arts-cat-data"
+else:
+    baseurl = ""
+    basedir = "custom-data"
 
+for file in sys.argv[2:]:
+    f = f"{basedir}/{file}"
+    if not os.path.exists(f):
+        os.makedirs(os.path.split(f)[0], exist_ok=True)
+        print(f"Downloading {f}")
+        urllib.request.urlretrieve(f"{baseurl}{file}", f)
+        print(f"Downloaded {f}")
+    else:
+        print(f"Skipping {file}, exists at {os.path.abspath(f)}")
