3f3dab3c78ed64a2389de0f3796ce2b872339447
Oliver Lemke <olemke@gmail.com>
2024-08-21 08:15:56 +0200

    Reenable GIL in Numeric*Operator

--- src/python_interface/py_operators.cpp
+++ src/python_interface/py_operators.cpp
@@ -10,7 +10,13 @@
 namespace Python {
 void py_operators(py::module_& m) {
   py::class_<NumericUnaryOperator> nuop(m, "NumericUnaryOperator");
-  nuop.def(py::init_implicit<NumericUnaryOperator::func_t>())
+  nuop.def("__init__",
+           [](NumericUnaryOperator* op, NumericUnaryOperator::func_t f) {
+             new (op) NumericUnaryOperator([f](Numeric x) {
+               py::gil_scoped_acquire gil{};
+               return f(x);
+             });
+           })
       .def(
           "__call__",
           [](NumericUnaryOperator& f, py::object x) {
@@ -18,9 +24,35 @@ void py_operators(py::module_& m) {
           },
           "x"_a);
   workspace_group_interface(nuop);
+  py::implicitly_convertible<NumericUnaryOperator::func_t, NumericUnaryOperator>();
+
+  py::class_<NumericBinaryOperator> nbop(m, "NumericBinaryOperator");
+  nbop.def("__init__",
+           [](NumericBinaryOperator* op, NumericBinaryOperator::func_t f) {
+             new (op) NumericBinaryOperator([f](Numeric x, Numeric y) {
+               py::gil_scoped_acquire gil{};
+               return f(x, y);
+             });
+           })
+      .def(
+          "__call__",
+          [](NumericBinaryOperator& f, py::object x, py::object y) {
+            return vectorize(f.f, x, y);
+          },
+          "x"_a,
+          "y"_a);
+  workspace_group_interface(nbop);
+  py::implicitly_convertible<NumericBinaryOperator::func_t, NumericBinaryOperator>();
 
   py::class_<NumericTernaryOperator> ntop(m, "NumericTernaryOperator");
-  ntop.def(py::init_implicit<NumericTernaryOperator::func_t>())
+  ntop.def("__init__",
+           [](NumericTernaryOperator* op, NumericTernaryOperator::func_t f) {
+             new (op)
+                 NumericTernaryOperator([f](Numeric x, Numeric y, Numeric z) {
+                   py::gil_scoped_acquire gil{};
+                   return f(x, y, z);
+                 });
+           })
       .def(
           "__call__",
           [](NumericTernaryOperator& f,
@@ -31,5 +63,6 @@ void py_operators(py::module_& m) {
           "y"_a,
           "z"_a);
   workspace_group_interface(ntop);
+  py::implicitly_convertible<NumericTernaryOperator::func_t, NumericTernaryOperator>();
 }
 }  // namespace Python
