351a108d3c7422b0d2362884cca2d40ee39b1846
Richard Larsson <richard.larsson@uni-hamburg.de>
2024-08-26 17:25:15 +0200

    More split, small optimization

--- src/workspace_wsv_other_fn.cpp
+++ src/workspace_wsv_other_fn.cpp
@@ -5,5 +5,3 @@ std::size_t Wsv::index() const { return value.index(); }
 bool Wsv::holds_same(const Wsv& other) const {
   return index() == other.index();
 }
-
-const std::string_view Wsv::type_name() const { return name_wsg(value); }
