5244dd5989b35760bc6cca64ea8ec08eb6cf43d9
Oliver Lemke <oliver.lemke@uni-hamburg.de>
2024-09-05 15:20:52 +0200

    Move xml operators to their dedicated file

--- src/xml_operators.cc
+++ src/xml_operators.cc
@@ -1,17 +1,64 @@
-#include "atm.h"
+#include "callback.h"
 #include "debug.h"
+#include "operators.h"
 #include "xml_io.h"
 
-//! AtmFunctionalData
+//=== CallbackOperator =========================================
 
-void xml_read_from_stream(std::istream&, AtmFunctionalData&, bifstream*) {
-  ARTS_USER_ERROR("Cannot read AtmFunctionalData from XML.");
+void xml_read_from_stream(std::istream&,
+                          CallbackOperator&,
+                          bifstream* /* pbifs */) {
+  ARTS_USER_ERROR("Reading operators from XML is not supported.");
 }
 
 void xml_write_to_stream(std::ostream&,
-                         const AtmFunctionalData&,
-                         bofstream*,
-                         const String&) {
-  ARTS_USER_ERROR(
-      "Cannot write AtmFunctionalData to XML, please convert it to valid type first.");
+                         const CallbackOperator&,
+                         bofstream* /* pbofs */,
+                         const String& /* name */) {
+  ARTS_USER_ERROR("Writing operators to XML is not supported.");
+}
+
+//=== NumericUnaryOperator =========================================
+
+void xml_read_from_stream(std::istream&,
+                          NumericUnaryOperator&,
+                          bifstream* /* pbifs */) {
+  ARTS_USER_ERROR("Reading operators from XML is not supported.");
+}
+
+void xml_write_to_stream(std::ostream&,
+                         const NumericUnaryOperator&,
+                         bofstream* /* pbofs */,
+                         const String& /* name */) {
+  ARTS_USER_ERROR("Writing operators to XML is not supported.");
+}
+
+//=== NumericBinaryOperator =========================================
+
+void xml_read_from_stream(std::istream&,
+                          NumericBinaryOperator&,
+                          bifstream* /* pbifs */) {
+  ARTS_USER_ERROR("Reading operators from XML is not supported.");
+}
+
+void xml_write_to_stream(std::ostream&,
+                         const NumericBinaryOperator&,
+                         bofstream* /* pbofs */,
+                         const String& /* name */) {
+  ARTS_USER_ERROR("Writing operators to XML is not supported.");
+}
+
+//=== NumericTernaryOperator =========================================
+
+void xml_read_from_stream(std::istream&,
+                          NumericTernaryOperator&,
+                          bifstream* /* pbifs */) {
+  ARTS_USER_ERROR("Reading operators from XML is not supported.");
+}
+
+void xml_write_to_stream(std::ostream&,
+                         const NumericTernaryOperator&,
+                         bofstream* /* pbofs */,
+                         const String& /* name */) {
+  ARTS_USER_ERROR("Writing operators to XML is not supported.");
 }
