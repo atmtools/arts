2ff42fcc81af3b928ef628264136c61d6164d2bd
Oliver Lemke <olemke@gmail.com>
2024-11-20 10:36:38 +0100

    Extend nb stubs to all arts submodules

---
+++ cmake/modules/ArtsNanobindStubs.cmake
@@ -0,0 +1,27 @@
+function (ARTS_ADD_CPP_STUBS)
+
+  foreach (MODULENAME IN LISTS ARGN)
+    nanobind_add_stub(
+      pyarts_${MODULENAME}_cpp_stub
+      MODULE arts.${MODULENAME}
+      OUTPUT ${ARTS_BINARY_DIR}/python/src/pyarts/arts/${MODULENAME}.pyi
+      PYTHON_PATH ${ARTS_BINARY_DIR}/python/src/pyarts
+      DEPENDS pyarts_cpp
+    )
+    list(APPEND deplist "pyarts_${MODULENAME}_cpp_stub")
+  endforeach()
+
+  nanobind_add_stub(
+    pyarts_cpp_stub
+    MODULE arts
+    OUTPUT ${ARTS_BINARY_DIR}/python/src/pyarts/arts/__init__.pyi
+    PYTHON_PATH ${ARTS_BINARY_DIR}/python/src/pyarts
+    DEPENDS pyarts_cpp "${deplist}"
+  )
+  set_property(
+    TARGET pyarts_cpp_stub
+    APPEND
+    PROPERTY ADDITIONAL_CLEAN_FILES ${ARTS_BINARY_DIR}/python/src/pyarts/arts
+  )
+
+endfunction()
