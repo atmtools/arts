f26e26527dd8d665b348a69cccece7f8ec22a6c6
Oliver Lemke <olemke@gmail.com>
2024-12-03 14:37:13 +0100

    Collect examples/tests from toplevel
    Create and change to working directory in build dir.

--- cmake/modules/ArtsTestcases.cmake
+++ cmake/modules/ArtsTestcases.cmake
@@ -24,10 +24,17 @@ macro (ARTS_TEST_RUN_PYFILE TESTNAME PYFILE)
 
   set(PYFILE "controlfiles/${PYTHONCTLFILE}")
   get_filename_component(CFILESUBDIR ${PYFILE} DIRECTORY)
+  add_custom_target(
+    make_dir_${TESTNAME}_${TESTNAME_LONG}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/${CFILESUBDIR}
+  )
+  add_dependencies(check-deps make_dir_${TESTNAME}_${TESTNAME_LONG})
+
   add_test(
     NAME ${TESTNAME_LONG}
     COMMAND ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/${PYFILE}
-    )
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${CFILESUBDIR}"
+  )
   set_tests_properties(
     ${TESTNAME_LONG} PROPERTIES
     ENVIRONMENT "PYTHONPATH=${ARTS_BINARY_DIR}/python/src;ARTS_HEADLESS=1;ARTS_XML_DATA_DIR=${ARTS_XML_DATA_DIR};ARTS_CAT_DATA_DIR=${ARTS_CAT_DATA_DIR};ARTS_DATA_PATH=${CMAKE_CURRENT_SOURCE_DIR}/${CFILESUBDIR}"
@@ -46,7 +53,6 @@ macro (COLLECT_TEST_SUBDIR SUBDIR)
   foreach(ARTSFILE ${ARTSFILES})
     arts_test_run_ctlfile(${CURRENTDIR} ${ARTSFILE})
   endforeach()
-
 endmacro ()
 
 macro (SETUP_ARTS_CHECKS)
