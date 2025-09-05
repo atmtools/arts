dd477ef6bed1ce5054cda8afc9f49b643a328667
Oliver Lemke <olemke@gmail.com>
2025-09-05 14:01:47 +0200

    Skip stub generation when asan is enabled
    nanobind_add_stub macro breaks with asan.

--- cmake/modules/ArtsNanobindStubs.cmake
+++ cmake/modules/ArtsNanobindStubs.cmake
@@ -1,27 +1,31 @@
 function (ARTS_ADD_CPP_STUBS)
 
-  foreach (MODULENAME IN LISTS ARGN)
-    nanobind_add_stub(
-      pyarts_${MODULENAME}_cpp_stub
-      MODULE pyarts3.arts.${MODULENAME}
-      OUTPUT ${ARTS_BINARY_DIR}/python/src/pyarts3/arts/${MODULENAME}.pyi
-      PYTHON_PATH ${ARTS_BINARY_DIR}/python/src
-      DEPENDS pyarts_cpp
-    )
-    list(APPEND deplist "pyarts_${MODULENAME}_cpp_stub")
-  endforeach()
+  if (ENABLE_ASAN)
+    add_custom_target(pyarts_cpp_stub)
+  else()
+    foreach (MODULENAME IN LISTS ARGN)
+      nanobind_add_stub(
+        pyarts_${MODULENAME}_cpp_stub
+        MODULE pyarts3.arts.${MODULENAME}
+        OUTPUT ${ARTS_BINARY_DIR}/python/src/pyarts3/arts/${MODULENAME}.pyi
+        PYTHON_PATH ${ARTS_BINARY_DIR}/python/src
+        DEPENDS pyarts_cpp
+      )
+      list(APPEND deplist "pyarts_${MODULENAME}_cpp_stub")
+    endforeach()
 
-  nanobind_add_stub(
-    pyarts_cpp_stub
-    MODULE pyarts3.arts
-    OUTPUT ${ARTS_BINARY_DIR}/python/src/pyarts3/arts/__init__.pyi
-    PYTHON_PATH ${ARTS_BINARY_DIR}/python/src
-    DEPENDS pyarts_cpp "${deplist}"
-  )
-  set_property(
-    TARGET pyarts_cpp_stub
-    APPEND
-    PROPERTY ADDITIONAL_CLEAN_FILES ${ARTS_BINARY_DIR}/python/src/pyarts3/arts
-  )
+    nanobind_add_stub(
+      pyarts_cpp_stub
+      MODULE pyarts3.arts
+      OUTPUT ${ARTS_BINARY_DIR}/python/src/pyarts3/arts/__init__.pyi
+      PYTHON_PATH ${ARTS_BINARY_DIR}/python/src
+      DEPENDS pyarts_cpp "${deplist}"
+    )
+    set_property(
+      TARGET pyarts_cpp_stub
+      APPEND
+      PROPERTY ADDITIONAL_CLEAN_FILES ${ARTS_BINARY_DIR}/python/src/pyarts3/arts
+    )
+  endif()
 
 endfunction()
