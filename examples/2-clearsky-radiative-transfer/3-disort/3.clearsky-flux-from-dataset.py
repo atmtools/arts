25c719c0ae8c5e8248ddbd4e6232c8abd4552078
Oliver Lemke <olemke@gmail.com>
2025-09-17 11:52:58 +0200

    Output message why example doesn't work in LGPL

--- examples/2-clearsky-radiative-transfer/3-disort/3.clearsky-flux-from-dataset.py
+++ examples/2-clearsky-radiative-transfer/3-disort/3.clearsky-flux-from-dataset.py
@@ -4,71 +4,74 @@ import xarray as xa
 from dataclasses import dataclass
 import matplotlib.pyplot as plt
 
-if not pyarts.arts.globals.data.is_lgpl:
-    NQuad = 16
-    max_level_step = 1e3
-    atm_latitude = 0.0
-    atm_longitude = 0.0
-    solar_latitude = 0.0
-    solar_longitude = 0.0
-    surface_temperature = 293.0
-    surface_reflectivity = 0.05
-    cutoff = ["ByLine", 750e9]
-    remove_lines_percentile = 70
-    sunfile = "star/Sun/solar_spectrum_QUIET.xml"
-    planet = "Earth"
+if pyarts.arts.globals.data.is_lgpl:
+    print("CKDMT models are not available in LGPL mode, compile with -DENABLE_ARTS_LGPL=0")
+    exit(0)
 
-    xarr = pyarts.data.xarray_open_dataset("atm.nc")
+NQuad = 16
+max_level_step = 1e3
+atm_latitude = 0.0
+atm_longitude = 0.0
+solar_latitude = 0.0
+solar_longitude = 0.0
+surface_temperature = 293.0
+surface_reflectivity = 0.05
+cutoff = ["ByLine", 750e9]
+remove_lines_percentile = 70
+sunfile = "star/Sun/solar_spectrum_QUIET.xml"
+planet = "Earth"
 
-    ws = pyarts.Workspace()
+xarr = pyarts.data.xarray_open_dataset("atm.nc")
 
-    ws.frequency_grid = pyarts.arts.convert.kaycm2freq(np.linspace(500, 2500, 1001))
-    ws.atmospheric_field = pyarts.data.to_atmospheric_field(xarr)
+ws = pyarts.Workspace()
 
-    v = pyarts.data.to_absorption_species(ws.atmospheric_field)
+ws.frequency_grid = pyarts.arts.convert.kaycm2freq(np.linspace(500, 2500, 1001))
+ws.atmospheric_field = pyarts.data.to_atmospheric_field(xarr)
 
-    ws.absorption_species = v
-    ws.ReadCatalogData(ignore_missing=True)
-    ws.propagation_matrix_agendaAuto(T_extrapolfac=1e9)
+v = pyarts.data.to_absorption_species(ws.atmospheric_field)
 
-    for band in ws.absorption_bands:
-        ws.absorption_bands[band].cutoff = cutoff[0]
-        ws.absorption_bands[band].cutoff_value = cutoff[1]
+ws.absorption_species = v
+ws.ReadCatalogData(ignore_missing=True)
+ws.propagation_matrix_agendaAuto(T_extrapolfac=1e9)
 
-    ws.absorption_bands.keep_hitran_s(remove_lines_percentile)
+for band in ws.absorption_bands:
+    ws.absorption_bands[band].cutoff = cutoff[0]
+    ws.absorption_bands[band].cutoff_value = cutoff[1]
 
-    ws.surface_fieldPlanet(option=planet)
+ws.absorption_bands.keep_hitran_s(remove_lines_percentile)
 
-    sun = pyarts.arts.GriddedField2.fromxml(sunfile)
+ws.surface_fieldPlanet(option=planet)
 
-    ws.surface_field["t"] = surface_temperature
+sun = pyarts.arts.GriddedField2.fromxml(sunfile)
 
-    ws.sunFromGrid(
-        sun_spectrum_raw=sun,
-        latitude=solar_latitude,
-        longitude=solar_longitude,
-    )
+ws.surface_field["t"] = surface_temperature
 
-    ws.disort_quadrature_dimension = NQuad
-    ws.disort_fourier_mode_dimension = 1
-    ws.disort_legendre_polynomial_dimension = 1
+ws.sunFromGrid(
+    sun_spectrum_raw=sun,
+    latitude=solar_latitude,
+    longitude=solar_longitude,
+)
 
-    ws.ray_pathGeometricDownlooking(
-        latitude=atm_latitude,
-        longitude=atm_longitude,
-        max_step=max_level_step,
-    )
+ws.disort_quadrature_dimension = NQuad
+ws.disort_fourier_mode_dimension = 1
+ws.disort_legendre_polynomial_dimension = 1
 
-    ws.disort_settings_agendaSetup(
-        sun_setting="Sun",
-        surface_setting="ThermalLambertian",
-        surface_lambertian_value=surface_reflectivity * np.ones_like(ws.frequency_grid),
-    )
+ws.ray_pathGeometricDownlooking(
+    latitude=atm_latitude,
+    longitude=atm_longitude,
+    max_step=max_level_step,
+)
 
-    ws.disort_spectral_flux_fieldFromAgenda()
+ws.disort_settings_agendaSetup(
+    sun_setting="Sun",
+    surface_setting="ThermalLambertian",
+    surface_lambertian_value=surface_reflectivity * np.ones_like(ws.frequency_grid),
+)
 
-    plt.semilogy(pyarts.arts.convert.freq2kaycm(ws.frequency_grid),
-                ws.disort_spectral_flux_field.down_diffuse)
+ws.disort_spectral_flux_fieldFromAgenda()
 
-    f, s = pyarts.plots.AtmField.plot(ws.atmospheric_field,
-                            alts=np.linspace(0, ws.atmospheric_field.top_of_atmosphere))
+plt.semilogy(pyarts.arts.convert.freq2kaycm(ws.frequency_grid),
+            ws.disort_spectral_flux_field.down_diffuse)
+
+f, s = pyarts.plots.AtmField.plot(ws.atmospheric_field,
+                        alts=np.linspace(0, ws.atmospheric_field.top_of_atmosphere))
