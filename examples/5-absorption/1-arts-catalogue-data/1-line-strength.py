a4b4b4ad605093cfc3333c2e5aad2c072f6ee7a6
Oliver Lemke <olemke@gmail.com>
2025-09-25 09:19:35 +0200

    Output band and line info to console

--- examples/5-absorption/1-arts-catalogue-data/1-line-strength.py
+++ examples/5-absorption/1-arts-catalogue-data/1-line-strength.py
@@ -3,6 +3,7 @@ This file will showcase how you can load line data from arts-cat-data
 into the workspace and generate a plot of the line strengths.
 """
 
+import os
 import textwrap
 
 import matplotlib.pyplot as plt
@@ -31,6 +32,10 @@ ws.absorption_bandsReadSpeciesSplitCatalog(basename="lines/")
 # Throw away lines outside the frequency range
 ws.absorption_bandsSelectFrequencyByLine(fmin=f_min, fmax=f_max)
 
+# Bail out if no lines were found inside the frequency range
+if len(ws.absorption_bands) == 0:
+    raise RuntimeError("No lines found in frequency range")
+
 # Get the intensity of the strongest line in each band
 max_strengths = [
     np.max([line.hitran_s(isotopologue, T0=T0) for line in b.lines])
@@ -46,17 +51,29 @@ sorted_bands = {keys[i]: ws.absorption_bands[keys[i]] for i in indices}
 fig, ax = plt.subplots()
 
 # Plot the two bands with the highest line strengths
-for qi, band in list(sorted_bands.items())[:2]:
+for i, (qi, band) in enumerate(list(sorted_bands.items())[:2]):
     # Line center frequencies
     pos = np.array([line.f0 for line in band.lines])
     strengths = [line.hitran_s(isotopologue, T0=T0) for line in band.lines]
+
+    # Print band and line information
+    band_name = f"Band {i + 1}"
+    print(f"{band_name} info:")
+    print(" ", "\n  ".join(textwrap.wrap(f"{qi:h}", width=80)))
+    print(f"{band_name} lines:")
+    for line, strength in zip(band.lines, strengths):
+        line_info = f"{line:h}".split(";")
+        strength = f"Line strength: {strength:.4g}"
+        print(f"  {line_info[0].ljust(25)} {strength.ljust(25)} {line_info[-2]}")
+    print()
+
     color = ax._get_lines.get_next_color()
     ax.vlines(
         pos / 1e12,
         0,
         strengths,
         color=color,
-        label="\n".join(textwrap.wrap(f"{qi:h}", width=40)),
+        label="\n".join(textwrap.wrap(f"{band_name}: {qi:h}", width=40)),
     )
 
 ax.legend(fontsize="x-small")
@@ -68,3 +85,6 @@ ax_wn.set_xlabel("Wavenumber [cm$^{-1}$]")
 ax.set_ylabel("Hitran line strength")
 ax.set_title(isotopologue)
 ax.set_ylim(bottom=0)
+
+if "ARTS_HEADLESS" not in os.environ:
+    plt.show()
