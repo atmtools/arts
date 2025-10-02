4d1085979ff0c4b788d7c77bd8f218b5fbd80483
Oliver Lemke <olemke@gmail.com>
2025-10-02 09:14:57 +0200

    Read all data files with xml.load

---
+++ tests/core/xml/xmlload-arts-xml-data.py
@@ -0,0 +1,9 @@
+from xmlload_helper import pyarts, recurse
+
+for path in pyarts.arts.globals.parameters.datapath:
+    if "arts-xml-data" in path:
+        print(f"arts-xml-data found at {path} - starting test run")
+        recurse(path)
+        print("All XML files read successfully!")
+else:
+    print("arts-xml-data not found - skipping test run")
