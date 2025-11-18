3381ba8e5d32f27a9a87c89f05483cf1c21d1e15
Richard Larsson <ric.larsson@gmail.com>
2025-11-18 18:20:37 +0900

    Add check that it works

---
+++ tests/core/xml/array-extend-append.py
@@ -0,0 +1,26 @@
+import pyarts3 as pyarts
+import numpy as np
+
+x = pyarts.arts.ArrayOfIndex()
+
+assert np.allclose(x, [])
+
+x.append(0)
+
+assert np.allclose(x, [0]), "Cannot append"
+
+pyarts.arts.Index(1).savexml("index1.xml")
+
+x.appendxml("index1.xml")
+
+assert np.allclose(x, [0] + [1]), "Cannot appendxml"
+
+pyarts.arts.ArrayOfIndex([2, 3]).savexml("index2.xml")
+
+x.extendxml("index2.xml")
+
+assert np.allclose(x, [0] + [1] + [2, 3]), "Cannot externdxml"
+
+x.extend([4, 5, 6])
+
+assert np.allclose(x, [0] + [1] + [2, 3] + [4, 5, 6]), "Cannot extend"
