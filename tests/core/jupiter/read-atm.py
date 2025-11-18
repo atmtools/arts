1a871e1410f001db9e18f38379715270478314e2
Richard Larsson <ric.larsson@gmail.com>
2025-11-18 13:10:12 +0900

    Make compat

--- tests/core/jupiter/read-atm.py
+++ tests/core/jupiter/read-atm.py
@@ -2,6 +2,6 @@ import pyarts3 as pyarts
 
 ws = pyarts.Workspace()
 
-ws.atmospheric_fieldRead(toa=500e3,
-                         basename="planets/Jupiter/MPS/",
-                         deal_with_field_component="Ignore")
+ws.atm_fieldRead(toa=500e3,
+                 basename="planets/Jupiter/MPS/",
+                 deal_with_field_component="Ignore")
