acc94777e4110e4e1d2870aab8940f24afbd6ea1
Richard Larsson <ric.larsson@gmail.com>
2025-11-30 15:51:52 +0900

    Restore old reading order...

--- src/core/lbl/lbl_lineshape_model.cpp
+++ src/core/lbl/lbl_lineshape_model.cpp
@@ -294,7 +294,8 @@ std::istream& operator>>(std::istream& is,
 
 std::istream& operator>>(std::istream& is, model& x) try {
   Index v;
-  is >> v >> double_imanip() >> x.T0;
+  is >> double_imanip() >> x.T0;
+  is >> v;
   return is >> x.single_models;
 } catch (const std::exception& e) {
   throw std::runtime_error(
