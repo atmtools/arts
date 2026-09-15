c763b4b9a10d3920c93d7018b9e0e64750372499
Richard Larsson <ric.larsson@gmail.com>
2026-09-15 22:14:14 +0900

    Remove assert

--- src/m_rad.cc
+++ src/m_rad.cc
@@ -70,7 +70,6 @@ void spectral_rad_jacAddPathPropagation(StokvecMatrix                     &spect
       for (auto &w : weights) {
         if (w.second != 0.0) {
           const auto i = w.first + atm_block.x_start;
-          assert(i < static_cast<Size>(nx));
           auto sr = spectral_rad_jac[i];
           std::transform(local.begin(),
                          local.end(),
